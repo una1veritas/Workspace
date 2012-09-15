deaa28710337c3f9dee792fc91c58087c591d10e
Sin Shimozono <una.veritas@me.com>
2012-09-15 15:50:00 +0900

    STM32 directory modified

--- STM32F1/STM32_yagarto_Test/.dep/main.o.d
+++
@@ -1,7 +0,0 @@
-src/main.o: src/main.c \
- /usr/local/yagarto-4.7.1/bin/../lib/gcc/arm-none-eabi/4.7.1/include/stdint.h \
- /usr/local/yagarto-4.7.1/bin/../lib/gcc/arm-none-eabi/4.7.1/../../../../arm-none-eabi/sys-include/stdint.h
-
-/usr/local/yagarto-4.7.1/bin/../lib/gcc/arm-none-eabi/4.7.1/include/stdint.h:
-
-/usr/local/yagarto-4.7.1/bin/../lib/gcc/arm-none-eabi/4.7.1/../../../../arm-none-eabi/sys-include/stdint.h:
---
+++ STM32F4/yagarto_Test/.dep/main.o.d
@@ -0,0 +1,7 @@
+src/main.o: src/main.c \
+ /usr/local/cross/lib/gcc/arm-none-eabi/4.7.1/include/stdint.h \
+ /usr/local/cross/lib/gcc/arm-none-eabi/4.7.1/../../../../arm-none-eabi/include/stdint.h
+
+/usr/local/cross/lib/gcc/arm-none-eabi/4.7.1/include/stdint.h:
+
+/usr/local/cross/lib/gcc/arm-none-eabi/4.7.1/../../../../arm-none-eabi/include/stdint.h:
