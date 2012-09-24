9ae64af001a06d0f59728d0dc8f464a01c5459a3
Sin Shimozono <una.veritas@me.com>
2012-09-24 10:28:03 +0900

    STM32F4 Eclipse Environment

---
+++ STM32F4Discovery_IO_toggle/build/STM32F4-Discovery/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+/Users/sin/Documents/Eclipse/Workspace/STM32F4_library/stm32f4/Utilities/STM32F4-Discovery/stm32f4_discovery.c 
+
+OBJS += \
+./STM32F4-Discovery/stm32f4_discovery.o 
+
+C_DEPS += \
+./STM32F4-Discovery/stm32f4_discovery.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+STM32F4-Discovery/stm32f4_discovery.o: /Users/sin/Documents/Eclipse/Workspace/STM32F4_library/stm32f4/Utilities/STM32F4-Discovery/stm32f4_discovery.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross GCC Compiler'
+	arm-none-eabi-gcc -I"/Users/sin/Documents/Eclipse/Workspace/STM32F4_library/CMSIS/Device/ST/STM32F4xx/Include" -I"/Users/sin/Documents/Eclipse/Workspace/STM32F4_library/CMSIS/Include" -I"/Users/sin/Documents/Eclipse/Workspace/STM32F4Discovery_IO_toggle/IO_toggle" -I"/Users/sin/Documents/Eclipse/Workspace/STM32F4_library/stm32f4/STM32F4xx_StdPeriph_Driver/inc" -I"/Users/sin/Documents/Eclipse/Workspace/STM32F4_library/stm32f4/Utilities/STM32F4-Discovery" -O2 -mcpu=cortex-m4 -mthumb -mlittle-endian -ffreestanding -g -Wall -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
