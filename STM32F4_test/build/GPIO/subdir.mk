40ecdb381c32971155a08b181ede242212999ebb
Sin Shimozono <una.veritas@me.com>
2012-10-01 13:59:04 +0900

    elf

--- STM32F4_test/build/GPIO/subdir.mk
+++ STM32F4_test/build/GPIO/subdir.mk
@@ -4,34 +4,31 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../GPIO/main.c \
 ../GPIO/system_stm32f4xx.c 
 
 S_UPPER_SRCS += \
 ../GPIO/startup_stm32f4xx.S 
 
 OBJS += \
-./GPIO/main.o \
 ./GPIO/startup_stm32f4xx.o \
 ./GPIO/system_stm32f4xx.o 
 
 C_DEPS += \
-./GPIO/main.d \
 ./GPIO/system_stm32f4xx.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-GPIO/%.o: ../GPIO/%.c
-	@echo 'Building file: $<'
-	@echo 'Invoking: Cross GCC Compiler'
-	arm-none-eabi-gcc -DUSE_STDPERIPH_DRIVER=1 -I"/Users/sin/Documents/Eclipse/Workspace/STM32_library/CMSIS/Device/ST/STM32F4xx/Include" -I"/Users/sin/Documents/Eclipse/Workspace/STM32_library/CMSIS/Include" -I"/Users/sin/Documents/Eclipse/Workspace/STM32F4_test/GPIO" -I"/Users/sin/Documents/Eclipse/Workspace/STM32_library/stm32f4/STM32F4xx_StdPeriph_Driver/inc" -I"/Users/sin/Documents/Eclipse/Workspace/STM32F4_test/mycore" -Os -mthumb -mcpu=cortex-m4 -mlittle-endian -g3 -gdwarf-2 -Wall -c -fmessage-length=0  -ffreestanding -std=c99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
-	@echo 'Finished building: $<'
-	@echo ' '
-
 GPIO/%.o: ../GPIO/%.S
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross GCC Assembler'
 	arm-none-eabi-as -I"/Users/sin/Documents/Eclipse/Workspace/STM32_library/CMSIS/Device/ST/STM32F4xx/Include" -I"/Users/sin/Documents/Eclipse/Workspace/STM32_library/CMSIS/Include" -I"/Users/sin/Documents/Eclipse/Workspace/STM32F4_test/GPIO" -I"/Users/sin/Documents/Eclipse/Workspace/STM32_library/stm32f4/STM32F4xx_StdPeriph_Driver/inc" -I"/Users/sin/Documents/Eclipse/Workspace/STM32F4_test/mycore" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+GPIO/%.o: ../GPIO/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross GCC Compiler'
+	arm-none-eabi-gcc -DUSE_STDPERIPH_DRIVER=1 -I"/Users/sin/Documents/Eclipse/Workspace/STM32_library/CMSIS/Device/ST/STM32F4xx/Include" -I"/Users/sin/Documents/Eclipse/Workspace/STM32_library/CMSIS/Include" -I"/Users/sin/Documents/Eclipse/Workspace/STM32F4_test/GPIO" -I"/Users/sin/Documents/Eclipse/Workspace/STM32_library/stm32f4/STM32F4xx_StdPeriph_Driver/inc" -I"/Users/sin/Documents/Eclipse/Workspace/STM32F4_test/mycore" -Os -mthumb -mcpu=cortex-m4 -mlittle-endian -g3 -gdwarf-2 -Wall -c -fmessage-length=0  -ffreestanding -std=c99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
