fe42e9a7583c9f6070117202334b38f40806bc88
Sin Shimozono <una.veritas@me.com>
2012-11-04 14:30:27 +0900

    succ.

---
+++ stm32f4-i2c/build/build_flash/subdir.mk
@@ -0,0 +1,15 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+O_SRCS += \
+../build_flash/main.o \
+../build_flash/newlib_stubs.o \
+../build_flash/startup_stm32f4xx.o \
+../build_flash/stm32f4xx_it.o \
+../build_flash/system_stm32f4xx.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+
--- stm32f4-i2c/build/sources.mk
+++ stm32f4-i2c/build/sources.mk
@@ -23,6 +23,10 @@ C_UPPER_DEPS :=
 
 # Every subdirectory with source files must be described here
 SUBDIRS := \
+uVision \
 . \
 stdperiph_driver \
+build_flash \
+build_flash/stdperiph_driver \
+build_flash/armcore \
 
