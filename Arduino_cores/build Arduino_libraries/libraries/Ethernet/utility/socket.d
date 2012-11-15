96bffae9ad97c1d81480a460b1ecea66b34a4a42
Sin Shimozono <una.veritas@me.com>
2012-11-16 08:44:29 +0900

    Arduino_cores dropped

--- Arduino_cores/build Arduino_libraries/libraries/Ethernet/utility/socket.d
+++
@@ -1,8 +0,0 @@
-libraries/Ethernet/utility/socket.d libraries/Ethernet/utility/socket.o:  \
- ../libraries/Ethernet/utility/socket.cpp \
-  ../libraries/Ethernet/utility/w5100.h \
-  ../libraries/Ethernet/utility/socket.h
-
-../libraries/Ethernet/utility/w5100.h:
-
-../libraries/Ethernet/utility/socket.h:
--- Arduino_cores/build Arduino_libraries/libraries/Ethernet/utility/w5100.d
+++
@@ -1,5 +0,0 @@
-libraries/Ethernet/utility/w5100.d libraries/Ethernet/utility/w5100.o:  \
- ../libraries/Ethernet/utility/w5100.cpp \
-  ../libraries/Ethernet/utility/w5100.h
-
-../libraries/Ethernet/utility/w5100.h:
