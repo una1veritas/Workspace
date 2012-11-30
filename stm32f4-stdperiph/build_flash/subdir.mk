e9b6c7b407283fb997893beb23cbf3355785e682
Sin Shimozono <una.veritas@me.com>
2012-11-30 09:05:47 +0900

    modified:   Arduino_Home_libraries/.project 	modified:   Java/SQLTimeTabling++/bin/SQLTimeTabling.class 	modified:   Java/SQLTimeTabling++/bin/TimeTableScheduling.class 	modified:   Java/SQLTimeTabling++/bin/data/Period.class 	modified:   Java/SQLTimeTabling++/bin/data/Processor.class 	modified:   Java/SQLTimeTabling++/bin/data/SQLRule.class 	modified:   Java/SQLTimeTabling++/bin/data/Task.class 	modified:   Java/SQLTimeTabling++/bin/data/Timetable.class 	modified:   Java/SQLTimeTabling++/bin/data/Triplet.class 	modified:   stm32f4-float-math_test/.project 	modified:   stm32f4-i2c/.project 	modified:   stm32f4-i2c_lcd/.project 	modified:   stm32f4-i2c_rtc/.project 	modified:   stm32f4-stdperiph/.cproject 	modified:   stm32f4-stdperiph/.project 	modified:   stm32f4-stdperiph/.settings/org.eclipse.cdt.core.prefs 	modified:   stm32f4-stdperiph/build_flash/makefile 	modified:   stm32f4-stdperiph/build_flash/objects.mk 	modified:   stm32f4-stdperiph/build_flash/sources.mk 	deleted:    stm32f4-stdperiph/build_flash/stm32f4-stdperiph.elf 	modified:   stm32f4-stdperiph/build_flash/subdir.mk 	modified:   stm32f4-usart/.project

--- stm32f4-stdperiph/build_flash/subdir.mk
+++ stm32f4-stdperiph/build_flash/subdir.mk
@@ -33,14 +33,14 @@ C_DEPS += \
 %.o: ../%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross GCC Compiler'
-	arm-none-eabi-gcc -DUSE_STDPERIPH_DRIVER=1 -I"/Users/sin/Documents/Eclipse/Workspace/STM32Library/CMSIS/Include" -I"/Users/sin/Documents/Eclipse/Workspace/STM32Library/STM32F4xx_StdPeriph_Driver/inc" -I"/Users/sin/Documents/Eclipse/Workspace/STM32Library/CMSIS/STM32F4xx" -I"/Users/sin/Documents/Eclipse/Workspace/STM32Library/armcore" -I"/Users/sin/Documents/Eclipse/Workspace/stm32f4-stdperiph" -O2 -mcpu=cortex-m4 -mthumb -mlittle-endian -mfloat-abi=soft -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	arm-none-eabi-gcc -DUSE_STDPERIPH_DRIVER=1 -I"C:\Users\Sin\Documents\Workspace\stm32f4-stdperiph" -O2 -mcpu=cortex-m4 -mthumb -mlittle-endian -mfloat-abi=soft -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 %.o: ../%.S
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross GCC Assembler'
-	arm-none-eabi-as -I"/Users/sin/Documents/Eclipse/Workspace/STM32Library/CMSIS/Include" -I"/Users/sin/Documents/Eclipse/Workspace/STM32Library/STM32F4xx_StdPeriph_Driver/inc" -I"/Users/sin/Documents/Eclipse/Workspace/STM32Library/CMSIS/STM32F4xx" -I"/Users/sin/Documents/Eclipse/Workspace/STM32Library/armcore" -I"/Users/sin/Documents/Eclipse/Workspace/stm32f4-stdperiph" -o "$@" "$<"
+	arm-none-eabi-as -I"C:\Users\Sin\Documents\Workspace\stm32f4-stdperiph" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
