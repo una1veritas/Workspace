bf995003e109fc9cfda40375eab1162dd2da363c
Sin Shimozono <una.veritas@me.com>
2014-06-13 11:32:23 +0900

    of you

--- TestCode/src/TestCode.c
+++ TestCode/src/TestCode.c
@@ -28,89 +28,81 @@ enum {
 };
 
 boolean next(char * a, int n);
-void asort(char array[], int s, int n ) {
-	char t;
-	int i, j;
-	for(i = s; i < n - 1; i++)
-		for(j = i + 1; j < n; j++)
-			if ( array[i] > array[j] ) {
-				t = array[i];
-				array[i] = array[j];
-				array[j] = t;
-			}
-}
-void dsort(char array[], int n ) {
-	char t;
-	int i, j;
-	for(i = 0; i < n - 1; i++)
-		for(j = i + 1; j < n; j++)
-			if ( array[i] < array[j] ) {
-				t = array[i];
-				array[i] = array[j];
-				array[j] = t;
-			}
-}
-void translate(char * str, char map[], char alphabet[], int asize) {
-	int i, target;
-	for(i = 0; i < strlen(str); i++) {
-		for(target = 0; target < asize; target++)
-			if ( (char)(str[i]) == alphabet[target] )
-				break;
-		str[i] = (char) map[(int)target];
-	}
-}
+void asort(char array[], int s, int n );
+void dsort(char array[], int n );
+void translate(char * str, char map[], char alphabet[], int asize);
 
 int main(int argc, char * argv[]) {
 	char * ptr;
 	int count[128];
+	char map[128], talphabet[128] = "abcdefghijklmnopqrstuvwxyz";
+	int iasize, tasize = strlen(talphabet);
 
-	printf("Hi.\n\r");
+	printf("Hi.\n");
 
-	if ( argc == 1 )
+	if ( argc == 1 ) {
+		printf("Specify input(s) please.\n\n");
 		return 1;
+	}
 	ptr = argv[1];
-	printf("Input: %s\n\r", ptr);
+	printf("Input string: \"%s\" of length %ld.\n", ptr, strlen(ptr));
+	if ( argc == 3 ) {
+		strcpy(talphabet, argv[2]);
+		tasize = strlen(talphabet);
+		strcpy(map, talphabet);
+	}
 
-	int i;
+	int i, j;
 	for(i = 0; i < 128; i++)
 		count[i] = 0;
 	for(i = 0; i < strlen(ptr); i++) {
 		count[(int)ptr[i]]++;
 	}
-	printf("\n\rFrequency:\n\r");
+	printf("\nFrequency:\n");
 	for(i = 0; i < 128; i++) {
 		if ( count[i] != 0 ) {
-			printf("%c: %d\n\r", (char)i, count[i]);
+			printf("%c: %d\n", (char)i, count[i]);
+
+			for(j = 0; map[j] != 0; j++) {
+				if ( map[j] == (char)i )
+					break;
+			}
+			if ( map[j] == 0 ) {
+				map[j] = (char)i;
+				map[j+1] = 0;
+			}
 		}
 	}
-	printf("\n\r");
+	iasize = strlen(map);
+	printf("\n");
 	fflush(stdout);
 
-	char map[128], alphabet[128] = "abcdefghijklmnopqrstuvwxyz.?";
-	int asize = strlen(alphabet);
-
-	asort(alphabet, 0, asize);
-	memcpy(map, alphabet, asize+1);
-	printf("alphabet: %s\n\r", alphabet);
+	asort(map, 0, tasize);
+	printf("target alphabet: %s\n", talphabet);
+	printf("input alphabet: %s\n", map);
 
 	char transed[256];
 	long counter;
 	for (counter = 0; ; counter++) {
 		printf("%012ld: ", counter);
-		for(i = 0; i < asize; i++) {
-			printf("%c", map[i]);
+		for(i = 0; i < iasize; i++) {
+			if ( isprint(map[i]) ) {
+				printf("%c", map[i]);
+			} else {
+				printf(" ");
+			}
 		}
 		strcpy(transed, ptr);
-		translate(transed, map, alphabet, asize);
-		printf(": %s\n\r", transed);
+		translate(transed, map, talphabet, iasize);
+		printf(": %s\n", transed);
 		fflush(stdout);
 
-		if ( !next(map, asize) )
+		if ( !next(map, tasize) )
 			break;
 	}
 
-	printf("\n\r");
-	printf("Bye.\n\r\n\r");
+	printf("\n");
+	printf("Bye.\n\n");
 	return 0;
 }
 
@@ -129,4 +121,39 @@ boolean next(char * a, int n) {
 		}
 	}
 	return false;
+}
+
+void asort(char array[], int s, int n ) {
+	char t;
+	int i, j;
+	for(i = s; i < n - 1; i++)
+		for(j = i + 1; j < n; j++)
+			if ( array[i] > array[j] ) {
+				t = array[i];
+				array[i] = array[j];
+				array[j] = t;
+			}
+}
+
+void dsort(char array[], int n ) {
+	char t;
+	int i, j;
+	for(i = 0; i < n - 1; i++)
+		for(j = i + 1; j < n; j++)
+			if ( array[i] < array[j] ) {
+				t = array[i];
+				array[i] = array[j];
+				array[j] = t;
+			}
+}
+
+void translate(char * str, char map[], char alphabet[], int asize) {
+	int i, target;
+	for(i = 0; i < strlen(str); i++) {
+		for(target = 0; target < asize; target++)
+			if ( (char)(str[i]) == alphabet[target] )
+				break;
+		if ( target < asize )
+			str[i] = (char) map[(int)target];
+	}
 }
