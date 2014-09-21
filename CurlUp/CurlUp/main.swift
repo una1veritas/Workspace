c88929cbbaa505fa3ed5fc0b8b80feb3f5b1fb17
Sin Shimozono <sin@Fomalhaut.local>
2014-09-21 22:48:22 +0900

    CurlUp

--- CurlUp/CurlUp/main.swift
+++ CurlUp/CurlUp/main.swift
@@ -36,19 +36,36 @@ class KMPMachine {
     init(pattern : String) {
         var i, j: Int
         mypattern = pattern
+        var periodic : [ Int: Character ] = [ Int: Character]()
         border = [Int](count: countElements(mypattern), repeatedValue: -1)
         for i = 1, j = 0, border[0] = 0; i < countElements(mypattern) ;  {
-            if mypattern[i] == mypattern[j] {
+            println("(i, j) = (\(i), \(j))")
+            
+            var match = (mypattern[i] == mypattern[j])
+            println("pattern[\(i)]=\(pattern[i]) <=> pattern[\(j)]=\(pattern[j])")
+            if pattern[i] == "*" {
+                periodic[i] = mypattern[j]
+                println(periodic)
+                match = true
+            } else if pattern[j] == "*" {
+                if let tpm = periodic[j] {
+                    if periodic[j] == pattern[i] {
+                        match = true
+                    }
+                }
+            }
+            if match {
                 j++;
                 border[i] = j;
                 i++;
-            } else if j == 0 {
+            } else if j > 0 {
+                j = border[j-1];
+            } else /* j == 0 */ {
                 border[i] = 0
                 i++;
-            } else /* j > 0 */ {
-                j = border[j-1];
             }
         }
+        println("\n\(periodic).")
     }
 
     var pattern : String {
@@ -58,7 +75,7 @@ class KMPMachine {
 
 println("Hello, World!")
 
-var kmp : KMPMachine = KMPMachine(pattern: "abbabaabbabaababbabaab")
+var kmp : KMPMachine = KMPMachine(pattern: "11*111**1*11") //"110*100**011001*11001***10")
 
 for var i : Int = 0; i < countElements(kmp.pattern) ; i++ {
     print(kmp.pattern[i])
