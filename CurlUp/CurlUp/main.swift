5167382104f741f8216d47840f434d83005bf31d
Sin Shimozono <sin@Fomalhaut.local>
2014-10-31 01:33:46 +0900

    Curl up

--- CurlUp/CurlUp/main.swift
+++ CurlUp/CurlUp/main.swift
@@ -52,12 +52,15 @@ func == (lhs: IntPair, rhs: IntPair) -> Bool {
 }
 
 
+func length1(s : [Character]) -> Int {
+    return countElements(s)-1
+}
+
 /* program main part. */
     println("Hello, World!")
 
-let base0 : Int = -1
     var ell = [Character]("_01110111011")
-    println("ell = \(ell).")
+    println("ell = \(ell), length \(countElements(ell))")
     println()
     
     /* algorithm naive */
@@ -68,25 +71,41 @@ let base0 : Int = -1
     for pp in 1...n /* test period pp */ {
         var t = [Character](count: pp+1, repeatedValue: "_") /* we ignore the 0th element */
         t[1...pp] = ell[n-pp+1...n]
-        println("status: t = \(t), pp = \(pp),")
-        var k : Int
-        for k = pp+1 ; k < n; ++k {
-            println("k = \(k).")
-            //print("* = ell[\(n-k+1)]? "); println("*" == ell[n-k+1] )
-            if "*" != ell[n-k+1] {
-                println("t[pp-(k % pp)] = t[\(pp-(k % pp))] = \(t[pp-(k % pp)]).")
-                if t[pp-(k % pp)] == "*" {
-                    t[pp - (k % pp)] = ell[n - k]
-                } else if t[pp-(k % pp)] != ell[n-k] {
+        println("period table: t[] = \(t[1...length1(t)]), pp = \(pp),")
+        var k : Int = pp
+        for var kp = pp + 1; kp <= n; ++kp {
+            print("k' = \(kp): ")
+            //print("* = ell[\(n-kp+1)]? "); println("*" == ell[n-k'+1] )
+            if "*" != ell[n-kp+1] {
+                println("t[pp-((k'+pp-1) % pp)] = t[\(pp-((kp+pp-1) % pp))] = \(t[pp-((kp+pp-1) % pp)]).")
+                if t[pp-((kp+pp-1) % pp)] == "*" {
+                    t[pp - ((kp+pp-1) % pp)] = ell[n - kp+1]
+                } else if t[pp-((kp+pp-1) % pp)] != ell[n-kp+1] {
+                    println("compare failed.")
                     break
                 }
-                println("passed.")
+                println("compare passed.")
+            } else {
+                println("compare don't cared.")
             }
+            k = kp
         }
-        if n - k - 1 + pp < d + p - 1 {
-            (d, p) = (n-k, pp)
+        if n - k + pp < d + p - 1{
+            (d, p) = (n-k+1, pp)
             print("(d, p) = (\(d), \(p)), t = \(t[1...pp]); ")
-            println("\(ell[1...d]) -> \(ell[d+1 ... d+p])")
+            if d-1 > 0 {
+                print("\(ell[1...d-1]) -> ")
+            }
+            print("([\(ell[d])] -> ")
+            if p > 1 {
+                print("\(ell[d+1...d+p]))");
+            } else {
+                print(")")
+            }
+            println()
+            println("temporary the minimum.")
+        } else {
+            println("skipped since not the minimum.")
         }
         println()
     }
