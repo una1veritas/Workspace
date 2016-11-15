759eaa2139d57fcf3f73c65c6c1038e1cb1a2e88
Sin Shimozono <una.veritas@me.com>
2016-11-15 10:15:18 +0900

    CUFFTMatching

--- CUDA8.0/CUFFTMatching/main.cu
+++ CUDA8.0/CUFFTMatching/main.cu
@@ -28,12 +28,13 @@ Description : Factored discrete Fourier transform, or FFT, and its inverse iFFT
 
 #define VECTOR_MAXSIZE 512
 
+#define DEBUG_VECTOR
+
 struct compvect {
 	cufftComplex * elem;
 	unsigned int dim;
 };
 typedef struct compvect compvect;
-const int CONJ_REVERSE = 1;
 
 struct text {
 	char * str;
@@ -56,7 +57,7 @@ long smallestpow2(const long n) {
 #define max(x,y)  ( ((x) < (y) ? (y) : (x)) )
 #define abs(x)  ( (x) < 0 ? (-(x)) : (x) )
 
-int make_signal(const text * text1, const int dimsize, compvect * vec, const int flag);
+__global__ void make_signal(const char * text1, const int strlen, const int dimsize, cuComplex * vec, const int flag);
 void print_vector(const char *title, compvect *x);
 
 __host__ __device__ static __inline__ cuComplex cuCexpf(cuComplex x)
@@ -87,11 +88,11 @@ int main(int argc, char * argv[]) {
 	vecsize = smallestpow2(min(max(text1.length, text2.length), VECTOR_MAXSIZE));
 	pattlen = min(text1.length, text2.length);
 
-	make_signal(&text1, vecsize, &vec1, !CONJ_REVERSE);
-	make_signal(&text2, vecsize, &vec2, CONJ_REVERSE);
-	/* FFT, iFFT of v[]: */
-	print_vector("text1 ", &vec1);
-	print_vector("text2 ", &vec2);
+
+	vec1.dim = vecsize;
+	vec1.elem = (cuComplex *)malloc(sizeof(cuComplex)*vecsize);
+	vec2.dim = vecsize;
+	vec2.elem = (cuComplex *)malloc(sizeof(cuComplex)*vecsize);
 
 	/*タイマーを作成して計測開始*/
 	StopWatchInterface *timer = NULL;
@@ -99,17 +100,35 @@ int main(int argc, char * argv[]) {
 	sdkResetTimer(&timer);
 	sdkStartTimer(&timer);
 
-	/* GPU用メモリ割り当て */
-	cufftComplex *devmemptr;
-//	int * devresptr;
-	/* バッチ数 2 (vec1.elem, vec2.elem)*/
-	cudaMalloc((void**)&devmemptr, sizeof(cufftComplex) * vecsize * 2 );
-//	cudaMalloc((void**)&devresptr, sizeof(int) * vecsize);
 
-	/* GPU用メモリに転送 */
+	/* GPU用定義 */
+	dim3 grid(16, 1);
+	dim3 block(vecsize / 16, 1);
+	cufftComplex *devmemptr;
+	char * devstrptr;
+
+	cudaMalloc((void**)&devmemptr, sizeof(cufftComplex) * vecsize * 2);  /* バッチ数 2 (vec1.elem, vec2.elem)*/
+	cudaMalloc((void**)&devstrptr, sizeof(char) * vecsize);
+
+	/* 複素数ベクトル作成 */
+	cudaMemcpy(devstrptr, text1.str, sizeof(char)*vecsize, cudaMemcpyHostToDevice);
+	make_signal<<<grid,block>>>(devstrptr, text1.length, vecsize, devmemptr, 0);
+	cudaMemcpy(devstrptr, text2.str, sizeof(char)*vecsize, cudaMemcpyHostToDevice);
+	make_signal<<<grid,block>>>(devstrptr, text1.length, vecsize, devmemptr+vecsize, 1);
+	/* FFT, iFFT of v[]: */
+
+#ifdef DEBUG_VECTOR
+	cudaMemcpy(vec1.elem, devmemptr, sizeof(cuComplex)*vecsize, cudaMemcpyDeviceToHost);
+	cudaMemcpy(vec2.elem, devmemptr + vecsize, sizeof(cuComplex)*vecsize, cudaMemcpyDeviceToHost);
+	print_vector("text1 ", &vec1);
+	print_vector("text2 ", &vec2);
+#endif
+
+	/* GPU用メモリに転送 */ 
+	/*
 	cudaMemcpy(devmemptr, vec1.elem, sizeof(cufftComplex)* vecsize, cudaMemcpyHostToDevice);
 	cudaMemcpy(devmemptr+vecsize, vec2.elem, sizeof(cufftComplex)* vecsize, cudaMemcpyHostToDevice);
-
+	*/
 	/* 1D FFT plan作成 */
 	cufftHandle plan2way, plan1inv;
 	cufftPlan1d(&plan2way, vecsize, CUFFT_C2C, 2);
@@ -117,51 +136,34 @@ int main(int argc, char * argv[]) {
 
 	cufftExecC2C(plan2way, devmemptr, devmemptr, CUFFT_FORWARD);
 
-#ifdef DEBUG
+#ifdef DEBUG_VECTOR
 	/* 計算結果をGPUメモリから転送して表示 */
-	cudaMemcpy(vec1.elem, devmemptr, sizeof(cufftComplex)*vec1.dimsize, cudaMemcpyDeviceToHost);
-	cudaMemcpy(vec2.elem, devmemptr+vecsize, sizeof(cufftComplex)*vec2.dimsize, cudaMemcpyDeviceToHost);
-	print_vector("fft1: 2 ", vec1.elem, vec1.dimsize);
-	print_vector("fft2 ", vec2.elem, vec2.dimsize);
+	cudaMemcpy(vec1.elem, devmemptr, sizeof(cufftComplex)*vec1.dim, cudaMemcpyDeviceToHost);
+	cudaMemcpy(vec2.elem, devmemptr+vecsize, sizeof(cufftComplex)*vec2.dim, cudaMemcpyDeviceToHost);
+	print_vector("fft1 ", &vec1);
+	print_vector("fft2 ", &vec2);
 #endif
 
 	/* ベクトルの積をとる */
-	dim3 grid(16, 1);
-	dim3 block(VECTOR_MAXSIZE/16, 1);
 	cuCvecmul_into <<< grid, block >>> (devmemptr, devmemptr+vecsize, vecsize);
 
-#ifdef DEBUG
+#ifdef DEBUG_VECTOR
 	/* 計算結果をGPUメモリから転送して表示 */
 	cudaMemcpy(vec1.elem, devmemptr, sizeof(cufftComplex)* vecsize, cudaMemcpyDeviceToHost);
 	cudaMemcpy(vec2.elem, devmemptr + vecsize, sizeof(cufftComplex)*vecsize, cudaMemcpyDeviceToHost);
-	print_vector("prod ", vec1.elem, vec1.dimsize);
+	print_vector("prod ", &vec1);
 #endif
 
 	cufftExecC2C(plan1inv, devmemptr, devmemptr, CUFFT_INVERSE);
 
-
+#ifdef DEBUG_VECTOR
 	cudaMemcpy(vec1.elem, devmemptr, sizeof(cufftComplex)* vecsize, cudaMemcpyDeviceToHost);
 	print_vector("iFFT ", &vec1);
-
+#endif
 	cuCfindpos<<<grid, block>>>(devmemptr, (int *) (devmemptr+vecsize), vecsize, pattlen);
 
-	/*
-	int pos = vec1.dimsize;
-	for (int i = pattlen - 1; i < vec1.dimsize + pattlen - 1; i++) {
-		if (abs(cuCrealf(vec1.elem[(vec1.dimsize + i - 1) % vec1.dimsize])/vecsize - (double)pattlen) < (1.0F/4096.0F))
-			pos = min(pos, i);
-	}
-	*/
-	/*
-	printf("Occurring positions: ");
-	for(int i = 0; i < vec1.dimsize; i++) {
-	if ( abs(creal(vec1.elem[(vec1.dimsize - pattlen + i) % vec1.dimsize]) - (double) pattlen) < 0.0001220703125 )
-	printf("%d, ", i - pattlen + 1);
-	}
-	printf(".\n");
-	*/
-	int * pos = (int *)malloc(sizeof(int)*vecsize);
-	cudaMemcpy(pos, devmemptr+vecsize, sizeof(int) * vecsize, cudaMemcpyDeviceToHost);
+	int pos[VECTOR_MAXSIZE];
+	cudaMemcpy(&pos, devmemptr+vecsize, sizeof(int) * vecsize , cudaMemcpyDeviceToHost);
 
 	/*タイマーを停止しかかった時間を表示*/
 	sdkStopTimer(&timer);
@@ -169,10 +171,12 @@ int main(int argc, char * argv[]) {
 	sdkDeleteTimer(&timer);
 
 	printf("\nResult: \n");
+#ifdef DEBUG_VECTOR
 	for (int i = 0; i < vecsize; i++) {
 		printf("[%d] = %d, ", i, pos[i]);
 	}
 	printf("\n");
+#endif
 	if (pos[0] < vecsize)
 		printf("First occurrence is at %d.\n", pos[0] - pattlen + 1);
 	else
@@ -180,15 +184,15 @@ int main(int argc, char * argv[]) {
 
 	/* GPU用メモリ開放 */
 	cudaFree(devmemptr);
-//	cudaFree(devresptr);
+	cudaFree(devstrptr);
 
 	/* CUFFT plan削除 */
 	cufftDestroy(plan2way);
 	cufftDestroy(plan2way);
 
-	free(pos);
 	free(vec1.elem);
 	free(vec2.elem);
+	
 	free(text1.str);
 	free(text2.str);
 
@@ -196,29 +200,27 @@ int main(int argc, char * argv[]) {
 }
 
 
-int make_signal(const text * str, const int dim, compvect * vec, const int flag) {
+__global__ void make_signal(const char * str, int strlen, const int dim, cuComplex * vec, const int flag) {
 	int dst;
 	float factor;
+	int idx = blockDim.x*blockIdx.x+threadIdx.x;
 
-	// the first as normal
-	vec->elem = (cuComplex*)malloc(sizeof(cuComplex)*dim);
-	vec->dim = dim;
-	for (int i = 0; i < vec->dim; ++i) {
+	if (idx < dim) {
 		if (!flag) {
-			dst = i;
+			dst = idx;
 			factor = 2 * 3.14159265358979323846264338327950288F;
 		}
 		else {
-			dst = vec->dim - i - 1;
+			dst = dim - idx - 1;
 			factor = -2 * 3.14159265358979323846264338327950288F;
 		}
-		if ( i < str->length )
-			vec->elem[dst] = cuCexpf(make_cuComplex(0, factor * (float)(str->str[i]) / 256.0f));  // by rotated unit vector
+		if ( idx < strlen )
+			vec[dst] = cuCexpf(make_cuComplex(0, factor * (float)(str[idx]) / 256.0f));  // by rotated unit vector
 																								  // (*array)[i] = (float)(str[i]) / 128.0f  ;  // by char value
 		else
-			vec->elem[dst] = make_cuComplex(0, 0);
+			vec[dst] = make_cuComplex(0, 0);
 	}
-	return 1;
+	__syncthreads();
 }
 
 /* Print a vector of complexes as ordered pairs. */
