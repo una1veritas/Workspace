e9068ca5cc8e214a6db50822e54cffc1054589a1
Sin Shimozono <una.veritas@me.com>
2016-12-09 10:46:59 +0900

    parallel bubble sort

--- CUDA8.0/bubbleSort/bubble.cu
+++ CUDA8.0/bubbleSort/bubble.cu
@@ -16,120 +16,149 @@
 
 #include <helper_timer.h>
 
-#define ARRAY_SIZE 512
+#define ARRAY_ELEMENTS_MAX 0x7fffffff
 //262144
-#define BLOCK_SIZE 1024
+#define THREADS_IN_BLOCK_MAX 128
 
-__global__ void bubSortEven(unsigned int *A, unsigned int *B);
-__global__ void bubSortOdd(unsigned int *A, unsigned int *B);
+#define min(x,y)  ((y) > (x) ? (x) : (y))
 
-int main(void) {
+__global__ void exchEven(unsigned int *A, const unsigned int n);
+__global__ void exchOdd(unsigned int *A, const unsigned int n);
 
-	if (((ARRAY_SIZE - 1) & ARRAY_SIZE) != 0) {
-		printf("ARRAY_SIZE --- exponent of 2\n");
-		return 0;
+int main(const int argc, const char * argv[]) {
+	int elemCount = 0;
+
+	printf("sizeof(unsigned int) = %d, sizeof(long) = %d.\n",sizeof(unsigned int), sizeof(long));
+
+	if (argc != 2)
+		return EXIT_FAILURE;
+
+	elemCount = atoi(argv[1]);
+
+	if (!(elemCount > 0 && elemCount <= ARRAY_ELEMENTS_MAX)) {
+		printf("Supplied number of elements is out of bound %d.\n", ARRAY_ELEMENTS_MAX);
+		return EXIT_FAILURE;
 	}
+	printf("%u elements.\n\n");
 
 	// device(0) : GTX 1080
 	// device(1) : GTX 750Ti
 	cudaSetDevice(0);
-
-	unsigned int  arraySize = sizeof(unsigned int) * ARRAY_SIZE;
 	
-	unsigned int *hA, *hB;
-	hA = (unsigned int*)malloc(arraySize);
-	hB = (unsigned int*)malloc(arraySize);
+	unsigned int *A;
+	A = (unsigned int*) malloc(sizeof(unsigned int) * elemCount );
 
-	unsigned int i;
-	
+	// setup dummy input 
 	srand(time(NULL));
-
-	for (i = 0; i < ARRAY_SIZE; i++) {
-		hA[i] = rand() % 1024;
-
+	for (unsigned int i = 0; i < elemCount; i++) {
+		A[i] = rand() % 10000;
 	}
 
-	for (i = 0; i < 24 /*100*/; i++)
-		printf("%d, ", hA[i]);
+	for (unsigned int i = 0; i < elemCount; i++) {
+		if (i < 100 || i == elemCount - 1) {
+			printf("%4u ", i);
+		}
+		else if (i == elemCount - 2) {
+			printf("... ");
+		}
+	}
 	printf("\n");
-			
-	unsigned int *dA, *dB;
-	cudaMalloc((void**)&dA, arraySize);
-	cudaMalloc((void**)&dB, arraySize);
+	for (unsigned int i = 0; i < elemCount; i++) {
+		if (i < 100 || i == elemCount - 1) {
+			printf("%4u ", A[i]);
+		}
+		else if (i == elemCount - 2) {
+			printf("... ");
+		}
+	}
+	printf("\n\n");
+	
+	// setup input copy on device mem
+	unsigned int *devA;
+	const unsigned devACapa = ((elemCount + 31) / 32) * 32;
+	cudaMalloc((void**)&devA, sizeof(unsigned int) * devACapa);
+	cudaMemcpy(devA, A, sizeof(unsigned int) * devACapa, cudaMemcpyHostToDevice);
 
-	cudaMemcpy(dA, hA, arraySize, cudaMemcpyHostToDevice);
-
-	dim3 grid = ARRAY_SIZE / (2 * BLOCK_SIZE);
-	dim3 block = BLOCK_SIZE;
+	const unsigned int blockSize = min(THREADS_IN_BLOCK_MAX, devACapa/2);
+	const unsigned int gridSize = (devACapa/2+(blockSize -1))/blockSize;
+	printf("Going to use %d blocks, %d threas per block with capacity %d.\n\n", gridSize, blockSize, devACapa);
+	dim3 grids(gridSize), blocks(blockSize);
 
 	StopWatchInterface *timer = NULL;
 	sdkCreateTimer(&timer);
 	sdkResetTimer(&timer);
 	sdkStartTimer(&timer);
 
-	//for (i = 0; i < (ARRAY_SIZE / 2); i++) {
-		bubSortEven << < grid, block >> > (dA, dB);
-		cudaThreadSynchronize();
-		bubSortOdd << < grid, block >> > (dB, dA);
-		cudaThreadSynchronize();
-	//}
+	for (unsigned int i = 0; i < (elemCount>>1); i++) {
+		exchEven << < grids, blocks >> > (devA, elemCount);
+		exchOdd << < grids, blocks >> > (devA, elemCount); // possibly the last call is redundant
+	}
 	
-	cudaMemcpy(hA, dA, arraySize, cudaMemcpyDeviceToHost);
-	cudaMemcpy(hB, dB, arraySize, cudaMemcpyDeviceToHost);
+	cudaMemcpy(A, devA, sizeof(unsigned int) * devACapa, cudaMemcpyDeviceToHost);
 
 	sdkStopTimer(&timer);
 
-	for (i = 0; i < 24 /*ARRAY_SIZE*/; i++) {
-		//printf("[%d] = %u\n", i, hA[i]);
-		printf("%d, ", hB[i]);
+	int flag = 0;
+	unsigned int firstFailure = elemCount;
+	for (unsigned int i = 0; i < elemCount; i++) {
+		if (i < elemCount - 1) {
+			if (A[i] > A[i + 1]) {
+				flag = 1;
+				firstFailure = i;
+			}
+		}
+		if (i < 100 || i == elemCount - 1) {
+			printf("%4u ", A[i]);
+		}
+		else if (i == elemCount - 2) {
+			printf("... ");
+		}
+
 	}
 	printf("\n");
-	printf("[%d] = %u\n", ARRAY_SIZE - 1, hA[ARRAY_SIZE - 1]);
+	if (flag != 0) {
+		printf("!!!Sort failure deteced at A[%d] = %d and A[%d] = %d!!!\n", 
+			firstFailure, A[firstFailure], firstFailure+1, A[firstFailure+1]);
+	}
+	printf("[%u] = %u\n", elemCount - 1, A[elemCount - 1]);
 
-	printf("[TIME] =%f(ms)\n", sdkGetTimerValue(&timer));
+	printf("Elapsed time %f msec.\n", (float)((int)(sdkGetTimerValue(&timer)*1000))/1000 );
 	sdkDeleteTimer(&timer);
 
-	free(hA);
-	free(hB);
-	cudaFree(dA);
-	cudaFree(dB);
+	cudaFree(devA);
+	free(A);
 
-	cudaThreadExit();
+	cudaDeviceReset();
 
 }
 
-__global__ void bubSortEven(unsigned int *bef, unsigned int *aft) {
 
-	unsigned int fro_idx = (blockIdx.x * blockDim.x + threadIdx.x) * 2;
-	unsigned int beh_idx = (blockIdx.x * blockDim.x + threadIdx.x) * 2 + 1;
+__global__ void exchEven(unsigned int *a, const unsigned int n) {
+	unsigned int thix = blockIdx.x * blockDim.x + threadIdx.x;
+	unsigned int left = thix << 1;
+	unsigned int tmp;
 
-	if (bef[fro_idx] > bef[beh_idx]) {
-		aft[fro_idx] = bef[beh_idx];
-		aft[beh_idx] = bef[fro_idx];
-	}
-	else {
-		aft[fro_idx] = bef[fro_idx];
-		aft[beh_idx] = bef[beh_idx];
+	if ( left + 1 < n ) {
+		if (a[left] > a[left+1]) {
+			tmp = a[left];
+			a[left] = a[left+1];
+			a[left+1] = tmp;
+		}
 	}
 	__syncthreads();
 }
 
-__global__ void bubSortOdd(unsigned int *bef, unsigned int *aft) {
+__global__ void exchOdd(unsigned int *a, const unsigned int n) {
+	unsigned int thix = blockIdx.x * blockDim.x + threadIdx.x;
+	unsigned int left = (thix << 1) + 1;
+	unsigned int tmp;
 
-	unsigned int fro_idx = (blockIdx.x * blockDim.x + threadIdx.x) * 2 + 1;
-	unsigned int beh_idx = (blockIdx.x * blockDim.x + threadIdx.x) * 2 + 2;
-
-	if (bef[fro_idx] > bef[beh_idx] && beh_idx != ARRAY_SIZE) {
-		aft[fro_idx] = bef[beh_idx];
-		aft[beh_idx] = bef[fro_idx];
+	if (left + 1 < n) {
+		if (a[left] > a[left+1]) {
+			tmp = a[left];
+			a[left] = a[left+1];
+			a[left+1] = tmp;
+		}
 	}
-	else if (beh_idx != ARRAY_SIZE){
-		aft[fro_idx] = bef[fro_idx];
-		aft[beh_idx] = bef[beh_idx];
-	}
-
-	aft[0] = bef[0];
-	aft[ARRAY_SIZE - 1] = bef[ARRAY_SIZE - 1];
-
 	__syncthreads();
 }
