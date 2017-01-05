413b5fa0eac361175d3bd703594283f43b17c98d
Sin Shimozono <una.veritas@me.com>
2017-01-05 09:25:20 +0900

    .metadata remove

--- Permutator/Debug/src/subdir.mk
+++ Permutator/Debug/src/subdir.mk
@@ -4,30 +4,30 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../src/Permutation.cpp \
+../src/permutation.cpp \
 ../src/permutator.cpp 
 
 OBJS += \
-./src/Permutation.o \
+./src/permutation.o \
 ./src/permutator.o 
 
 CPP_DEPS += \
-./src/Permutation.d \
+./src/permutation.d \
 ./src/permutator.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
-	@echo 'Invoking: Cygwin C++ Compiler'
-	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -std=c++0x -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 src/permutator.o: ../src/permutator.cpp
 	@echo 'Building file: $<'
-	@echo 'Invoking: Cygwin C++ Compiler'
-	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"src/permutator.d" -o "$@" "$<"
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -std=c++0x -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"src/permutator.d" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
