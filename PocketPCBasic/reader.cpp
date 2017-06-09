17cfd0531b3adeb191261a3ccc17f037541ba05d
Sin Shimozono <una.veritas@icloud.com>
2017-06-09 17:47:08 +0900

    BASIC -> PocketPCBasic

---
+++ PocketPCBasic/reader.cpp
@@ -0,0 +1,17 @@
+/*
+ * reader.cpp
+ *
+ *  Created on: 2017/06/09
+ *      Author: sin
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(const int argc, const char * argv[]) {
+
+	cout << "Hello." << endl;
+
+	return 0;
+}
