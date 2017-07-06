c207bc43e604716e6c55a0290e1ca1d2b5cd3ad0
Sin Shimozono <una.veritas@me.com>
2017-07-06 21:27:04 +0900

    PocketPC

--- PocketPCBasic/reader.cpp
+++ PocketPCBasic/reader.cpp
@@ -29,22 +29,22 @@ struct FileInfo {
 		PC_E500_SAVE_ASC = 0x02,
 		PC_E500_CSAVE_BAS = 0x04,
 
-		PC_E500_RAMDISK = 0xff,
+		PC_E500_DISKFILE = 0xff,
 
 		FILEINFOTYPE_UNKNOWN = 0,
 	};
 
 	enum SYSTEMTYPE {
-		PC_E500_CASSETTE,
-		PC_E500_RAMDISK,
+		PC_E500_CMT,
+		PC_E500_DISK = PC_E500_CMT,
 	};
 
-	FileInfo(const std::vector<uint8> & buf, const uint8 stype = PC_E500_CASSETTE);
+	FileInfo(const std::vector<uint8> & buf, const std::string & name = "", const uint8 stype = PC_E500_CMT);
 
 	ostream & printOn(ostream & outs) const;
 };
 
-FileInfo::FileInfo(const std::vector<uint8> & buff, const uint8 stype) {
+FileInfo::FileInfo(const std::vector<uint8> & buff, const std::string & name, const uint8 stype) {
 	type = FILEINFOTYPE_UNKNOWN;
 	save_name[0] = 0;
 	byte_size = buff.size();
@@ -52,7 +52,7 @@ FileInfo::FileInfo(const std::vector<uint8> & buff, const uint8 stype) {
 	call_address = 0;
 	checksum = 0;
 
-	if ( stype == PC_E500_CASSETTE ) {
+	if ( stype == PC_E500_CMT ) {
 		type = buff[0];
 		switch(type) {
 		case PC_E500_CSAVE_M:
@@ -62,9 +62,21 @@ FileInfo::FileInfo(const std::vector<uint8> & buff, const uint8 stype) {
 			}
 			save_name[0x10] = 0;
 			byte_size = ((uint32)buff[0x1d])<<16 | ((uint32)buff[0x13])<<8 | buff[0x12];
-			load_address = ((uint32)buff[0x1e])<<16 | ((uint32)buff[0x15]) | buff[0x14];
-			call_address = ((uint32)buff[0x1f])<<16 | ((uint32)buff[0x17]) | buff[0x16];
+			load_address = ((uint32)buff[0x1e])<<16 | ((uint32)buff[0x15])<<8 | buff[0x14];
+			call_address = ((uint32)buff[0x1f])<<16 | ((uint32)buff[0x17])<<8 | buff[0x16];
 			checksum = ((uint16)buff[0x30]<<8) | buff[0x31];
+			break;
+		case PC_E500_DISKFILE:
+			if ( name.length() > 0 ) {
+				for(int i = 0; i < 16; i++) {
+					save_name[i] = toupper(name[i]);
+				}
+				save_name[16] = 0;
+			}
+			byte_size = buff[0x05] | ((uint16)buff[0x06])<<8 | ((uint32)buff[0x07])<<16;
+			load_address = buff[0x08] | ((uint16)buff[0x09])<<8 | ((uint32)buff[0x0a])<<16;
+			// 0x02, 0x03, 0x04 are unknwon bytes.
+			//call_address = buff[0x08] | ((uint16)buff[0x09])<<8 | ((uint32)buff[0x0a])<<16;
 			break;
 		case PC_E500_SAVE_ASC:
 		default:
@@ -80,20 +92,20 @@ FileInfo::FileInfo(const std::vector<uint8> & buff, const uint8 stype) {
 ostream & FileInfo::printOn(ostream & outs) const {
 	outs << "type = "<< (int) type << ", ";
 	outs << "save name = \""<< save_name << "\", ";
-	outs << "size = " << std::dec << byte_size << "(0x" << std::hex << byte_size << "), ";
-	outs << "load address = " << "0x" << std::setw(8) << std::setfill('0') << load_address << ", ";
-	outs << "call address = " << "0x" << std::setw(8) << std::setfill('0') << call_address << ", ";
-	outs << "checksum = " << "0x" << std::setw(4) << std::setfill('0') << checksum << ", ";
+	outs << "size = " << std::dec << byte_size << " (0x" << std::hex << std::uppercase << byte_size << "), ";
+	outs << "load address = " << "0x" << std::setw(8) << std::setfill('0') << std::uppercase << load_address << ", ";
+	outs << "call address = " << "0x" << std::setw(8) << std::setfill('0') << std::uppercase << call_address << ", ";
+	outs << "checksum = " << "0x" << std::setw(4) << std::setfill('0') << std::uppercase << checksum << ", ";
 	return outs;
 }
 
 void dump(std::vector<uint8> & buff);
+void disk2cmt(FileInfo & finfo, std::vector<uint8> & buff);
 
 int main(const int argc, const char * argv[]) {
 	cout << "Hello." << endl;
 
 	std::ifstream infile(argv[1], std::ios::binary);
-
 	if ( ! infile )
 		return -1;
 
@@ -102,12 +114,72 @@ int main(const int argc, const char * argv[]) {
 
 	infile.close();
 
-	dump(buff);
-	FileInfo info(buff);
+	FileInfo info(buff, string(argv[1]));
 	info.printOn(std::cout);
 	std::cout << std::endl;
+	dump(buff);
+	std::cout << std::endl;
+	disk2cmt(info,buff);
+	dump(buff);
+	FileInfo info2(buff);
+	info2.printOn(std::cout);
+	std::cout << std::endl;
 
+	if ( argc >= 3 ) {
+		std::ofstream outfile(argv[2], std::ios::binary);
+		if ( outfile ) {
+			for(std::vector<uint8>::iterator it = buff.begin();
+					it != buff.end(); ++it) {
+				outfile.put(*it);
+			}
+			outfile.close();
+		} else {
+			std::cerr << "Failed to open output file " << argv[2] << std::endl;
+		}
+	}
 	return 0;
+}
+
+void disk2cmt(FileInfo & finfo, std::vector<uint8> & buff) {
+	std::vector<uint8> newbuff;
+	unsigned int ui;
+
+	newbuff.push_back(FileInfo::PC_E500_CSAVE_M);
+	for(ui=0; ui< 16 && finfo.save_name[ui] != 0; ++ui) {
+		newbuff.push_back(finfo.save_name[ui]);
+	}
+	for( ; ui< 16; ++ui) {
+		newbuff.push_back(0x20);
+	}
+	newbuff.push_back(0x0D);
+	//
+	newbuff.push_back(finfo.byte_size & 0xff);
+	newbuff.push_back(finfo.byte_size>>8 & 0xff);
+	newbuff.push_back(finfo.load_address & 0xff);
+	newbuff.push_back(finfo.load_address>>8 & 0xff);
+	newbuff.push_back(finfo.call_address & 0xff);
+	newbuff.push_back(finfo.call_address>>8 & 0xff);
+	for(ui = 0x18; ui < 0x1D; ++ui)
+		newbuff.push_back(0x00);
+	newbuff.push_back(finfo.byte_size>>16 & 0xff);
+	newbuff.push_back(finfo.load_address>>16 & 0xff);
+	newbuff.push_back(finfo.call_address>>16 & 0xff);
+	for(ui = 0x20; ui < 0x30; ++ui)
+		newbuff.push_back(0x00);
+	// calculate checksum
+	unsigned int xsum = 0;
+	for(ui = 0; ui < 0x30; ++ui) {
+		for(int bp = 0; bp < 8; bp++)
+			xsum += (newbuff[ui]>>bp & 1) == 1;
+	}
+	newbuff.push_back(xsum>>8 & 0xff);
+	newbuff.push_back(xsum & 0xff);
+	//
+	for(ui = 16; ui < buff.size(); ui++) {
+		newbuff.push_back(buff[ui]);
+	}
+	buff.clear();
+	buff.insert(buff.begin(), newbuff.begin(), newbuff.end());
 }
 
 void dump(std::vector<uint8> & buff) {
