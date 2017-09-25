ffe01a7f52af45cb9bf36e0d43c99af4ba43b927
Sin Shimozono <una.veritas@me.com>
2017-09-26 01:47:00 +0900

    asm

--- z80/z80pack-1.35/test.asm
+++ z80/z80pack-1.35/test.asm
@@ -1,22 +1,13 @@
 	ORG 0000h
 init:
-	LD A,6
-	LD (var_x),A
+	LD A,(count)
 loop:
-	LD A,(var_x)
-	INC A
-	OUT ($02),A
-	LD (var_x),A
-	IN A,($01)
-	LD (var_y),A
-	AND A
+	DEC A
+	LD (count),A
 	JP NZ,loop
 stop:
 	HALT
-var_x:
-	db 0
-var_y:
-	db 0
-
-	END
+count:	
+	db 5
 	
+	END
