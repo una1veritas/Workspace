38107a797f569ae29607e197f3a0e6c754cfc83c
Sin Shimozono <una.veritas@me.com>
2017-11-20 00:41:14 +0900

    z80 ipl

--- z80/test/ipl.asm
+++ z80/test/ipl.asm
@@ -14,24 +14,39 @@
 ;    You should have received a copy of the GNU General Public License
 ;    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-org $2000
+CSTATUS:	equ $00
+CIN:	equ $01
+COUT:	equ $02
+	
+READ_FUNC_BIT:  equ	7
+WRITE_FUNC_BIT: equ	6
+BOOT_FUNC_BIT:  equ 5
+HOME_FUNC_BIT:  equ 4
+
+	org $0000
+boot:	
+	ld sp,$0100
+	jp $0100
+
+	ds  $32,0
+	org $0038
+rst38h:	
+	ret
+
+	ds  $c7,0
+
+	org $0100
 	; IPL for the CP/M-emu in an AVR. Loads CPM from the 'disk' from
 	; track 0 sector 2 to track 1 sector 26.
+start_ipl:	
+	ld hl, IPLMESSAGE
+	call print_str
 
-READ_FUNC:  equ	7
-WRITE_FUNC: equ	6
-BOOT_FUNC:  equ 5
-HOME_FUNC:  equ 4
-
-	ld sp,$1000
-	
-	call printipl
-
-
-	ld b,51
-	ld de,$0001
+	ld b,49			; 
+	ld de,$0001		; track (d) & sector (e) counter, begin with 1
 	ld hl,$3400+$A800
-loadloop:
+	
+diskread:
 	ld a,d ;track
 	out (16),a
 	ld a,e ; sector (base-0)
@@ -40,40 +55,43 @@ loadloop:
 	out (20),a
 	ld a,h ;dma H
 	out (21),a
-	ld a,1<<READ_FUNC
+	ld a,1<<READ_FUNC_BIT
 	out (22),a
 
-	push bc
-	ld bc,$80
-	add hl,bc
-	pop bc
+	push 	bc
+	ld	bc, 128
+	add 	hl,bc 		; increase 128 
+	pop 	bc
 
-	inc e
-	ld a,e
-	cp 26
-	jp nz,noNextTrack
-
-	inc d
-	ld e,0
-
-noNextTrack:
-
+	inc 	e		; increase sector No.
+	ld 	a,e
+	cp 	26		; sectors in a track
+	jr 	nz, next_sector
+	inc 	d		; move to next track
+	ld 	e,0		; and the first sector
+next_sector
 	dec b
-	jp nz,loadloop
+	jr nz,diskread
 
+	ld hl, DONEMESSAGE
+	call print_str
+exit_ipl:	
 	jp $4A00+$A800
 
-printipl:
-	ld a,'i'
-	out (2),a
-	ld a,'p'
-	out (2),a
-	ld a,'l'
-	out (2),a
-	ld a,13
-	out (2),a
-	ld a,10
-	out (2),a
-	ret
+print_str:
+	ld a,(hl)
+	and a
+	ret z
+	out (COUT), a
+	inc hl
+	jr print_str
 
-;end
+stop:
+	halt
+
+IPLMESSAGE:	
+	dm 13,10,"IPL loading...",13,10,0
+DONEMESSAGE:
+	dm "done.",13,10,0
+	
+end.
