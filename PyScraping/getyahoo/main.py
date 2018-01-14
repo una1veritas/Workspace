d4351b982b618ccba28f4829c4f2b90b4dda23b1
Sin Shimozono <una.veritas@me.com>
2018-01-15 02:32:00 +0900

    pyscraping

--- PyScraping/getyahoo/main.py
+++ PyScraping/getyahoo/main.py
@@ -5,138 +5,112 @@ Created on 2017/12/24
 @author: sin
 '''
 import sys
+import math
 import os
-import requests
-from bs4 import BeautifulSoup
 from datetime import datetime
+from pyquery import PyQuery
+
 #import csv
-#import time
 
 if __name__ == '__main__':
     pass
 
-ranking_kind=1
-ranking_timespan='w'
-ranking_market=2
-ranking_lastpage=1
-if len(sys.argv) == 5:
-    ranking_kind = int(sys.argv[1])
-    ranking_timespan = sys.argv[2]
-    ranking_market = int(sys.argv[3])
-    ranking_lastpage = int(sys.argv[4])
+code = '5698.T'
+pdstart = int('20171210')
+pdend = int('20180112')
+tmspan = 'd'
+if len(sys.argv) >= 4:
+    code = sys.argv[1]
+    pdstart = int(sys.argv[2])
+    pdend = int(sys.argv[3])
+    if len(sys.argv) == 5:
+        tmspan = sys.argv[4]
         
-filepath = ''
-tstamp = datetime.now().strftime("%Y%m%d-%H%M")
-print ("current time stamp: ", tstamp)
+filepath = './'
+timestamp = datetime.now().strftime("%Y%m%d-%H%M")
+print ("current time stamp: ", timestamp)
 # 1カラム目に時間を挿入します
 #rowlist.append(timestamp)
 
-def yahooFinanceRanking(kind=1, timespan='d', market=1, lastpage=1):
-#    ranking_kind = { 1:u'値上がり率', 33:u'出来高増加率' }
-#    ranking_market = { 1:u'全市場', 2:u'東証全体', 3:u'東証1部', 4:u'東証2部', 5:u'東証マザーズ', 6:u'JASDAQ全体', 6:u'JASDAQスタンダート', }
-#    ranking_timespan = { 'd':'日次', 'w':u'週次', 'm':u'月次' }
+def CalDate(jd) :
+    jd = jd + 0.5
+    z = int(jd)
+    a = z
+    f = jd - int(jd)
+    if ( z >= 2299161 ) :
+        alpha = int( (z-1867216.25)/36524.25 )
+        a = a + 1 + alpha - int(alpha/4)
+    b = a + 1524
+    c = int( (b-122.1)/365.25 )
+    d = int(365.25 * c)
+    e = int( (b-d)/30.6001 )
+    date = b - d - int(30.6001 * e) + f
+    if ( e < 13.5 ) :
+        month = e - 1
+    else:
+        month = e-13
+    if ( month > 2.5) :
+        year = c - 4716
+    else:
+        year = c - 4715
+    return math.copysign(1,year)*(math.fabs(year)*10000 + month*100 + date)
 
-    ranking = {}
-    target_page = 1
-    while target_page <= lastpage:
-        url = "https://info.finance.yahoo.co.jp/ranking/?kd={0}&tm={1}&vl=a&mk={2}&p={3}".format(kind,timespan,market,target_page)
-        bsoup = BeautifulSoup(requests.get(url).content, "html.parser")
-        if target_page == 1:
-            div_ttl = bsoup.find("div", {"class" : "ttl"})
-            datestr = str(div_ttl.find("div"))#,"utf-8"
-            datestr = datestr.split(u'：')[1].split('</div>')[0]
-            datestr = datestr.replace(u'年','/')
-            datestr = datestr.replace(u'月','/')
-            datestr = datestr.replace(u'日','-')
-            datestr = datestr.replace(u'時',':')
-            datestr = datestr.replace(u'分','')
-            print ("time stamp of the ranking: ",datestr)
-            tmp_date = datestr.split('-')[0].split('/')
-            tmp_time = datestr.split('-')[1].split(':')
-            ranking[0] = ["{0:02d}{1}{2:02d}".format(kind,timespan,market),
-                          "{0}{1}{2}".format(tmp_date[0].zfill(4),tmp_date[1].zfill(2),tmp_date[2].zfill(2)),
-                          "{0}{1}".format(tmp_time[0].zfill(2),tmp_time[1].zfill(2)) ]
-            
-        div_rankingTableWrapper = bsoup.find("div", {"class": "rankingTableWrapper"})
-        rankingTable = div_rankingTableWrapper.find("tbody");
-        for tr_rankingTabledata in rankingTable.find_all("tr"):
-            tds = tr_rankingTabledata.find_all("td")
-            rank = int(tds[0].string)
-            if kind == 1 and timespan == 'd':
-                #順位    コード    市場    名称    時刻    取引値    前日比    出来高    掲示板
-                #print rank, ": ", tds[1].string,"; ", tds[2].string,"; ", tds[3].string,"; ", float(tds[4].string.lstrip('+').replace(',','')),"; ", float(tds[6].string.replace(',','')),"; ", float(tds[7].string.replace(',',''))
-                ranking[rank] = [ tds[1].string, tds[2].string, tds[3].string, 
-                                 tds[4].string, 
-                                 float(tds[5].string.replace(',','')), 
-                                 float(tds[6].find("span").string.lstrip('+')), 
-                                 float(tds[7].string.lstrip('+').replace(',','')),
-                                 float(tds[8].string.replace(',','')) ]
-            elif kind == 1 and timespan == 'w':
-                ranking[rank] = [ tds[1].string, tds[2].string, tds[3].string, 
-                                 float(tds[4].string.lstrip('+').replace(',','')), 
-                                 #float(tds[5].string.replace(',','')), 
-                                 float(tds[6].string.replace(',','')), float(tds[7].string.replace(',','')) ]
-            elif kind == 33:
-                print (rank, ": ", tds[1].string,"; ", tds[2].string,"; ", tds[3].string,"; ", tds[4],"; ", tds[5],"; ", tds[6],"; ", tds[7] )
-            else:
-                print ('ranking kind error')
-                return ranking
-        #output
-        target_page = target_page + 1
-        
-    return ranking
+def JulianDay(year, month, date):
+    if ( month <= 2 ) :
+        month = month + 12
+        year = year - 1
+    
+    a = 0
+    b = 0
+    if ( year*10000+month*100+date >= 15821015 ) :
+        a = int(year/100)
+        b = 2-a+int(a/4)
+    return int(365.25 * year) + int(30.6001 * (month+1)) + date + b + 1720994.5;
+
+def yahooFinanceTimeSeries(code,pdstart,pdend,timespan='d'):
+    pdstart = int(pdstart)
+    pdend = int(pdend)
+    url = 'https://info.finance.yahoo.co.jp/history/?code={0}&sy={1}&sm={2}&sd={3}&ey={4}&em={5}&ed={6}&tm={7}'
+    url = url.format(code,pdstart//10000,pdstart // 100 % 100, pdstart % 100,
+                     pdend//10000,pdend//100 % 100, pdend % 100,timespan)
+    rows = []
+    pyquery = PyQuery(url)
+    for table in pyquery('div.padT12')('table'):
+        pytable = pyquery(table)
+        for tr in pytable('tr'):
+            row = pytable(tr)
+            #skip header line
+            if len(row('td')) == 0: 
+                continue
+            columns = []
+            for td in row('td'):
+                td_str = row(td).text()
+                columns.append(td_str)
+            rows.append(columns)
+    return rows
 #    for key in ranking:
 #        if ranking[key][1] != u'東証ETF':
 #            print key,": ",ranking[key]
 
-ranking = yahooFinanceRanking(kind=ranking_kind,timespan=ranking_timespan,market=ranking_market,lastpage=ranking_lastpage)
-f_name = 'yfr' + ranking[0][0]+'-'+ranking[0][1]+ranking[0][2] + '.csv'
+jpstart = int(0.5+JulianDay(pdstart//10000,pdstart//100%100,pdstart%100))
+jpend = int(0.5+JulianDay(pdend//10000,pdend//100%100,pdend%100))
+table = []
+for jd in range(jpstart, jpend+1, 32):
+    if jd+31 > jpend:
+        je = jpend
+    else:
+        je = jd+31
+    pjstart = int(CalDate(jd))
+    pjend = int(CalDate(je))
+    table = table + yahooFinanceTimeSeries(code,pjstart,pjend,tmspan)
+
+table.sort(reverse=True)
+for row in table:
+    for index in range(0,len(row)):
+        print(row[index].replace(',',''), end='')
+        if index+1 < len(row): 
+            print(',', end='')
+        else:
+            print()
 #yahooFinanceRanking(ranking, timespan='w',page=2)
 
-f_encode = 'sjis'
-if not os.path.exists(filepath+f_name):
-    with open(f_name,'w') as csv_file:
-        for rank in ranking:
-            #if rank == 0: continue
-            rowlist = [ rank ] + ranking[rank]
-            itemnum = len(rowlist)
-            icount = 0
-            for item in rowlist:
-                if type(item) == int or type(item) == float:
-                    itemstr = str(item)
-                else:
-                    itemstr = item.encode(f_encode)
-    #            sys.stdout.write(itemstr)
-                csv_file.write(itemstr)
-                icount = icount + 1
-                if icount < itemnum:
-                    csv_file.write(',')
-            csv_file.write('\n')
-    csv_file.close()
-else:
-    print ('file ' + f_name + ' already exists.')
-print ('done.')
-#table_stocksTable = divtmp.find("table")
-#divtmp = bsoup.find_all("div",{"class":"lineFi clearfix"})
-#<div class="lineFi clearfix">
-#<div class="stocksDtl clearFix">
-
-#print table_stocksTable.find("th", class_ = "symbol").string
-#print table_stocksTable.find_all("td", class_ = "stoksPrice")[1].string
-#print table_stocksTable.find("td", class_ = "change").find_all("span")
-
-#for div in divtmp:
-#    print div
-#for div in tables:
-#    print div
-#    for row in table.find_all("td",{"class":"stoksPrice"}):
-#        if row.string :
-#            price = row.string
-
-#print price
-# 2カラム目に日経平均を記録します
-#rowlist.append(timestamp)
-# csvに追記敷きます
-#writer.writerow(csv_list)
-# ファイル破損防止のために閉じます
-#f.close()
