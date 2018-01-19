67ea6637b7a6f33a24ffa32cc077313707efbe94
Sin Shimozono <una.veritas@icloud.com>
2018-01-19 17:12:50 +0900

    math

--- PyScraping/addtechnical/main.py
+++ PyScraping/addtechnical/main.py
@@ -11,33 +11,37 @@ files_list = glob.glob('../yfseries/'+stockcode+'-*-*.csv')
 print(files_list)
 
 tseries = []
+header = []
 for fname in files_list:
     with open(fname, 'r') as file:
         csv_reader = csv.reader(file)
-        header = next(csv_reader)  # ヘッダーを読み飛ばしたい時
-        header = next(csv_reader)  # ヘッダーを読み飛ばしたい時
+        next(csv_reader)  # ヘッダーを読み飛ばしたい時
+        if len(header) == 0 : 
+            header = next(csv_reader)  # ヘッダーを読み飛ばしたい時
+        else:
+            next(csv_reader)
         for row in csv_reader:
             for index in range(1,len(row)):
                 row[index] = int(row[index])
             tseries.append(row)
 
-tseries[1:].sort(reverse=False)
+print (header)
+tseries.sort()
 
-averages = []
+#moving average
 avrspans = [ 5, 21, 55 ]
+for span in avrspans:
+    for d in range(span - 1, len(tseries)) :
+        psum = 0
+        vsum = 0
+        for i in range(d - span + 1, d) :
+            psum = psum + tseries[i][4]*tseries[i][5]
+            vsum = vsum + tseries[i][5]
+        avr = round(psum / vsum, 1)
+        tseries[d].append(avr)
 
-for dnum in range(0,len(tseries)):
-    avrrow = [ tseries[dnum][0] ]
-    for span in avrspans:
-        if dnum > span:
-            avr = 0
-            for d in range(dnum-span+1,dnum):
-                avr = avr + tseries[d][4]
-            avr = avr/span
-            avrrow.append(avr)
-    averages.append(avrrow)
-
-print(averages)
+for row in tseries:
+    print(row)
 #for code in ranking:
 #    print(code,': ',ranking[code],'  ',codedict[code])
 
