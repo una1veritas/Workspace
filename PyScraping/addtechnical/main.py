6adc133cfa1765f72f3b6c69621502c94daa876a
Sin Shimozono <una.veritas@me.com>
2018-01-19 11:04:43 +0900

    addtech

--- PyScraping/addtechnical/main.py
+++ PyScraping/addtechnical/main.py
@@ -8,7 +8,6 @@ if len(sys.argv) < 2:
 stockcode = sys.argv[1]
 print (stockcode)
 files_list = glob.glob('../yfseries/'+stockcode+'-*-*.csv')
-files_list.sort(reverse=False)
 print(files_list)
 
 tseries = []
@@ -18,9 +17,27 @@ for fname in files_list:
         header = next(csv_reader)  # ヘッダーを読み飛ばしたい時
         header = next(csv_reader)  # ヘッダーを読み飛ばしたい時
         for row in csv_reader:
+            for index in range(1,len(row)):
+                row[index] = int(row[index])
             tseries.append(row)
-    
-print(tseries)
+
+tseries[1:].sort(reverse=False)
+
+averages = []
+avrspans = [ 5, 21, 55 ]
+
+for dnum in range(0,len(tseries)):
+    avrrow = [ tseries[dnum][0] ]
+    for span in avrspans:
+        if dnum > span:
+            avr = 0
+            for d in range(dnum-span+1,dnum):
+                avr = avr + tseries[d][4]
+            avr = avr/span
+            avrrow.append(avr)
+    averages.append(avrrow)
+
+print(averages)
 #for code in ranking:
 #    print(code,': ',ranking[code],'  ',codedict[code])
 
