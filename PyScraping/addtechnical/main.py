6fa888ede7f14528c3ac6e2290d91f93a156868f
Sin Shimozono <una.veritas@me.com>
2018-01-22 01:41:35 +0900

    addtechnical

--- PyScraping/addtechnical/main.py
+++ PyScraping/addtechnical/main.py
@@ -6,23 +6,38 @@ import math
 from collections import deque
 from statistics import stdev
 
-if len(sys.argv) < 2:
+# default values for options
+params = { 'mavr' : [5, 25, 50] }
+
+for arg in sys.argv[1:] :
+    if arg == '-vw' :
+        params['volweighted'] = True
+    elif arg[:2] == '-m' :
+        t = arg.split('.')[1:]
+        params['mavr'] = [ int(t[0]), int(t[1]), int(t[2]) ]
+    elif arg == '-ohlc' :
+        params['ohlc'] = True
+    else:
+        if len(arg.split('.')) == 2 :
+            params['code'] = arg
+        else:
+            params['code'] = arg + '.T'
+
+if not ('code' in params) :
     exit
-stockcode = sys.argv[1]
-print (stockcode)
-files_list = glob.glob('../yfseries/'+stockcode+'-*-*.csv')
+    
+print (params)
+
+files_list = glob.glob('../yfseries/'+params['code']+'-*-*.csv')
 print(files_list)
 
 tseries = []
-header = []
+header = ['date', 'open', 'high', 'low', 'close', 'volume', 'adj.close' ]
 for fname in files_list:
     with open(fname, 'r') as file:
         csv_reader = csv.reader(file)
         next(csv_reader)  # ヘッダーを読み飛ばしたい時
-        if len(header) == 0 : 
-            header = next(csv_reader)  # ヘッダーを読み飛ばしたい時
-        else:
-            next(csv_reader)
+        next(csv_reader)
         for row in csv_reader:
             for index in range(1,len(row)):
                 row[index] = int(row[index])
@@ -30,69 +45,61 @@ for fname in files_list:
 
 tseries.sort()
 
-#moving average
-avrspans = [ 5, 21, 55 ]
+avrspans = params['mavr']
 spanqs = [ deque([ ]), deque([ ]), deque([ ]) ]
-header = header + ['m.avr.5', 'm.avr.21', 'm.avr.55']
-for i in range(0,len(tseries)) :
-    mavrs = [ 0, 0, 0 ]
-    for sindex in range(0, len(avrspans)) :
-        spanqs[sindex].append(tseries[i][4])
-        if len(spanqs[sindex]) >= avrspans[sindex] :
-            spanqs[sindex].popleft()
-        mavrs[sindex] = round(sum(spanqs[sindex])/len(spanqs[sindex]),1)
-    tseries[i] = tseries[i] + mavrs
-    if len(spanqs[2]) > 1 :
-        tseries[i].append(round(stdev(spanqs[2]),1))
-    else:
-        tseries[i].append(0)
-
-#moving volume weighted average
-avrspans = [ 5, 21, 55 ]
-spanqs = [ deque([ ]), deque([ ]), deque([ ]) ]
-volqs  = [ deque([ ]), deque([ ]), deque([ ]) ]
-header = header + ['m.vwavr.5', 'm.vwavr.21', 'm.vwavr.55']
-for i in range(0,len(tseries)) :
-    vwmavrs = [ 0, 0, 0 ]
-    for sindex in range(0, len(avrspans)) :
-        spanqs[sindex].append(tseries[i][4])
-        volqs[sindex].append(tseries[i][5]/100)
-        if len(spanqs[sindex]) >= avrspans[sindex] : 
-            spanqs[sindex].popleft()
-            volqs[sindex].popleft()
-        psum = 0
-        vsum = 0
-        for j in range(0,len(spanqs[sindex])) :
-            psum = psum + spanqs[sindex][j]*volqs[sindex][j]
-            vsum = vsum + volqs[sindex][j]
-        vwmavrs[sindex] = round(psum/vsum, 1)
-    tseries[i] = tseries[i] + vwmavrs
-#    if len(spanqs[2]) > 1 :
-#                
-#    else:
-#        tseries[i].append(0)
-
-#Bollinger band, volume weighted
-span = 21
-header = header + ['vw.stddev. 21']
-for i in range(0,len(tseries)) :
-    newrow = [ '' ]
-    if i < span - 1:
-        continue
-    else:
-        psum = 0
-        vsum = 0
-        for j in range(i-span+1, i+1) :
-            psum = psum + tseries[j][4] * (tseries[j][5]/100)
-            vsum = vsum + (tseries[j][5]/100)
-        avr = round(psum / vsum, 1)
-        dev2sum = 0
-        for j in range(i-span+1, i+1) :
-            dev2sum = dev2sum + (tseries[j][5]/100) * ((tseries[j][4] - avr)** 2)
-        s = math.sqrt(dev2sum/(vsum-1))
-        newrow[0] = round(s,1)
-    tseries[i] = tseries[i] + newrow
-
+header = header + ['m.avr.'+str(avrspans[0]), 'm.avr.'+str(avrspans[1]), 'm.avr.'+str(avrspans[2]), 'm.stddev']
+if not ('volweighted' in params) :
+    # moving average and std. deviations
+    for i in range(0,len(tseries)) :
+        mavrs = [ 0, 0, 0 ]
+        for sindex in range(0, len(avrspans)) :
+            if 'ohlc' in params :
+                price = round(sum(tseries[i][1:4])/4,1)
+            else:
+                price = tseries[i][4]
+            spanqs[sindex].append(price)
+            if len(spanqs[sindex]) >= avrspans[sindex] :
+                spanqs[sindex].popleft()
+            mavrs[sindex] = round(sum(spanqs[sindex])/len(spanqs[sindex]),1)
+        tseries[i] = tseries[i] + mavrs
+        if len(spanqs[2]) > 1 :
+            tseries[i].append(round(stdev(spanqs[2]),1))
+        else:
+            tseries[i].append(0)
+else:
+    # moving volume weighted average, and std. dev.
+    volqs  = [ deque([ ]), deque([ ]), deque([ ]) ]
+    for i in range(0,len(tseries)) :
+        vwmavrs = [ 0, 0, 0 ]
+        for sindex in range(0, len(avrspans)) :
+            if 'ohlc' in params :
+                price = round(sum(tseries[i][1:5])/4,1)
+            else:
+                price = tseries[i][4]
+            spanqs[sindex].append(price)
+            volqs[sindex].append(tseries[i][5]/100)
+            if len(spanqs[sindex]) >= avrspans[sindex] : 
+                spanqs[sindex].popleft()
+                volqs[sindex].popleft()
+            psum = 0
+            vsum = 0
+            for j in range(0,len(spanqs[sindex])) :
+                psum = psum + spanqs[sindex][j]*volqs[sindex][j]
+                vsum = vsum + volqs[sindex][j]
+            vwmavrs[sindex] = round(psum/vsum, 1)
+        tseries[i] = tseries[i] + vwmavrs
+        if len(spanqs[1]) == 1 :
+            sigma = 0
+        else:
+            vwmavr = vwmavrs[1]
+            dev2sum = 0
+            vsum = 0
+            for j in range(0, len(spanqs[1])) :
+                dev2sum = dev2sum + volqs[1][j] * ((spanqs[1][j] - vwmavr)**2) 
+                vsum = vsum + volqs[1][j]
+            sigma = math.sqrt(dev2sum / (vsum - 1))
+        tseries[i].append(round(sigma,1))
+        
 for colname in header:
     print(str(colname)+',',end='')
 print()
