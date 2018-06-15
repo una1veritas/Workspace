033c4b1f15e9e7800a526135745f8170bef3bf65
Sin Shimozono <una.veritas@icloud.com>
2018-06-15 17:12:51 +0900

    chg

--- PyTest/HamiltonCircuit/__main__.py
+++ PyTest/HamiltonCircuit/__main__.py
@@ -1,17 +1,24 @@
 import sys
 import random
 
-vertices = set()
-for v in sys.argv[1].split():
-    vertices.add(v)
-elist = sys.argv[2].split()
-edges = set()
-for i in range(0, len(elist), 2) :
-    edges.add( tuple(sorted([elist[i], elist[i+1]])) )
+def get_graph(arglist):
+    vertices = set()
+    if ',' in arglist[0]:
+        arglist[0] = arglist[0].split(',')
+    else:
+        arglist[0] = arglist[0].split()
+    for v in arglist[0] :
+        vertices.add(v)
+    arglist.pop(0)
+    elist = arglist.pop(0).split()
+    edges = set()
+    for i in range(0, len(elist), 2) :
+        edges.add( tuple(sorted([elist[i], elist[i+1]])) )
+    return (vertices, edges)
 
+(vertices,edges) = get_graph(sys.argv[1:])
 print('Vertices = '+str(vertices))
 print('Edges = '+str(edges))
-
 circuit = list(vertices)
 #非決定的な順列の生成をランダムシャッフルでシミュレーション
 random.shuffle(circuit)
