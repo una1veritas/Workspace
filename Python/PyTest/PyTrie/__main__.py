938cebd8149362565c799df92bf3f4c2f797992c
Sin Shimozono <una.veritas@icloud.com>
2018-09-05 16:46:29 +0900

    pytrie

--- Python/PyTest/PyTrie/__main__.py
+++ Python/PyTest/PyTrie/__main__.py
@@ -13,12 +13,21 @@ class Trie:
     def addPath(self, a_word):
         curr_node = self.root
         for c in a_word:
-            if not ( (curr_node, c) in self.arcs) :
+            if not (curr_node in self.arcs) :
                 ''' new node == the last node id + 1 == the size '''
                 new_node = len(self.nodes)
                 self.nodes.add(new_node)
-                self.arcs[(curr_node,c)] = new_node 
+                self.arcs[curr_node] = dict()
+            if not (c in self.arcs[curr_node]) :
+                self.arcs[curr_node][c] =  
             curr_node = self.arcs[(curr_node,c)]
+
+    def traverse(self):
+        curr_node = self.root
+        prev_node = -1 # NIL
+        while ( curr_node != -1 ):
+            print('current = ' + str(curr_node) + ', prev = ' + str(prev_node) )
+            children = self.arcs[current
             
     def __str__(self):
         return 'Trie' + str(self.arcs)
