3335e72b6c1ff88a7eacffd3c89acf4292805ce8
Sin Shimozono <una.veritas@me.com>
2018-09-20 22:21:59 +0900

    read main

--- Python/ReadExcel/reader/__main__.py
+++ Python/ReadExcel/reader/__main__.py
@@ -5,14 +5,33 @@ import os
 import glob
 import sys
 
+args = dict()
+args['base_path'] = 'C:/Users/Sin Shimozono/Downloads'
+args['file_pattern'] = './*_Short_Positions.xls'
+args['codes'] = []
+index = 1
+while index  < len(sys.argv) :
+    if sys.argv[index][0] == '-' :
+        if sys.argv[index] == '-path' :
+            index = index + 1
+            args['base_path'] = sys.argv[index]
+        elif sys.argv[index] == '-code' :
+            index = index + 1
+            for a_code in sys.argv[index].split(':') :
+                args['codes'].append(int(a_code))
+        else:
+            args[sys.argv[index]] = True
+    else:
+        args['file_pattern'] = sys.argv[index]
+    index = index + 1
 
-base_path = 'C:/Users/Sin Shimozono/Downloads'
-print(sys.argv)
-base_path=sys.argv[1]
-os.chdir(base_path)
-file_list = sorted([os.path.abspath(path) for path in glob.glob('./*_Short_Positions.xls')])
+print(args)
+os.chdir(args['base_path'])
+file_list = sorted([os.path.abspath(path) for path in glob.glob(args['file_pattern'])])
+#print(file_list)
+#exit()
 
-def xls_read(file_name):
+def xls_read(file_name, codes):
     column_names = ['leftmargin',
                     'Date',
                     'Code',
@@ -31,7 +50,7 @@ def xls_read(file_name):
                     'Notes',
                     'rightmargin'
                     ]
-    column_list=[1,2,3, 5,  10,11, 13,14 ]
+    column_list=[1,2,3, 5,  10,11, ]
     data_types = {#'Date' : str,
                 'Code': int,
                 'Name' : str,
@@ -53,6 +72,7 @@ def xls_read(file_name):
 #        file_timestamp = datetime.datetime.strptime(str(df.iat[2,1]), '%Y-%m-%d %H:%M:%S')
 #     print('Date of disclosure: ' + file_timestamp.strftime('%Y-%m-%d'))
     df.drop(index=[0,1,2,3,4,5,6],inplace=True)
+    df = df.loc[ df['Code'].isin(codes) ]
     # replace chars not compatible with shift-jis
     replace_dict = {'\u2013':'-', u'\uff0d': u'\u2212', u'\xa0': '', '\u3231': u'(株)' }
     for (t_key, t_value) in replace_dict.items() :
@@ -64,10 +84,13 @@ def xls_read(file_name):
 df = pandas.DataFrame()
 for filename in file_list:
     print(filename)
-    df = df.append(xls_read(filename))
+    df = df.append(xls_read(filename, args['codes']))
 print('reading files finished.')
 
-df = df.set_index(['Code', 'Name', 'Short Seller', 'Date'], drop=True)
-df.sort_index(inplace=True)
+#df.set_index(['Code', 'Short Seller', 'Date'], inplace=True)
+df.sort_values('Date',inplace=True)
+short_sellers = df['Short Seller'].unique()
+print(sorted(short_sellers))
+exit()
 df.to_csv('short-positions-'+datetime.datetime.today().strftime('%Y%m%d')+'.csv', encoding='shift-jis')
 print('writing output finished.')
