bc988db2d4b7fe6be5505710981fabea32c701ca
Sin Shimozono <una.veritas@icloud.com>
2018-10-03 17:06:13 +0900

    last log

--- Python/PyLogparser/__main__.py
+++ Python/PyLogparser/__main__.py
@@ -5,32 +5,58 @@ Created on 2018/08/01
 '''
 
 import sys
+import subprocess
 
 if __name__ == '__main__':
     pass
+else:
+    exit()
 
-fname = sys.argv[1]
-print('filename: '+fname)
-bseq = [ ]
-monthdic = { 'Jan':1, 'Feb':2, 'Mar': 3, 'Apr': 4, 
-            'May': 5, 'Jun':6, 'Jul': 7, 'Aug': 8,
-            'Sep': 9, 'Oct': 10, 'Nov':11, 'Dec': 12, }
-with open(fname, "r") as ifile:
-    for a_line in ifile:
-        data = a_line.split()
-        if len(data) == 0:
-            break
-        if data[1][0:4] == 'ttys' :
-            continue
-        dcstr = data[0]
-        if not (dcstr == 'reboot' or dcstr == 'shutdown') :
-            dcstr = 'user'
-        datestr = str(monthdic[data[3]]).zfill(2)+'/'+data[4].zfill(2)
-        timestr = data[5]
-        bseq.append([datestr, timestr, dcstr])
+cmd_list = ['last']
+try:
+    res = subprocess.run(
+        cmd_list, 
+        shell=True, 
+        stdout=subprocess.PIPE,
+        universal_newlines=True,
+        )
+except subprocess.CalledProcessError:
+    print('error on '+str(cmd_list))
 
-bseq.reverse()
-#bseq.sort(key=lambda x:(x[0],x[1]))
+for a_line in res.stdout.splitlines():
+    line_list = a_line.split()
+    if len(line_list) <= 1:
+        continue
+    if line_list[1][:4] != 'ttys' :
+        print(line_list)
+    
+exit()
+    
+
+exit()
+
+# fname = sys.argv[1]
+# print('filename: '+fname)
+# bseq = [ ]
+# monthdic = { 'Jan':1, 'Feb':2, 'Mar': 3, 'Apr': 4, 
+#             'May': 5, 'Jun':6, 'Jul': 7, 'Aug': 8,
+#             'Sep': 9, 'Oct': 10, 'Nov':11, 'Dec': 12, }
+# with open(fname, "r") as ifile:
+#     for a_line in ifile:
+#         data = a_line.split()
+#         if len(data) == 0:
+#             break
+#         if data[1][0:4] == 'ttys' :
+#             continue
+#         dcstr = data[0]
+#         if not (dcstr == 'reboot' or dcstr == 'shutdown') :
+#             dcstr = 'user'
+#         datestr = str(monthdic[data[3]]).zfill(2)+'/'+data[4].zfill(2)
+#         timestr = data[5]
+#         bseq.append([datestr, timestr, dcstr])
+# 
+# bseq.reverse()
+# #bseq.sort(key=lambda x:(x[0],x[1]))
 
 date = ''
 time_from = ''
