f80b58a9f454a08d2809ba512feb2bc19dbcd08b
Sin Shimozono <una.veritas@me.com>
2018-10-05 11:32:49 +0900

    mysql

--- PySQL/PyMySQL/__main__.py
+++ PySQL/PyMySQL/__main__.py
@@ -13,6 +13,8 @@ def fetch_table(conection, query):
             break
     return t
 
+args = { 'code': 5301, 'date_from': '20180101'}
+
 url = urlparse('mysql://sin:qpm4nz@localhost:3306/stockanal')
 
 conn = mysql.connector.connect(
@@ -27,11 +29,19 @@ if not conn.is_connected():
     print('connection not available.')
     exit()
 
-sql = 'SELECT DISTINCT Short_Seller FROM short_positions WHERE Date >= \'{date}\' AND Code = {code}'.format(date='2018-09-01', code=5301)
-seller_table = sorted([row[0] for row in fetch_table(conn,sql) ])
-print(len(seller_table), seller_table[:10])
+sql_sellers = 'SELECT DISTINCT Short_Seller FROM short_positions WHERE Date >= \'{date}\' AND Code = {code}'
+sql_sellers = sql_sellers.format(date=args['date_from'], code=args['code'])
+seller_list = sorted([row[0] for row in fetch_table(conn, sql_sellers)])
 
-sql = 'SELECT Date, Short_Seller, Ratio FROM short_positions WHERE Date >= \'{date}\' AND Code = {code}'.format(date='2018-09-01', code=5301)
+sql_name = 'SELECT DISTINCT Name FROM short_positions WHERE Date >= \'{date}\' AND Code = {code}'
+sql_name = sql_name.format(date=args['date_from'], code=args['code'])
+name_list = sorted([row[0] for row in fetch_table(conn, sql_name)])
+
+sql = 'SELECT Date, Short_Seller, Ratio FROM short_positions WHERE Date >= \'{date}\' AND Code = {code}'.format(date=args['date_from'], code=args['code'])
+sql = sql.format(date=args['date_from'], code=args['code'])
 short_table = fetch_table(conn,sql)
+
+print(name_list,seller_list)
 print(short_table[:5])
+
 print('finished.')
