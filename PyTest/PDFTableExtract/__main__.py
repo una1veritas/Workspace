8446247860963741e64217ad3d3f5573f0b3df30
Sin Shimozono <una.veritas@me.com>
2018-10-19 09:38:31 +0900

    main

--- PyTest/PDFTableExtract/__main__.py
+++ PyTest/PDFTableExtract/__main__.py
@@ -1,1 +1,13 @@
-http://stanford.edu/~mgorkove/cgi-bin/rpython_tutorials/Using_Python_to_Extract_Tables_From_PDFs.php
+#
+# -*- coding: utf-8 -*-
+
+from glob import glob
+from tabula import read_pdf
+
+file_list = glob('../*.pdf') # PDFファイル取り込み
+print(file_list)
+
+pdf_file = file_list[0]
+
+df = read_pdf(pdf_file)
+print(df)
