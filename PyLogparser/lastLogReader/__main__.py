8c728c9055dd3c19865efa9ea4b0ee0080eaf242
Sin Shimozono <una.veritas@me.com>
2018-11-04 13:16:20 +0900

    logreader

---
+++ PyLogparser/lastLogReader/__main__.py
@@ -0,0 +1,90 @@
+'''
+Created on 2018/08/01
+
+@author: sin
+'''
+
+import sys
+import subprocess
+
+if __name__ == '__main__':
+    pass
+else:
+    exit()
+
+month_dict = { 'Jan':1, 'Feb':2, 'Mar': 3, 'Apr': 4, 
+             'May': 5, 'Jun':6, 'Jul': 7, 'Aug': 8,
+             'Sep': 9, 'Oct': 10, 'Nov':11, 'Dec': 12, }
+
+cmd_list = ['last']
+try:
+    res = subprocess.run(
+        cmd_list, 
+        shell=True, 
+        stdout=subprocess.PIPE,
+        universal_newlines=True,
+        )
+except subprocess.CalledProcessError:
+    print('error on '+str(cmd_list))
+    exit()
+
+event_log = dict()
+
+for each_line in res.stdout.splitlines():
+    item_list = each_line.split()
+    if len(item_list) >= 5:
+        line_date = str(month_dict[item_list[3]])+'/'+item_list[4].zfill(2)
+        line_day = item_list[2]
+        line_time = item_list[5]
+        if item_list[0] == 'reboot' or item_list[0] == 'shutdown':
+            if not (line_date, line_day) in event_log : 
+                event_log[(line_date, line_day)] = [(line_time, item_list[0])]
+            else:
+                event_log[(line_date, line_day)].append( (line_time, item_list[0]) )
+    
+    
+for key in sorted(event_log.keys()):
+    print(key, sorted(event_log[key]))
+exit()
+    
+
+exit()
+
+# fname = sys.argv[1]
+# print('filename: '+fname)
+# bseq = [ ]
+# with open(fname, "r") as ifile:
+#     for a_line in ifile:
+#         data = a_line.split()
+#         if len(data) == 0:
+#             break
+#         if data[1][0:4] == 'ttys' :
+#             continue
+#         dcstr = data[0]
+#         if not (dcstr == 'reboot' or dcstr == 'shutdown') :
+#             dcstr = 'user'
+#         datestr = str(monthdic[data[3]]).zfill(2)+'/'+data[4].zfill(2)
+#         timestr = data[5]
+#         bseq.append([datestr, timestr, dcstr])
+# 
+# bseq.reverse()
+# #bseq.sort(key=lambda x:(x[0],x[1]))
+
+date = ''
+time_from = ''
+time_to = ''
+for tp in bseq:
+    if date != tp[0] :
+        if date != '' :
+            print(date+'\t'+time_from+'\t'+time_to)
+        date = tp[0]
+        time_from = ''
+        time_to = ''
+    else:
+        if time_from == '' or tp[1] < time_from :
+            time_from = tp[1]
+        elif tp[1] > time_to :
+            time_to = tp[1]
+print(date+',\t'+time_from+',\t'+time_to)
+
+print('finished.')
--- PyXBRL/BeautifulSoupReader/__main__.py
+++ PyXBRL/BeautifulSoupReader/__main__.py
@@ -17,25 +17,84 @@ def main():
     if __name__ == '__main__':
         pass
     params = get_params()
+    file_name = dict()
     #tse-qcedussm-77510-20181017418792-ixbrl.htm
-    file_sum_name = params['basedir'].rstrip('/')+'/Summary/tse-*-ixbrl.htm'
-    file_sum = glob.glob(file_sum_name)
-    if len(file_sum) > 1 or len(file_sum) == 0:
-        print('no or more than one file(s) exist: ', file_sum_name, file_sum)
-        exit()
-    #html = urllib.request.urlopen(url_sum).read().decode('utf-8') 
-    xrblfile = open(file_sum[0], 'r', encoding='utf-8') 
-    soup = BeautifulSoup(xrblfile, 'lxml')
-    xrblfile.close()
+    #Attachment/0101010-qcbs01-tse-qcedjpfr-68610-2018-09-20-01-2018-11-01-ixbrl.htm
+    patterns = {'summary' : params['basedir'].rstrip('/')+'/Summary/tse-*-ixbrl.htm', 
+                'bs': params['basedir'].rstrip('/')+'/Attachment/*-qcbs*-tse-*-ixbrl.htm',
+                'pl': params['basedir'].rstrip('/')+'/Attachment/*-qcpl*-tse-*-ixbrl.htm',
+                'ci': params['basedir'].rstrip('/')+'/Attachment/*-qcci*-tse-*-ixbrl.htm',
+                'cf': params['basedir'].rstrip('/')+'/Attachment/*-qccf*-tse-*-ixbrl.htm', 
+                }
+    ix_dict = dict()
+    for key in patterns :
+        file_pattern = patterns[key]
+        file_name[key] = glob.glob(file_pattern)
+        if len(file_name[key]) != 1 or len(file_name[key][0]) == 0:
+            print('no or more than one file(s) exist: ', file_name[key], file_pattern)
+            continue
+        xrblfile = open(file_name[key][0], 'r', encoding='utf-8') 
+        soup = BeautifulSoup(xrblfile, 'lxml')
+        xrblfile.close()
+        f = open(key+'.txt', 'w', encoding='utf-8')
+        f.write(soup.prettify())
+        f.close()
 
-    f = open('prettify.txt', 'w', encoding='utf-8')
-    f.write(soup.prettify())
-    f.close()
-    ''' replace im-parsable ix:xxx tag to ix_xxx '''
+        ix_dict[key] = get_ix_dict(soup)
+
+    xbrl_info = BeautifulSoup('','lxml')
+    xbrl_info.append(xbrl_info.new_tag('database'))
+    xbrl_info.database.append(xbrl_info.new_tag('document'))
+    xbrl_info.database.document.append(xbrl_info.new_tag('bs'))
+
+    make_xbrl_subtree(ix_dict['bs'], xbrl_info.database.document.bs, xbrl_info)
+    print(xbrl_info.database.document.bs.prettify())
+    exit()
     
+    summary = get_summary(docroot)
+        
+    for key in summary: 
+        print(key + ' = ' + str(summary[key]) )
+#    print(curr2q.ConsolidatedMember.ResultMember.prettify())
+    exit()
+
+def make_xbrl_subtree(ix_dict, docroot, bs4):
+#     xbrldb = BeautifulSoup('','lxml')
+#     xbrldb.append(xbrldb.new_tag('database'))
+#     xbrldb.database.append(xbrldb.new_tag('document'))
+    for a_key in sorted(ix_dict):
+        if a_key == 'NoContextref' :
+            continue
+        node = docroot
+        for tag_name in a_key.split('_'):
+            nextnode = node.find(tag_name)
+            if nextnode == None:
+                node.append(bs4.new_tag(tag_name))
+                nextnode = node.find(tag_name)
+            node = nextnode
+        for tag_dict in ix_dict[a_key]:
+            newtag = bs4.new_tag(tag_dict['name'])
+            tag_dict.pop('name')
+            tag_dict.pop('type')
+            if 'format' in tag_dict: 
+                if (tag_dict['format'] == 'booleantrue') or (tag_dict['format'] == 'booleanfalse'):
+                    if tag_dict['format'] == 'booleantrue':
+                        tag_dict['boolean'] = True
+                    elif tag_dict['format'] == 'booleanfalse' :
+                        newtag['boolean'] = False
+                    tag_dict.pop('format')
+                elif tag_dict['format'] == 'numdotdecimal':
+                    tag_dict['text'] = tag_dict['text'].replace(',','')
+                    tag_dict.pop('format')
+            for attr_name in tag_dict:
+                if not (isinstance(tag_dict[attr_name], str) and tag_dict[attr_name].strip() == '') :
+                    newtag[attr_name] = tag_dict[attr_name]
+            node.append(newtag)
+    
+def get_ix_dict(bs):
     tag_pattern = re.compile('ix:\w*')
     contents = dict()
-    for node in soup.body.find_all(tag_pattern):
+    for node in bs.body.find_all(tag_pattern):
         ix_tag = dict()
         ix_tag['type'] = node.name.split(':')[1]
         ix_tag['contextref'] = node.get('contextref', 'NoContextref')            
@@ -56,7 +115,7 @@ def main():
             ix_scale = node.get('scale')
             if ix_scale:
                 ix_tag['scale'] = ix_scale
-            ix_tag['value'] = node.get_text(strip=True)
+            ix_tag['text'] = node.get_text(strip=True)
 
         if 'contextref' in ix_tag :
             ref = ix_tag.pop('contextref', None)
@@ -71,79 +130,48 @@ def main():
                 contents[ref] = [ ix_tag ]
             else:
                 contents[ref].append(ix_tag)
-
-    xbrldb = BeautifulSoup('','lxml')
-    xbrldb.append(xbrldb.new_tag('database'))
-    xbrldb.database.append(xbrldb.new_tag('document'))
-    docroot = xbrldb.database.document
-    
-    for a_key in sorted(contents):
-        if a_key == 'NoContextref' :
-            continue
-        node = docroot
-        for tag_name in a_key.split('_'):
-            nextnode = node.find(tag_name)
-            if nextnode == None:
-                node.append(xbrldb.new_tag(tag_name))
-                nextnode = node.find(tag_name)
-            node = nextnode
-        for tag_dict in contents[a_key]:
-            newtag = xbrldb.new_tag(tag_dict['name'])
-            tag_dict.pop('name')
-            tag_dict.pop('type')
-            if 'format' in tag_dict: 
-                if (tag_dict['format'] == 'booleantrue') or (tag_dict['format'] == 'booleanfalse'):
-                    if tag_dict['format'] == 'booleantrue':
-                        tag_dict['boolean'] = True
-                    elif tag_dict['format'] == 'booleanfalse' :
-                        newtag['boolean'] = False
-                    tag_dict.pop('format')
-                elif tag_dict['format'] == 'numdotdecimal':
-                    tag_dict['value'] = tag_dict['value'].replace(',','')
-                    tag_dict.pop('format')
-            for attr_name in tag_dict:
-                if not (isinstance(tag_dict[attr_name], str) and tag_dict[attr_name].strip() == '') :
-                    newtag[attr_name] = tag_dict[attr_name]
-            node.append(newtag)
-    
-    q_pattern = re.compile('CurrentAccumulatedQ[1234]Duration')
-    print(docroot.prettify())
-    curr_q2 = docroot.find(q_pattern)
-    curr_q2_result_entry = ['NetSales', 'ChangeInNetSales', 
+    return contents            
+                
+def get_summary(root_node):
+    result = dict()
+    patt = re.compile('CurrentAccumulatedQ[1234]Instant')
+    instant_node = root_node.find(patt)
+    result['DocumentName']   = instant_node.find('DocumentName').get('text')
+    result['CompanyName']    = instant_node.find('CompanyName').get('text')
+    result['SecuritiesCode'] = instant_node.find('SecuritiesCode').get('text')
+    patt = re.compile('CurrentAccumulatedQ[1234]Duration')
+    duration_node = root_node.find(patt)
+    if 'IFRS' in result['DocumentName'] :
+        result['Standards'] = 'IFRS'
+    else:
+        result['Standards'] = 'Japanese'
+        
+    japanese_entries = ['NetSales', 'ChangeInNetSales', 
                     'OperatingIncome', 'ChangeInOperatingIncome', 
                     'OrdinaryIncome', 'ChangeInOrdinaryIncome', 
                     'ProfitAttributableToOwnersOfParent', 'ChangeInProfitAttributableToOwnersOfParent', 
                     'ComprehensiveIncome', 'ChangeInComprehensiveIncome', 
                     'NetIncomePerShare', 'DilutedNetIncomePerShare', ]
-    '''
-       <RevenueIFRS scale="6" value="884044">
-   </RevenueIFRS>
-   <ChangeInRevenueIFRS scale="-2" value="20.3">
-   </ChangeInRevenueIFRS>
-   <ProfitBeforeTaxIFRS scale="6" value="37915">
-   </ProfitBeforeTaxIFRS>
-   <ChangeInProfitBeforeTaxIFRS scale="-2" value="94.3">
-   </ChangeInProfitBeforeTaxIFRS>
-   <ProfitIFRS scale="6" value="30272">
-   </ProfitIFRS>
-   <ChangeInProfitIFRS scale="-2" value="80.6">
-   </ChangeInProfitIFRS>
-   <ProfitAttributableToOwnersOfParentIFRS scale="6" value="27241">
-   </ProfitAttributableToOwnersOfParentIFRS>
-   <ChangeInProfitAttributableToOwnersOfParentIFRS scale="-2" value="77.4">
-   </ChangeInProfitAttributableToOwnersOfParentIFRS>
-   <TotalComprehensiveIncomeIFRS scale="6" value="36764">
-   </TotalComprehensiveIncomeIFRS>
-   <ChangesInTotalComprehensiveIncomeIFRS>
-   </ChangesInTotalComprehensiveIncomeIFRS>
-   <BasicEarningsPerShareIFRS scale="0" value="21.78">
-   </BasicEarningsPerShareIFRS>
-   <DilutedEarningsPerShareIFRS scale="0" value="21.77">
-   </DilutedEarningsPerShareIFRS>
-    '''
-    curr_q2_result = dict()
-    for each in curr_q2_result_entry:
-        each_entry = curr_q2.find(each)
+    ifrs_entries = ['NetSalesIFRS', 'ChangeInNetSalesIFRS',
+                          'OperatingIncomeIFRS', 'ChangeInOperatingIncomeIFRS',
+                          'RevenueIFRS', 'ChangeInRevenueIFRS', 
+                          'ProfitBeforeTaxIFRS', 'ChangeInProfitBeforeTaxIFRS',
+                          'ProfitIFRS', 'ChangeInProfitIFRS', 
+                          'ProfitAttributableToOwnersOfParentIFRS', 'ChangeInProfitAttributableToOwnersOfParentIFRS', 
+                          'TotalComprehensiveIncomeIFRS', 'ChangesInTotalComprehensiveIncomeIFRS',
+                          'BasicEarningsPerShareIFRS', 
+                          'DilutedEarningsPerShareIFRS', 
+                          ]
+    if result['Standards'] == 'Japanese':
+        entries = japanese_entries
+    elif result['Standards'] == 'IFRS':
+        entries = ifrs_entries
+    for each in entries:
+        each_entry = duration_node.find(each)
+        if not each_entry:
+            print('lacks: ',each)
+            continue
+        
         if each_entry.get('scale') :
             scale = int(each_entry.get('scale')) 
         else:
@@ -155,31 +183,8 @@ def main():
             val = 0
         if scale != 0 :
             val = val * (10**scale)
-        curr_q2_result[each] = val
-    
-    curr_q2_result['DocumentName'] = docroot.CurrentAccumulatedQ2Instant.find('DocumentName').get('text')
-    curr_q2_result['CompanyName'] = docroot.CurrentAccumulatedQ2Instant.find('CompanyName').get('text')
-    curr_q2_result['SecuritiesCode'] = docroot.CurrentAccumulatedQ2Instant.find('SecuritiesCode').get('text')
-    
-    for key in curr_q2_result: 
-        print(key + ' = ' + str(curr_q2_result[key]) )
-#    print(curr2q.ConsolidatedMember.ResultMember.prettify())
-    exit()
-
-# def get_table(node):
-#     table = list()
-#     attrs = dict()
-#     for row in node.select('tr'):
-#         columns = list()
-#         for td in row.select('th, td'):
-#             if td.span :
-#                 text = td.span.get_text(strip=True) #''.join([s.get_text(strip=True) for s in td.find_all('span')])
-#             else:
-#                 text = td.get_text(strip=True)
-#             columns.append( text )
-#         if sum([len(td) for td in columns]):
-#             result_table.append(columns)
-#     return (result_table, result_dict)
+        result[each] = val
+    return result
     
 def get_params():
     params = { 'arg': [] }
