abb82cb564d35d93ad3ffb94b7b4a455f4cbe405
Sin Shimozono <una.veritas@icloud.com>
2019-02-14 16:58:11 +0900

    bitstream

--- BitStream/BitStream.cpp
+++ BitStream/BitStream.cpp
@@ -6,11 +6,10 @@
 
 typedef uint64_t uint64;
 class BitStream {
-	std::vector<uint64> octwords;
-	unsigned int bitsize;
-	unsigned int head, tail;
-	uint64 cache;
-	unsigned int cached_oct, cached_bit;
+	std::vector<uint64> words;
+	std::vector<uint64>::iterator wordpos;
+	unsigned int bitpos;
+	uint64 cache_word;
 
 	inline static uint64 rotl64(const uint64_t & x) {
 		// assert (n<32);
@@ -23,19 +22,31 @@ class BitStream {
 	}
 
 public:
-	BitStream(const unsigned int & maxsize = 256) :
-		octwords(maxsize>>5), bitsize(maxsize), head(0), tail(0),
-		cache(0), cached_oct(0), cached_bit(0) { }
+	BitStream(const unsigned int & init_size = 256) :
+		words(init_size>>5), cache_word(0) {
+		wordpos = words.end();
+		bitpos = 0;
+	}
 
-	unsigned int count() const { return tail - head; }
-	bool empty() const { return tail > head; }
+	unsigned int bitcount(void) const { return ((words.size() - 1)<<5) + bitpos; }
+	bool empty() const { return (wordpos == words.begin()) && (bitpos == 0); }
 
+	bool open() { wordpos = words.begin(); bitpos = 0; return true; }
+	void reset() { flush(); open(); }
+	void flush() { rotl64(cache_word, 64 - bitpos); *wordpos = cache_word; }
+	bool close() { flush(); wordpos = words.end(); bitpos = 0; return true; }
+
+	uint64 read64() {
+		cache_word = *wordpos;
+		return cache_word;
+		++wordpos;
+	}
 	//
 	friend std::ostream & operator<<(std::ostream & stream, const BitStream & bstream) {
 		uint64 octword;
-		for (unsigned int i = 0 ; i < bstream.octwords.size() ; ++i) {
+		for (unsigned int i = 0 ; i < bstream.words.size() ; ++i) {
 			for(unsigned int j = 0; j < sizeof(uint64)*8; ++j) {
-				stream << (unsigned int )(1 & ((unsigned int) rotl64(bstream.octwords[i],j)));
+				stream << (unsigned int )(1 & ((unsigned int) rotl64(bstream.words[i],j)));
 			}
 			stream << " ";
 		}
