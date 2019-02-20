9902d5e78a099169e3fa01e0662938f81b088d18
Sin Shimozono <una.veritas@icloud.com>
2019-02-20 18:19:04 +0900

    BitStream

--- BitStream/main.cpp
+++ BitStream/main.cpp
@@ -6,9 +6,8 @@
 
 typedef uint64_t uint64;
 class BitStream {
-	std::vector<uint64> word;
-	uint64 current_word;
-	unsigned int bit_pos;
+	std::vector<uint64> words;
+	unsigned int bitcount;
 
 	inline static void rotl64(uint64 & x) {
 		// assert (n<32);
@@ -21,19 +20,20 @@ class BitStream {
 	}
 
 public:
-	BitStream(const unsigned int & init_size = 256) :
-		word(init_size>>5), current_word(0), bit_pos(0) {
+	BitStream(void) :
+		words(), bitcount(0) {
 	}
 
 	void append(const unsigned int & bit) {
-		current_word |= (uint64) (bit != 0);
-		rotl64(current_word);
-		++bit_pos;
-		if ( bit_pos == 64 ) {
-			word.push_back(current_word);
-			bit_pos = 0;
-			current_word = 0;
+		uint64 word = (bit != 0);
+		if ( (bitcount & 0x3f ) == 0 ) {
+			word <<= 63;
+			words.push_back(word);
+		} else {
+			word <<= (63 - (bitcount & 0x3f));
+			words[words.size() - 1] |= word;
 		}
+		++bitcount;
 	}
 /*
 	unsigned int bitcount(void) const { return ((words.size() - 1)<<5) + bitpos; }
@@ -42,7 +42,8 @@ public:
 	/*
 	bool open() { wordpos = words.begin(); bitpos = 0; return true; }
 	void reset() { flush(); open(); }
-	void flush() { rotl64(cache_word, 64 - bitpos); *wordpos = cache_word; }
+	void flush() {
+	}
 	bool close() { flush(); wordpos = words.end(); bitpos = 0; return true; }
 */
 	/*
@@ -56,8 +57,8 @@ public:
 
 	friend std::ostream & operator<<(std::ostream & stream, const BitStream & bstream) {
 		uint64 octword;
-		for (unsigned int i = 0 ; i < bstream.word.size() ; ++i) {
-			stream << std::hex << bstream.word[i];
+		for (unsigned int i = 0 ; i < bstream.words.size() ; ++i) {
+			stream << std::hex << bstream.words[i];
 			stream << " ";
 		}
 		return stream;
@@ -68,7 +69,9 @@ public:
 int main(int argc, char **argv) {
 	std::cout << "Hello, there." << std::endl;
 
-	BitStream bstream(500);
+	BitStream bstream;
+	bstream.append(1);
+	bstream.append(0);
 	bstream.append(1);
 	std::cout << bstream << std::endl;
 	return EXIT_SUCCESS;
