a335f0ea39a323e251f732ad4bbc31310a47a4c6
Sin Shimozono <una.veritas@icloud.com>
2019-02-21 18:06:51 +0900

    bitstream

--- BitStream/main.cpp
+++ BitStream/main.cpp
@@ -4,10 +4,12 @@
 #include <cinttypes>
 #include <vector>
 
-typedef uint64_t uint64;
-class BitStream {
+class BitArray {
+	typedef uint64_t uint64;
 	std::vector<uint64> words;
 	unsigned int bitcount;
+	uint64 current_word;
+	unsigned int current_bit_pos;
 
 	inline static void rotl64(uint64 & x) {
 		// assert (n<32);
@@ -20,42 +22,26 @@ class BitStream {
 	}
 
 public:
-	BitStream(void) :
-		words(), bitcount(0) {
+	BitArray(const unsigned int & max_bits = 64) :
+		words(max_bits>>6), bitcount(max_bits), current_word(0), current_bit_pos(0) { }
+
+	void clearAll(void) {
+		for(auto i = words.begin(); i != words.end(); ++i)
+			*i = 0;
 	}
 
-	void append(const unsigned int & bit) {
-		uint64 word = (bit != 0);
-		if ( (bitcount & 0x3f ) == 0 ) {
-			word <<= 63;
-			words.push_back(word);
+	bool set(const unsigned int & pos, const unsigned int & bit) {
+		if ( bit != 0 ) {
+			words[pos>>6] |= ((uint64) 1) << (pos & 0x3f);
+			return 1;
 		} else {
-			word <<= (63 - (bitcount & 0x3f));
-			words[words.size() - 1] |= word;
+			words[pos>>6] &= ~(((uint64) 1) << (pos & 0x3f));
+			return 0;
 		}
-		++bitcount;
 	}
-/*
-	unsigned int bitcount(void) const { return ((words.size() - 1)<<5) + bitpos; }
-	bool empty() const { return (wordpos == words.begin()) && (bitpos == 0); }
-*/
-	/*
-	bool open() { wordpos = words.begin(); bitpos = 0; return true; }
-	void reset() { flush(); open(); }
-	void flush() {
-	}
-	bool close() { flush(); wordpos = words.end(); bitpos = 0; return true; }
-*/
-	/*
-	uint64 read64() {
-		cache_word = *wordpos;
-		return cache_word;
-		++wordpos;
-	}
-	*/
 	//
 
-	friend std::ostream & operator<<(std::ostream & stream, const BitStream & bstream) {
+	friend std::ostream & operator<<(std::ostream & stream, const BitArray & bstream) {
 		uint64 octword;
 		for (unsigned int i = 0 ; i < bstream.words.size() ; ++i) {
 			stream << std::hex << bstream.words[i];
@@ -69,10 +55,10 @@ public:
 int main(int argc, char **argv) {
 	std::cout << "Hello, there." << std::endl;
 
-	BitStream bstream;
-	bstream.append(1);
-	bstream.append(0);
-	bstream.append(1);
+	BitArray bstream(128);
+	bstream.set(0,1);
+	bstream.set(1,0);
+	bstream.set(2,1);
 	std::cout << bstream << std::endl;
 	return EXIT_SUCCESS;
 }
