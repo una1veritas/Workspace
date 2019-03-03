3e892240758b15a4f89cc834bb067eb4d1c4e9ee
Sin Shimozono <una.veritas@me.com>
2019-03-03 19:09:26 +0900

    bitstream

--- BitStream/main.cpp
+++ BitStream/main.cpp
@@ -4,12 +4,10 @@
 #include <cinttypes>
 #include <vector>
 
-class BitArray {
+class bit64array {
 	typedef uint64_t uint64;
-	std::vector<uint64> words;
+	uint64 bitarray;
 	unsigned int bitcount;
-	uint64 current_word;
-	unsigned int current_bit_pos;
 
 	inline static void rotl64(uint64 & x) {
 		// assert (n<32);
@@ -22,29 +20,59 @@ class BitArray {
 	}
 
 public:
-	BitArray(const unsigned int & max_bits = 64) :
-		words(max_bits>>6), bitcount(max_bits), current_word(0), current_bit_pos(0) { }
+	bit64array(void) : bitarray(0), bitcount(0) { }
 
-	void clearAll(void) {
-		for(auto i = words.begin(); i != words.end(); ++i)
-			*i = 0;
+	void appendBit(const bool & b) {
+		rotl64(bitarray);
+		bitarray |= (b? 1 : 0);
+		++bitcount;
 	}
 
-	bool set(const unsigned int & pos, const unsigned int & bit) {
-		if ( bit != 0 ) {
-			words[pos>>6] |= ((uint64) 1) << (pos & 0x3f);
-			return 1;
-		} else {
-			words[pos>>6] &= ~(((uint64) 1) << (pos & 0x3f));
-			return 0;
+	void append(const char * str) {
+		while (*str) {
+			if ( (sizeof(uint64)<<3) - bitcount >= (sizeof(char)<<3) ) {
+				rotl64(bitarray,8);
+				bitarray |= (uint64)(*str);
+				bitcount += 8;
+				++str;
+				std::cout << "." << std::flush;
+
+			} else {
+				for (unsigned int i = (sizeof(char)<<3) - ((sizeof(uint64)<<3) - bitcount);
+						i < sizeof(char)<<3; ++i) {
+					appendBit(((*str)>>i) & 1);
+				}
+				std::cout << "!" << std::flush;
+				return;
+			}
 		}
 	}
+
+	void append(const uint32_t & val) {
+		const unsigned int val_size =  sizeof(uint32_t);
+		for (unsigned int i = 0; i < val_size; ++i ) {
+			rotl64(bitarray,8);
+			bitarray |= (val >> ((val_size- 1 - i)<<3)) & 0xff;
+			bitcount += 8;
+		}
+	}
+
+	bool is_full(void) const {
+		return bitcount == 64;
+	}
+
+	unsigned int size() const { return (bitcount & 0x3f) + (bitcount & 0x40); }
+
+	bool operator[](const unsigned int i) const {
+		return bitarray & (1<<((bitcount-1-i) & 0x3f));
+	}
+
 	//
 
-	friend std::ostream & operator<<(std::ostream & stream, const BitArray & bstream) {
-		uint64 octword;
-		for (unsigned int i = 0 ; i < bstream.words.size() ; ++i) {
-			stream << std::hex << bstream.words[i];
+	friend std::ostream & operator<<(std::ostream & stream, const bit64array & barray) {
+		std::cout << barray.size() << std::endl;
+		for (unsigned int i = 0 ; i < barray.size() ; ++i) {
+			stream << std::hex << barray[i];
 			stream << " ";
 		}
 		return stream;
@@ -55,10 +83,17 @@ public:
 int main(int argc, char **argv) {
 	std::cout << "Hello, there." << std::endl;
 
-	BitArray bstream(128);
-	bstream.set(0,1);
-	bstream.set(1,0);
-	bstream.set(2,1);
-	std::cout << bstream << std::endl;
+	bit64array barray;
+	barray.appendBit(1);
+	barray.append("A stack.");
+	/*
+	barray.append((bool)0);
+	barray.append((bool)1);
+	barray.append((bool)1);
+	barray.append((bool)0);
+	*/
+	//barray.append((uint32_t)0x2bda0);
+	std::cout << "unsigned int size = " << sizeof(unsigned int) << std::endl;
+	std::cout << "bit64array (" << barray.size() << ") = " << barray << std::endl;
 	return EXIT_SUCCESS;
 }
