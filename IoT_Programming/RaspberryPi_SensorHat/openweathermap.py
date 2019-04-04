a9dd1bec755fbddc9aa81845dd7331fcc84bab8c
Sin Shimozono <una.veritas@me.com>
2019-04-05 01:23:44 +0900

    text

--- IoT_Programming/RaspberryPi_SensorHat/openweathermap.py
+++ IoT_Programming/RaspberryPi_SensorHat/openweathermap.py
@@ -1,34 +1,25 @@
-# 引数に都市名の入力が必要
-#URL format
-#http://maps.openweathermap.org/maps/2.0/weather/{op}/{z}/{x}/{y}
-#Parameters:
-#op - Weather map layer. Full list of available weather map layers here.
-#z - Number of zoom level
-#x - Number of x tile coordinate
-#y - Number of y tile coordinate
-#appid - Personal API key
-#date - Date and time of (Unix time, UTC), e.g. date=1527811200. If you do not specify any date and time, you will get Current weather map. More information about how to get Current, Forecast and Historical maps you can find here.
-#Optional parameters:
-#opacity - Degree of layer opacity. Available value from 0 to 1 (default - 0.8)
-#palette - Color palette. You can use custom palettes for each layer. You can also create a palette for yourself and pass it to the URL as follows {value}:{HEX color};..;{value}:{HEX color}. More information about it here.
-#fill_bound - 'true' or 'false'. If 'true', then all weather values outside the specified set of values will be filled by color corresponding to the nearest specified value (default value - 'false': all weather values outside the specified set of values are not filled).
-#arrow_step - Step of values for drawing wind arrows, specify in pixels (default - 32). Parameter only for wind layers (WS10, WND).
-#use_norm - 'true' or 'false'. If 'true', then the length of the arrows is normalizing (default - 'false': the length of the arrows is proportional to the speed wind value). Parameter only for wind layers (WS10, WND).
-#Example of URL
-#http://maps.openweathermap.org/maps/2.0/weather/TA2/{z}/{x}/{y}?date=1527811200&opacity=0.9&fill_bound=true&appid={api_key} 
-
+# -*- coding: utf-8 -*-
 import requests
 import json
+import datetime
 import sys
 
 params = dict()
 params['API_KEY'] = "3efcd2e4fecf61bd823bcc55b23b0aab"
 params['CITY_NAME']= "Fukuoka-shi"
-api = "http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={key}"
+
+if ( len(sys.argv) > 1 ) :
+    params['CITY_NAME']= sys.argv[1]
+
+api = ("http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={key}&units=metric")
 url = api.format(city = params['CITY_NAME'], key = params['API_KEY'])
-print("url = "+url)
 resp = requests.get(url)
 data_dict = json.loads(resp.text)
-for key in data_dict:
-    print(key, data_dict[key])
+print(data_dict)
 
+print(u"天気 "+data_dict["weather"][0]["main"])
+print(u"最高気温 {0} °C, 最低気温 {1} °C".format(data_dict["main"]["temp_max"], 
+                                         data_dict["main"]["temp_min"]))
+utimestamp = datetime.datetime.fromtimestamp(data_dict["sys"]["sunset"])
+sunset = utimestamp.strftime("%Y/%m/%d %H:%M:%S")
+print(u"日没 " + sunset)
--- IoT_Programming/RaspberryPi_SensorHat/quandl_test.py
+++ IoT_Programming/RaspberryPi_SensorHat/quandl_test.py
@@ -1,16 +1,9 @@
-#o-n5ZDbb-ewagY5j8VYf
-import quandl
 import requests
+import json
+import urllib.parse
 
-data = quandl.get("XJPX/13050", authtoken="o-n5ZDbb-ewagY5j8VYf")
-print(data[:5])
-
-#COLUMN    DESCRIPTION
-#Date    
-#Open    Stock price at market open
-#High    Highest price of the day
-#Low    Lowest price of the day
-#Close    Stock price at market close
-#Volume    Volume of trades for the day
-#Adjustment Factor    The factor by which historical share prices/volumes are adjusted
-#Adjustment Type    A numeric code (integer) corresponding to the corporate action that precipitated adjustment, such as dividend, consolidation, etc. If more than one corporate action occurs for the day, the individual codes are combined. For more details, see the Adjustment Types section below.
+api="https://www.quandl.com/api/v3/datatables/FXCM/H1?date=2019-04-03&symbol={0}&api_key=o-n5ZDbb-ewagY5j8VYf"
+qcode = urllib.parse.quote("XNAS/AAPL")
+resp = requests.get(api.format(qcode))
+data_dict = json.loads(resp.text)
+print(data_dict)
