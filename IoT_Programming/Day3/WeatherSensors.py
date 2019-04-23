26223b73a0a40610d9fdfdb39917681d491de0c5
Sin Shimozono <una.veritas@icloud.com>
2019-04-23 16:24:00 +0900

    Sensors

--- IoT_Programming/Day3/WeatherSensors.py
+++ IoT_Programming/Day3/WeatherSensors.py
@@ -5,12 +5,19 @@ sense = SenseHat()
 sense.clear()
 
 temp = round(sense.get_temperature(), 1)
+msg = 'Temp ' + str(temp) + 'deg '
+print(msg)
+sense.show_message(msg)
 humi = round(sense.get_humidity(), 1)
+msg = 'Humidity ' + str(humi) + '% '
+print(msg)
+sense.show_message(msg)
 pres = round(sense.get_pressure(), 1)
-
-sense.show_message('Temp ' + str(temp) + 'deg ')
-sense.show_message('Humidity ' + str(humi) + '% ')
-sense.show_message('Pressure ' + str(pres) + ' hPa ')
+msg = 'Pressure ' + str(pres) + ' hPa '
+print(msg)
+sense.show_message(msg)
 
 dt = datetime.now()
-sense.show_message(dt.strftime("%Y/%m/%d %H:%M:%S"))
+msg = dt.strftime("%Y/%m/%d %H:%M:%S")
+print(msg)
+sense.show_message(msg)
