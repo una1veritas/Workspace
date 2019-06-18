3a2d21e3daf4a344d3825e76cbfdbdeae0626a78
Sin Shimozono <una.veritas@me.com>
2019-06-18 20:15:22 +0900

    linenotify

--- IoT_Programming/Day3/LINENotify.py
+++ IoT_Programming/Day3/LINENotify.py
@@ -7,7 +7,7 @@ url_notifyapi = "https://notify-api.line.me/api/notify"
 token = "C8ehzq5NuFNre6HwwD31A3mIxhN7iElDsAahP3nNHO6"
 auth_headers = {"Authorization" : "Bearer "+ token}
 
-payload = {"message" :  "曇りのち晴れ"}
+payload = {"message" :  "がんがん雨"}
 opt_files = {"imageFile": open("cloudyandsunny.png", "rb")} #バイナリで画像ファイルを開きます。対応している形式はPNG/JPEGです。
 
 r = requests.post(url_notifyapi ,headers = auth_headers, params=payload, files=opt_files)
