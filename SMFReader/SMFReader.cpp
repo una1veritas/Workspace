2cb9943efb47422ef2eebbf9b03962b3741c00a3
Sin Shimozono <una.veritas@icloud.com>
2019-08-26 16:49:45 +0900

    SMFReader

--- SMFReader/SMFReader.cpp
+++ SMFReader/SMFReader.cpp
@@ -4,9 +4,50 @@
 #include <cctype>
 #include <cstring>
 
+typedef uint8_t uint8;
 typedef uint16_t uint16;
+typedef uint32_t uint32;
 
-struct SMFScore {
+struct SMFEvent {
+	uint32 deltatime;
+	union {
+		uint8 evtype;
+		uint8 note;
+	};
+	union {
+		char
+		uint16 duration;
+	};
+
+	enum EVENT_TYPE {
+		STAT_SYSEX = 0xf0, 	// System Exclusive
+		STAT_ESCEX = 0xf7, 	// Escaped System Exclusive
+		STAT_META = 0xff, 	// Meta
+		DATA = 0x0f, 		// Data
+	};
+
+	bool isData() const {
+		return (evtype & 0x80) == 0;
+	}
+
+	friend std::ostream & operator<<(std::ostream & ost, const SMFEvent & evt) {
+		if ( isData() ) {
+			if (evt.evtype == STAT_SYSEX) {
+				ost << "Status (Sys Exclusive) ";
+			} else if (evt.evtype == STAT_ESCEX) {
+				ost << "Status (Escape Sys Exclusive) ";
+			} else if (evt.evtype == STAT_META) {
+				if (evt.)
+			}
+		}
+		ost << std::hex << score.status << ", " << score.format << ", " << score.tracks << ", " << score.division;
+		return ost;
+	}
+
+};
+
+struct SMFStream {
+	std::ifstream & smfstream;
 	uint16 format, tracks, division;
 	uint16 status;
 
@@ -16,14 +57,19 @@ struct SMFScore {
 		ERROR_MTRK = 1<<2,
 	};
 
-	static void read_event(std::ifstream & ifs) {
-		unsigned int deltat = SMFScore::read_varlenint(ifs);
+	uint8 read_byte() {
+		return smfstream.get();
 	}
 
-	static unsigned int read_varlenint(std::ifstream & ifs) {
+	uint8 * read_byte(uint8 * buf, uint32 num) {
+		smfstream.read((char*)buf, num);
+		return buf;
+	}
+
+	uint32 read_varlenint() {
 		char tbyte;
-		unsigned int val = 0;
-		while ( ifs.get(tbyte) ) {
+		uint32 val = 0;
+		while ( smfstream.get(tbyte) ) {
 			val <<= 7;
 			val |= 0x07f & tbyte;
 			if ( (tbyte & 0x80) == 0 )
@@ -32,10 +78,11 @@ struct SMFScore {
 		return val;
 	}
 
-	SMFScore(std::ifstream & ifs) : format(0), tracks(0), division(0), status(0) {
+	SMFStream(std::ifstream & ifs) : smfstream(ifs),
+		format(0), tracks(0), division(0), status(0) {
 		unsigned char t[18];
 		status = 0;
-		if ( !ifs.read((char*) t, 18) ) {
+		if ( !smfstream.read((char*) t, 18) ) {
 			status |= ERROR_HEADER;
 			return;
 		}
@@ -52,8 +99,54 @@ struct SMFScore {
 		division = t[12]<<8 | t[13];
 	}
 
-	friend std::ostream & operator<<(std::ostream & ost, const SMFScore & score) {
-		ost << score.format << ", " << score.tracks << ", " << score.division;
+	~SMFStream() {
+		std::cerr << "closed." << std::endl;
+		smfstream.close();
+	}
+
+	SMFEvent getNextEvent() {
+		SMFEvent event;
+		uint32 deltat;
+		uint8 tbyte;
+		deltat = read_varlenint();
+		std::cout << "delta time = " << deltat << ", ";
+		tbyte = read_byte();
+		if ( (tbyte & 0x80) != 0 ) {
+			// status byte
+			std::cout << "status: ";
+			switch (tbyte) {
+			case SMFEvent::STAT_SYSEX:
+			case SMFEvent::STAT_ESCEX:
+				std::cout << "escaped/system exclusive event, ";
+				break;
+			case SMFEvent::STAT_META:
+				std::cout << "meta event, ";
+				tbyte = read_byte(); // event type
+				uint32 len = read_varlenint(); // event size
+				std::cout << "len = " << len << ", ";
+				if (tbyte == 0x2f) {
+					// may be 'end of track'
+					// len is always assumed to be zero.
+					//score.add(new MusicalNote(-1, -1, -1));
+					std::cout << "end of track/new track, ";
+				} else {
+					do {
+						std::cout << std::setw(2) << std::hex << (unsigned int) read_byte()
+								<< ", ";
+					} while (--len);
+					std::cout << std::endl;
+				}
+				break;
+			}
+		} else {
+			std::cout << "???";
+		}
+		std::cout << std::endl;
+		return event;
+	}
+
+	friend std::ostream & operator<<(std::ostream & ost, const SMFStream & score) {
+		ost << std::hex << score.status << ", " << score.format << ", " << score.tracks << ", " << score.division;
 		return ost;
 	}
 };
@@ -82,50 +175,11 @@ int main(int argc, char **argv) {
 		return -1;
 	}
 
-	SMFScore smfscore(infile);
-	std::cout << smfscore << std::endl;
-	char buf[4];
-	unsigned int deltat;
-	unsigned int len;
-	unsigned char tbyte;
-	infile.read(buf,4);
-	deltat = (unsigned int) SMFScore::read_varlenint(infile);
-	tbyte = (unsigned char) infile.get();
-	std::cout << "delta time = " << deltat << ", ";
-	if ( (tbyte & 0x80) != 0 ) {
-		// status byte
-		std::cout << "status byte: ";
-		switch (tbyte) {
-		case 0xf0:
-			// system exclusive event
-			std::cout << "system exclusive event, ";
-			break;
-		case 0xf7:
-			// escaped system exclusive event
-			std::cout << "secaped system exclusive event, ";
-			break;
-		case 0xff:
-			// meta event
-			std::cout << "meta event, ";
-			tbyte = (unsigned char) infile.get(); // event type
-			len = SMFScore::read_varlenint(infile); // event size
-			if (tbyte == 0x2f) {
-				// may be 'end of track'
-				// len is always assumed to be zero.
-				//score.add(new MusicalNote(-1, -1, -1));
-				std::cout << "end of track, ";
-				break; // go to the next track.
-			} else {
-				std::cout << "len = " << len << ", ";
-				do {
-					infile.get();
-				} while (--len);
-			}
-		}
-	} else {
-		std::cout << "???";
-	}
-	std::cout << std::endl;
-	infile.close();
+	SMFStream smf(infile);
+	std::cout << smf << std::endl;
+	uint8 buf[4];
+	smf.read_byte(buf, 4);
+
+	SMFEvent e = smf.getNextEvent();
 	return 0;
 }
