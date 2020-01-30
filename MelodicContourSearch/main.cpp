4cd669370c723cefeedb651da617c1627d29d1f7
Sin Shimozono <una.veritas@me.com>
2020-01-30 12:28:16 +0900

    opening

--- MelodicContourSearch/main.cpp
+++ MelodicContourSearch/main.cpp
@@ -12,7 +12,6 @@
 #include "SMFStream.h"
 
 #include "kmp.h"
-#include "DirectoryLister.h"
 
 typedef unsigned int  uint;
 typedef unsigned long ulong;
@@ -80,64 +79,109 @@ std::string & translate(const char * filename, std::string & sequence) {
 	}
 	for(int i = 0; i < 16; ++i) {
 		if ( melody[i].back() == -1) melody[i].pop_back();
-		for(int j = 0; j < melody[i].size(); ++j) {
-			sequence.push_back(melody[i][j]);
+		if ( melody[i].size() != 0 ) {
+			for(int j = 0; j < melody[i].size(); ++j) {
+				sequence.push_back(melody[i][j]);
+			}
+			sequence.push_back( -'\n' );
 		}
-		sequence.push_back( -'\n' );
 	}
 	return sequence;
 }
 
+std::stringstream & notename(std::stringstream & ssout, const int & nn) {
+	switch(nn%12) {
+	case 0:
+		ssout << "C";
+		break;
+	case 1:
+		ssout << "C#";
+		break;
+	case 2:
+		ssout << "D";
+		break;
+	case 3:
+		ssout << "D#";
+		break;
+	case 4:
+		ssout << "E";
+		break;
+	case 5:
+		ssout << "F";
+		break;
+	case 6:
+		ssout << "F#";
+		break;
+	case 7:
+		ssout << "G";
+		break;
+	case 8:
+		ssout << "G#";
+		break;
+	case 9:
+		ssout << "A";
+		break;
+	case 10:
+		ssout << "A#";
+		break;
+	case 11:
+		ssout << "B";
+		break;
+	}
+	ssout << (nn/12 - 2);
+	return ssout;
+}
+
 int main(int argc, char **argv) {
 
-	std::cout << "path = " << argv[1] << " melodic contour = " << argv[2] << std::endl;
-
-	const std::regex filepattern(".*\\.mid");
-	DirectoryLister dlister(argv[2]);
+	//const std::regex filepattern(".*\\.mid");
+	std::string filename(argv[2]);
 	kmp mcpat(argv[1]);
 
-	if ( ! dlister() ) {
-		std::cerr << "error: opendir returned a NULL pointer for the base path." << argv[2] << std::endl;
-		exit(1);
-	}
-
-	std::string melody, contour;
-	bool matched;
+	std::cout << "file: " <<filename << std::endl;
 	std::cout << "search for " << mcpat << std::endl << std::endl;
 
-	int i;
-	for(i = 1; dlister.get_next_file(filepattern) != NULL; ++i) {
-		melody.clear();
-		translate(dlister.entry_path().c_str(), melody);
+	std::string melody;
+	translate(filename.c_str(), melody);
 
-		contour.clear();
-		auto iter = melody.begin();
-		int8 lastchar = *iter;
-		++iter;
-		for( ; iter != melody.end(); ++iter) {
-			if ( *iter == -'\n' ) {
-				contour.push_back('\n');
-			} else if ( lastchar == *iter) {
-				contour.push_back('=');
-			} else if ( lastchar > *iter ) {
-				contour.push_back('+');
-			} else if ( lastchar < *iter ) {
-				contour.push_back('-');
+	std::stringstream contour;
+	int lastnote;
+	int notecounter = 0;
+	int track = 0;
+	for(auto iter = melody.begin(); iter != melody.end(); ++iter) {
+		if ( *iter == -'\n') {
+			contour << std::endl;
+			++track;
+			notecounter = 0;
+			continue;
+		} else if ( notecounter == 0 ) {
+			lastnote = *iter;
+			++notecounter;
+		} else {
+			if ( lastnote == *iter ) {
+				contour << '=';
+			} else if ( lastnote > *iter ) {
+				if ( lastnote > *iter + 2 )
+					contour << '-';
+				else
+					contour << 'b';
+			} else if ( lastnote < *iter ) {
+				if ( lastnote + 2 < *iter )
+					contour << '+';
+				else
+					contour << '#';
 			}
-			lastchar = *iter;
-		}
-
-		matched = false;
-		int res = mcpat.search(contour);
-		if ( res < melody.size() ) {
-			matched = true;
-			std::cout << "match found in " << melody.size() << " @" << res << " ";
-		}
-
-		if ( matched ) {
-			std::cout << dlister.entry_path().c_str() << std::endl;
+			lastnote = *iter;
+			++notecounter;
 		}
 	}
-	std::cout << i << " songs. " << std::endl;
+	contour << std::endl;
+	std::cout << contour.str() << std::endl << "finished." << std::endl;
+	/*
+	int res = mcpat.search(contour);
+	if ( res < melody.size() ) {
+		std::cout << "match found in " << filename << " " << res << " ` " << melody.size() << std::endl;
+	}
+	 */
 	return 0;
 }
---
+++ MelodicContourSearch/tanamachi.cpp
@@ -0,0 +1,238 @@
+/*きっちり5段階で比較*/
+#include <iostream> 
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <cctype>
+#include <cstring>
+
+#include <vector>
+#include <array>
+
+#include "libsmf/SMFEvent.h"
+#include "libsmf/SMFStream.h"
+#include "DirectoryLister.h"
+
+std::vector<int> f;
+int count = 0;
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+
+
+
+void loopcheck(char b[]){
+
+  int i = 0;
+  int j = 1;
+
+  f.resize(strlen(b)+1);
+
+  f[1] = 0;
+  while(b[j] != '\0'){
+    if(i == 0 || b[i] == b[j])
+      f[++j] = ++i;
+    else
+      i = f[i];
+  }
+}
+
+
+int search(const char a[], char b[]){
+  int i = 0;
+  int j = 0;
+  int frag = 0; // 1 まっちんぐちゅう。
+
+  //printf("pattern length = %d\n", strlen(b));
+  loopcheck(b);
+
+
+  while(b[j] != '\0'){
+    //printf("1 = %c",a[i]);
+    //printf(" 2 = %c\n",b[j]);
+    if(a[i] == '\0'){
+      //printf("not same \n");
+      return 0;
+    }
+    
+    if(a[i] != b[j] && frag == 0){
+      i++;
+    }
+    else if(a[i] != b[j] && frag == 1){
+      if(f[i] > 1) {
+	/*printf("j=%d, i=%d, f[i]=%d\n", j, i, f[i]);*/
+	j =f[j];
+      } else
+	j = 0;
+      frag = 0;
+    }
+    else{
+      i++;
+      j++;
+      frag = 1;
+    }
+    
+  }
+  
+  //printf("same \n");
+  //  allfrag = 1; 
+  return 1;
+  
+}
+
+
+std::array<std::vector<char>, 16> change(const char *filename) {
+	std::fstream infile;
+	std::vector<char> interval;
+	std::array<std::vector<char>, 16> allinterval;
+
+	infile.open(filename, (std::ios::in | std::ios::binary));
+	if (!infile) {
+		std::cerr << "失敗" << std::endl;
+		return allinterval;
+	}
+
+	SMFStream smf(infile);
+	count = count + 1;
+
+	uint32 delta_total = 0, last_total, last_number = 0, latest_number = 0;
+	for (int i = 1; i <= 16; i++) {
+		interval.clear();
+		smf.reset();
+		last_number = 0;
+		latest_number = 0;
+		while (smf.smfstream) {
+			SMFEvent evt = smf.getNextEvent();
+			if (evt.delta > 0)
+				delta_total += evt.delta;
+			if (evt.isMTRK()) {
+				delta_total = 0;
+				last_total = (uint32) -1;
+			}
+
+			if (evt.isNoteOn()) {
+				/*	if ( last_total != delta_total ) {
+				 std::cout << std::endl << std::dec << delta_total << " ";
+				 last_total = delta_total;
+				 }
+				 */
+
+				if (evt.channel() == i) {
+
+					if (last_total != delta_total) {
+						if (last_number > evt.number)
+						  if(last_number - evt.number > 2)
+							interval.push_back('-');
+						  else
+						    interval.push_back('b');
+
+						else if (last_number == evt.number)
+							interval.push_back('=');
+						else
+						  if(evt.number -last_number > 2)
+							interval.push_back('+');
+						  else
+						    interval.push_back('#');
+						
+						latest_number = last_number;
+						last_number = evt.number;
+						last_total = delta_total;
+					}
+
+					else { /*和音の時*/
+						if (interval.size() != 0) {
+							if (last_number < evt.number) {
+								interval.pop_back();
+								if (latest_number > evt.number)
+								  if(latest_number - evt.number > 2)
+								    interval.push_back('-');
+								  else
+								    interval.push_back('b');
+								else if (latest_number == evt.number)
+									interval.push_back('=');
+								else
+								  if(evt.number -latest_number > 2)
+								    interval.push_back('+');
+								  else
+								    interval.push_back('#');
+							}
+							last_number = evt.number;
+
+						}
+
+					}
+
+				}
+
+				//std::cout << evt << " ";
+			}
+
+		}
+		allinterval[i] = interval;
+		//printf("%d\n",i);
+
+	}
+	return allinterval;
+}
+
+
+  
+int main(int argc, char **argv) {
+
+  std::array<std::vector<char>, 16> allinterval;
+  DirectoryLister lister("./");
+  int counter = 0;
+  if ( ! lister() ) {
+    std::cerr << "error: opendir returned a NULL pointer." << std::endl;
+    exit(1);
+  }
+  while (lister.get_next_file(".*\\.mid") != NULL) {
+    ++counter;
+    printf("\n%s ",lister.entry_path().c_str());
+    allinterval=change(lister.entry_path().c_str());
+    printf("change finished.\n");
+    fflush(stdout);
+  
+  // std::vector<char> *allinterval= change(argv[1]); 
+
+    
+    /*printf("\n");
+	printf("[");
+	for(int i = 1; i <= 16; i++){
+	  for(int j = 0; allinterval[i].size()  > j; ++j) {
+	    printf("%c, ",allinterval[i][j]);
+	  }
+	printf("]\n");
+	}*/
+
+    int allfrag  = 0;
+	
+	for(int k = 0; k < 16; k++){
+	  
+	  if (allinterval[k].size() == 0) continue;
+	  
+	  std::string stdString(allinterval[k].begin(),allinterval[k].end());
+	  const char *a = stdString.c_str();
+	  
+	  //printf("%d: %s\n",k,a);
+	  char b[] = "+-#+=";
+	  allfrag = search(a,b);
+	  if (allfrag)
+	    break;
+	}
+	printf("総合結果 = ");
+	if(allfrag == 1)
+	  printf("一致 match %s\n",lister.entry_path().c_str());
+	else
+	  printf("不一致 no\n");
+
+  
+	std::cout << std::endl;
+  }
+  
+  printf("%d\n",count);
+  return 0;
+  	 
+}
+
