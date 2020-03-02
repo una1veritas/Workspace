5772632a67abaa1c40c3ef37d2784fc244d196ea
una1veritas <una.veritas@me.com>
2020-03-03 02:02:50 +0900

    dlister

--- MelodicContourSearch/dirlister.cpp
+++ MelodicContourSearch/dirlister.cpp
@@ -13,10 +13,11 @@
 #include "dirlister.h"
 
 int main( void ) {
-	std::regex regpat(".*\\.h");
-	dirlister dl(".", regpat);
-	while ( dl.get_next_entry() ) {
-		std::cout << dl.entry_name() << std::endl;
+	std::regex regpat(".*\\.(mid|MID)");
+	dirlister dl(".");
+	while ( dl.get_next_entry(regpat) ) {
+		//std::cout << dl.entry_name() << std::endl;
+		std::cout << dl.entry_fullpath() << std::endl;
 	}
 	std::cout << "finished." << std::endl;
 	return EXIT_SUCCESS;
