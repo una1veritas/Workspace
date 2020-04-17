1b2cfb2a9a456dd10317f6891380fe43e48a23c6
Sin Shimozono <una.veritas@me.com>
2020-04-18 01:07:17 +0900

    sars

--- SARS-CoV-2/fasta_reader/__main__.py
+++ SARS-CoV-2/fasta_reader/__main__.py
@@ -1,37 +1,87 @@
 #
 import sys
 
+def lcs(seq_a, seq_b, matchwidth):
+    # make sure _a is longer than or equal
+    if len(seq_a) < len(seq_b) :
+        seq_a, seq_b = seq_b, seq_a
+#    dpt = [[0 for c in range(0, len(seq_a) + 1)] for r in range(0,4)]
+    dpt = dict()
+    print('go.', file=sys.stderr)
+    for r in range(1, len(seq_b) + 1):
+        if (r % 2000) == 0 :
+            print('.', file=sys.stderr, flush=True, end='')
+        for c in range(max(1, r - matchwidth), 
+                       min(len(seq_a) - (len(seq_b) - matchwidth) + r, len(seq_a) + 1)):
+            if seq_a[c-1] == seq_b[r-1] :
+                #dpt[r & 3][c] = dpt[(r-1) & 3][c-1] + 1
+                if (r-1, c-1) not in dpt :
+                    dpt[(r,c)] = 1
+                else:
+                    dpt[(r,c)] = dpt[(r-1,c-1)] + 1
+            else:
+                #dpt[r & 3].append(max(dpt[(r-1) & 3][c], dpt[r & 3][c-1], dpt[r-1][c-1]))
+                if (r-1, c) not in dpt:
+                    r_1_c = 0
+                else:
+                    r_1_c = dpt[(r-1,c)]
+                if (r, c-1) not in dpt:
+                    r_c_1 = 0
+                else:
+                    r_c_1 = dpt[(r,c-1)]
+                if max(r_1_c, r_c_1) != 0 :
+                    dpt[(r,c)] = max(r_1_c, r_c_1)
+#               dpt[(r,c)] = max(dpt[(r-1)&3][c], dpt[r&3][c-1]) #, dpt[(r-1)&3][c-1])
+#   return ( dpt[len(seq_b) & 3][len(seq_a)], len(seq_a), len(seq_b) )
+    print()
+    return ( dpt[(len(seq_b), len(seq_a))], len(seq_a), len(seq_b) )
+
+
 filename = sys.argv[1]
 
+print('reading', end='', file=sys.stderr, flush=True)
 seqdb = list()
+min_seqlen = 19000
 lcount = 0
 with open(filename) as infile:
     a_tuple = list()
     strbuf = ''
-    for a_line in infile:
-        if a_line[0] == '>' :
+    while True:
+        a_line = infile.readline()
+        if len(a_line) == 0 or a_line[0] == '>':
             if len(a_tuple) != 0 :
-                a_tuple.append(strbuf)
-                seqdb.append(a_tuple)
+                a_tuple.append(strbuf.upper())
+                seqdb.append(tuple([a_tuple[1],a_tuple[2],a_tuple[0],a_tuple[3]]))
                 lcount += 1
-                if (lcount % 1000) == 0 :
-                    print(str(lcount)+'...')
+                if (lcount % 2000) == 0 :
+                    print('.', end='', file=sys.stderr, flush=True)
+            if len(a_line) == 0 :
+                break
             a_tuple = a_line.strip()[1:].split('|')
             strbuf = ''
         else:
             strbuf += a_line.strip()
-    else:
-        if len(a_tuple) != 0 :
-            a_tuple.append(strbuf)
-            seqdb.append(a_tuple)
-            lcount += 1
 
-print('reading done.')
-print('got ' + str(lcount) + ' sequences.')
-cnt = 0
-for a_record in sorted(seqdb, key=lambda r:r[1]):
-    cnt += 1
-    if cnt > 10 and cnt < 9170:
-        continue
-    print(a_record[0]+','+a_record[1]+','+a_record[2]+','+a_record[3][:24])
-print('finished.')
+print(' done.', file=sys.stderr)
+print('got ' + str(lcount) + ' sequences.', file=sys.stderr)
+
+#
+#for a_record in seqdb:
+#    print('{0},{1},{2},{3}'.format(a_record[0], a_record[1], a_record[2], a_record[3]))
+#print('finished.')
+
+seqdict = dict()
+keys = ['EPI_ISL_421585', 'EPI_ISL_419307', ]
+for i in range(0, len(seqdb)):
+#    if len(seqdb[i][3]) < 29900 * 0.97 :
+#        continue
+    if seqdb[i][0] in keys :
+        if seqdb[i][0] not in seqdict :
+            seqdict[seqdb[i][0]] = i
+
+seqs = [seqdb[i][3] for i in seqdict.values()]
+matchwidth = int(min(len(seqs[0]), len(seqs[1])) * 0.02)
+lcslen, len_longer, len_shorter = lcs(seqs[0], seqs[1], matchwidth)
+
+print('the length of longest common subsequence = ', lcslen)
+print(str(seqdict.keys()) + ' similarity =', float(lcslen)/len_shorter)
