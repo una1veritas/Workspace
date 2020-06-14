d82a2a2ebb1d3c94e68d885e9f825c3d9cd1ecd8
una1veritas <una.veritas@me.com>
2020-06-14 13:54:19 +0900

    blaster

--- MoodleBlasters/collect_answers/__main__.py
+++ MoodleBlasters/collect_answers/__main__.py
@@ -3,7 +3,7 @@ Created on 2020/05/21
 
 @author: sin
 '''
-
+# coding: utf-8
 import sys
 import os
 from bs4 import BeautifulSoup
@@ -13,7 +13,19 @@ if len(sys.argv) > 1:
 else:
     print('usage: command dir-path', file=sys.stderr)
     exit()
-    
+
+if not os.path.exists(basedir+'/namelist.csv') :
+    print('namelist.csv not exists.')
+    exit(1)
+
+namelist = dict()
+with open(basedir+'/namelist.csv', encoding='utf-8') as txtf:
+    for a_line in txtf:
+        a_row = a_line.split(',')
+        namelist[a_row[0]] = (a_row[0], a_row[1],a_row[3],a_row[5]) 
+
+print(namelist)
+
 dirlist = [basedir + '/' + path for path in os.listdir(basedir)]
 for probfolder in dirlist:
     print("entry: " + probfolder)
@@ -22,6 +34,7 @@ for probfolder in dirlist:
         for entry in indivs: 
             oltextpath = probfolder + '/' + entry + '/' + 'onlinetext.html'
             if os.path.exists(oltextpath) :
+                print(entry.split('_'))
                 with open(oltextpath, encoding='utf-8') as txtf:
                     contstr = txtf.read()
                 if len(contstr) :
