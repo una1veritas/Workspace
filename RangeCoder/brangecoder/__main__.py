6467a0d7015da1504f75fc2a725114ba4827cbf4
Sin Shimozono <una.veritas@icloud.com>
2020-10-29 17:10:56 +0900

    lastlog

--- RangeCoder/brangecoder/__main__.py
+++ RangeCoder/brangecoder/__main__.py
@@ -10,7 +10,7 @@ def bittream(fp, buffer_size = 32):
     buff = fp.read(buffer_size)
     while buff :
         for ch in buff:
-            for bitpos in range(0,8):
+            for bp in range(0,8):
                 yield 1 if (ch & 0x80) else 0
                 ch <<= 1
         buff = fp.read(buffer_size)
@@ -23,7 +23,7 @@ def nibblestream(fp, buffer_size = 32):
                 yield (0xf0 & (ch<<(4*i)))>>4
         buff = fp.read(buffer_size)
     
-def bytestream(fp, buffer_size = 32):    
+def bytestream(fp, buffer_size = 32) :
     buff = fp.read(buffer_size)
     while buff :
         for ch in buff:
@@ -32,23 +32,33 @@ def bytestream(fp, buffer_size = 32):
     
 def main(infilename=None):
     print(infilename)
-    hist = dict()
-    for i in range(256):
-        hist[i] = 1
-    hist_total = 256
+    hist = list()
+    hist.append(1)
+    hist_total = 1
+    for i in range(255):
+        hist.append(0)
+    ch_count = 1
     with open(infilename, "rb") as fp:
-        counter = 0
-        for a_byte in bytestream(fp, 32):
+        for a_byte in bytestream(fp, 128):
             hist[a_byte] += 1
             hist_total += 1
-            if chr(a_byte).isprintable() :
-                print(chr(a_byte), end='')
-            else:
-                print(hex(a_byte))
-            counter += 1
-            if (counter & 0x07) == 0 :
-                print() 
-    print(hist_total, hist)
+            ch_count += 1
+#             if chr(a_byte).isprintable() :
+#                 print(chr(a_byte), end='')
+#             else:
+#                 print(hex(a_byte))
+            if ch_count >= (1<<12) :
+                break
+    ranges = list()
+    subsum = 0
+    for i in range(0,len(hist)):
+        ranges.append( (subsum, subsum + hist[i]) )
+        subsum += hist[i]
+    print(subsum)
+    for ch in range(256):
+        (l, r) = ranges[ch]
+        if l != r :
+            print( chr(ch) if chr(ch).isprintable() else hex(ch) ,bin(l),bin(r), bin(l^r))
     print(2**math.ceil(math.log2(hist_total)))
     
 if __name__ == "__main__" :
