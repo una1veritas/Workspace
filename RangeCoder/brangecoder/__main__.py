094bccbaaf9fa30e409b4841a138f23db6b5bee7
Sin Shimozono <una.veritas@me.com>
2020-11-03 10:48:36 +0900

    main

--- RangeCoder/brangecoder/__main__.py
+++ RangeCoder/brangecoder/__main__.py
@@ -31,43 +31,60 @@ def bytestream(fp, buffer_size = 32) :
         buff = fp.read(buffer_size)
 
 def samehighbits(l, r, w): 
-    x = l^r
-    for b in range(w):
-        if (x>>(w-1-b)) & 1 :
+    bmask = 1<<(w-1)
+    bdigits = ''
+    while bmask :
+        if (l & bmask) != (r & bmask) :
             break
-#    print('w='+str(w)+', b='+str(b))
-    hbits = ''
-    if not b :
-        return hbits
-    for i in range(w):
-        if i < b :
-            if l>>(w-1-i) & 1 :
-                hbits += '1'
-            else:
-                hbits += '0'
-        else:
-            break
-    return hbits
+        bdigits += '1' if (l & bmask) != 0 else '0' 
+        bmask >>= 1 
+    return bdigits
    
-def update_histgram(hist, total, block, max_block_size):
+def update_histgram(hist, block, max_block_size):
     for c in block:
         hist[c] += 1
-        total += 1
+    
     if len(block) == max_block_size :
         for i in range(len(hist)) :
             hist[i] = (hist[i]+1)>>1
-        total >>= 1
-    return (hist, total)
+    return hist
+
+def encode_block(block, bitsize, sections, left = 0, right = 1, bits = 0):
+    codestr = ''
+    for i in range(len(block)) :
+        ch = block[i]
+        l, r = sections[ch], sections[ch+1]
+        width = right - left
+        right = (left<<bitsize) + (width * r)
+        left = (left<<bitsize) + (width * l)
+        bits += bitsize
+    # print( '[{}, {}), '.format(left/(1<<bits), right/(1<<bits)))
+        hbits = samehighbits(left, right, bits)
+        if len(hbits) :
+            mask = (1<<bits) - 1
+            mask >>= len(hbits)
+    # print(mask)
+            left &= mask
+            right &= mask
+            bits -= len(hbits)
+            codestr += hbits
+    #    print('{}:[{:b}, {:b}) ({})'.format(hbits, left, right, bits))
+    #                         print(encodedstr)
+    # #                     if (left & 0xff == 0) and (right & 0xff == 0) :
+    # #                         left >>= 8
+    # #                         right >>= 8
+    # #                         bits -= 8
+    codestr += '1'
+    return codestr
+
 
 def main(infile = None):
     byte_bitsize = 8
     print(infile)
     hist = list()
-    hist_total = 0
     # the initial histogram
     for i in range(1<<byte_bitsize):
         hist.append(1)
-        hist_total += 1
     
     try:
         block_size_bits = 8
@@ -78,55 +95,26 @@ def main(infile = None):
                 buff = fp.read(1<<block_size_bits)
                 # print('block_size = {}'.format(block_size) )
                 # if the block is full (not the final block)
-                if len(buff) == (1<<block_size_bits) :
-                    (hist, hist_total) = update_histgram(hist, hist_total, buff, 1<<block_size_bits_max)
-                    print('update '+str(hist_total))
+                if len(buff) == (1<<block_size_bits) and block_size_bits < block_size_bits_max :
+                    update_histgram(hist, buff, 1<<block_size_bits_max)
+                    print('update '+str(sum(hist)))
                 # setup the code_range
                 subsum = 0
                 code_range = [0]
                 for i in range(len(hist)):
                     subsum += hist[i]
                     code_range.append(subsum)
-
                 # encode
-                left = 0
-                right = 1
-                bits = 0
-                encodedstr = ''
-                for i in range(len(buff)) :
-                    ch = buff[i]
-                    l, r = code_range[ch], code_range[ch+1]
-                    width = right - left
-                    right = (left<<block_size_bits) + (width * r)
-                    left = (left<<block_size_bits) + (width * l)
-                    bits += block_size_bits
-#                    print( '[{}, {}), '.format(left/(1<<bits), right/(1<<bits)))
-                    hbits = samehighbits(left, right, bits)
-                    if len(hbits) :
-                        mask = (1<<bits) - 1
-                        mask >>= len(hbits)
-#                        print(mask)
-                        left &= mask
-                        right &= mask
-                        bits -= len(hbits)
-                        encodedstr += hbits
-#                         print('{}:[{:b}, {:b}) ({})'.format(hbits, left, right, bits))
-#                         print(encodedstr)
-# #                     if (left & 0xff == 0) and (right & 0xff == 0) :
-# #                         left >>= 8
-# #                         right >>= 8
-# #                         bits -= 8
-                encodedstr += '1'
-                print()
-                for i in range(len(encodedstr)):
+                encodedstr = encode_block(buff, block_size_bits, code_range, left = 0, right = 1, bits = 0)
+                for i in range(len(encodedstr)>>3):
                     binstr = encodedstr[i*8:(i+1)*8]
-                    print(binstr + ' ', end='')
+                    print(format(int(binstr,2),'02x') + ' ', end='')
                 print()
                 print(len(encodedstr), 1<<block_size_bits)
                 
                 # termination condition
                 if len(buff) != (1<<block_size_bits) :
-                    print(hist_total, 1<<block_size_bits)
+                    print(sum(hist), 1<<block_size_bits)
                     break
                 else:
                     if block_size_bits < block_size_bits_max :
@@ -134,7 +122,7 @@ def main(infile = None):
     except IOError:
         print('file "' + infile + '" open failed.')
         exit()
-    print('hist_total = {}'.format(hist_total))
+    print('hist_total = {}'.format(sum(hist)))
     for i in range(1<<byte_bitsize):
         if hist[i] == 1 :
             continue
