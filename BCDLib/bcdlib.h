1af82bc70b7f86bcc5bc8c68a4db39250d4c3f64
una1veritas <una.veritas@me.com>
2021-06-28 00:07:27 +0900

    bcd

--- BCDLib/bcdlib.h
+++ BCDLib/bcdlib.h
@@ -16,17 +16,17 @@
 struct bcdint {
 	uint64_t val;
 
+	static const uint64_t signflag = ((uint64_t)1 << 63);
+
 	bcdint(int64_t v) : val(0) {
 		int bits;
-		uint64_t negflag = v < 0 ? ((uint64_t)1 << 63) : 0;
-		//printf("v = %ld, neg = %ld\n",v,negflag);
-		if (negflag) {
+		uint64_t negflag = v < 0 ? signflag : 0;
+		if ( negflag )
 			v = -v;
-		}
 		for( bits = 0; v ; v /= 10, bits += 4 ) {
 			val |= (v % 10)<<bits;
 		}
-		val &= ~((uint64_t)1<<63);
+		val &= ~signflag;
 		val |= negflag;
 		//printf("%lx\n", val);
 	}
@@ -34,17 +34,18 @@ struct bcdint {
 	bcdint(const bcdint & v) : val(v.val) {}
 
 	static bool greaterthan(const bcdint & a, const bcdint & b) {
-		if (a.sign() >= 0 && b.sign() < 0) {
+		if (a.nonnegative() && !b.nonnegative()) {
 			return true;
-		} else if (a.sign() < 0 && b.sign() >= 0) {
-			return true;
-		} else if (a.sign() >= 0 && b.sign() >= 0) {
+		} else if (!a.nonnegative() && b.nonnegative()) {
+			return false;
+		} else if (a.nonnegative() && b.nonnegative()) {
 			return a.val > b.val;
 		} else {
 			return b.val > a.val;
 		}
 	}
 
+	// addition of nonnegative values;
 	static bcdint add(const bcdint & a, const bcdint & b) {
 		uint64_t a_val = a.val, b_val = b.val;
 		bcdint r = 0;
@@ -62,6 +63,7 @@ struct bcdint {
 		return r;
 	}
 
+	// subtraction between nonnegative values
 	static bcdint sub(const bcdint & a, const bcdint & b) {
 		uint64_t a_val, b_val, sgn;
 		bcdint r = 0;
@@ -70,7 +72,6 @@ struct bcdint {
 			a_val = a.val, b_val = b.val;
 			sgn = 0;
 		} else {
-			printf("negative\n");
 			b_val = a.val, a_val = b.val;
 			sgn = (uint64_t)1 << 63;
 		}
@@ -83,8 +84,29 @@ struct bcdint {
 			}
 			r.val |= d<<bits;
 		}
-		printf("r.val = %ld\n", r.val);
 		r.val |= sgn;
+		return r;
+	}
+
+	static bcdint mul(const bcdint & a, const bcdint & b) {
+		uint64_t a_val = a.val & ~signflag;
+		uint64_t b_val = b.val & ~signflag;
+		bcdint r = 0, s = 0;
+		uint64_t s_val;
+		int carry = 0;
+		for(int j = 0; b_val; b_val >>= 4, ++j) {
+			int d = b_val & 0x0f;
+			s_val = 0;
+			for(int i = 0; i < sizeof(uint64_t)*2; ++i) {
+				int t = ((a_val>>(i*4)) & 0x0f) * d;
+				t += carry;
+				carry = t / 10;
+				t %= 10;
+				s_val |= t<<((i+j)*4);
+			}
+			s.val = s_val;
+			r = add(r, s);  // replace bcdint on everytime.
+		}
 		return r;
 	}
 
@@ -93,17 +115,28 @@ struct bcdint {
 		r.val ^= ((uint64_t)1 << 63);
 		return r;
 	}
+
+	operator long() const {
+		long t = 0, d = 1;
+		uint64_t v = this->val;
+		for(int i = 0; i < sizeof(long)*2; ++i) {
+			t += (v & 0x0f) * d;
+			v >>= 4;
+			d *= 10;
+		}
+		return t;
+	}
+
+	bool nonnegative() const {
+		return (val & signflag) == 0;
+	}
+
 	int sign() const {
-		if ( ( val & ~((uint64_t)1 << 63) ) == 0 )
-			return 0;
-		if ( (val & ((uint64_t)1 << 63)) != 0 )
-			return -1;
-		else
-			return 1;
+		return (val & ~signflag) ? ((val & signflag) ? -1 : 1) : 0;
 	}
 
 	uint64_t value() const {
-		return ( val & ~((uint64_t)1 << 63) );
+		return val & ~signflag;
 	}
 
 	bcdint operator-() {
@@ -111,18 +144,33 @@ struct bcdint {
 	}
 
 	bcdint operator+(const bcdint & b) {
-		return bcdint::add(*this, b);
+		if (this->sign() >= 0) {
+			if (b.sign() >= 0) {
+				return add(*this, b);
+			} else {
+				return sub(*this, b);
+			}
+		}
+		if (b.sign() >= 0) {
+			return sub(b, *this);
+		} else {
+			return negate(add(negate(*this), negate(b)));
+		}
 	}
 
 	bcdint operator-(const bcdint & b) {
-		if (this->sign() < 0 && b.sign() < 0) {
-			return negate(add(negate(*this), negate(b)));
-		} else if (this->sign() < 0 && b.sign() >= 0) {
-			return sub(b, negate(*this));
-		} else if (this->sign() >= 0 && b.sign() < 0) {
-			return add(*this, negate(b));
+		if (this->sign() < 0) {
+			if (b.sign() < 0) {
+				return negate(add(negate(*this), negate(b)));
+			} else {
+				return sub(b, negate(*this));
+			}
 		}
-		return bcdint::sub(*this, b);
+		if (b.sign() < 0) {
+			return add(*this, negate(b));
+		} else {
+			return bcdint::sub(*this, b);
+		}
 	}
 
 	friend std::ostream & operator<<(std::ostream & out, const bcdint & bcd) {
--- BCDLib/test.cpp
+++ BCDLib/test.cpp
@@ -3,14 +3,15 @@
 #include "bcdlib.h"
 
 int main(int argc, char **argv) {
-	bcdint x = 12;
-	bcdint y = 19;
-	std::cout << x.sign() << ", " << y.sign() << std::endl;
+	bcdint x = 13;
+	bcdint y = 409;
 	std::cout << x << ", " << y << std::endl;
 	std::cout << x - y << std::endl;
-	std::cout << y - x << std::endl;
+	std::cout << y + x << std::endl;
+	std::cout << std::dec << (long) (x+y) << std::endl;
 	x = -x;
 	std::cout << x - y << std::endl;
-	std::cout << y - x << std::endl;
+	std::cout << "mul(x,y) " << bcdint::mul(x, y) << std::endl;
+	std::cout << "mul(y,x) " << bcdint::mul(y, x) << std::endl;
 	return 0;
 }
