8f25b9eda17a8014a34265ed4d646fa559ac96cc
Sin Shimozono <una.veritas@me.com>
2021-10-01 16:03:54 +0900

    dfa.c 2021101.1

--- dfa/src/dfa.c
+++ dfa/src/dfa.c
@@ -2,7 +2,7 @@
  ============================================================================
  Name        : dfa.c
  Author      : Sin Shimozono
- Version     :
+ Version     : 20211001.1
  Copyright   : Your copyright notice
  Description : Hello World in C, Ansi-style
  ============================================================================
@@ -18,7 +18,7 @@
 
 typedef struct {
 	/* 状態は ASCII 文字, 状態の集合は char 型の {1,...,127} の部分集合に限定. */
-	/* 状態 0 は black hole state ブラックホールとして扱う．  */
+	/* ただし状態 127 は遷移未定義の場合にたどり着く特別な吸い込み非受理状態として扱う．  */
 	/* 文字は ASCII 文字, 有限アルファベットは char 型の {1,...,127} の部分集合に限定. */
 	/* 文字 0 は文字列終端記号（ヌル文字）として処理されるため使用しない. */
 	char delta[STATE_LIMIT][ALPHABET_LIMIT];	/* 遷移関数 */
@@ -29,14 +29,14 @@ typedef struct {
 } dfa;
 
 /* マーカーとして使用する定数 */
-#define TRANSITION_NOT_DEFINED 	0
+#define TRANSITION_NOT_DEFINED 	127
 #define STATE_IS_NOT_FINAL 		0
 #define STATE_IS_FINAL 			1
 
 /* 定義文字列から dfa を初期化 */
 void dfa_define(dfa * mp,
 		char * trans,
-		char * initial,
+		char initial,
 		char * finals) {
 	char triple[7];
 	char * ptr = trans;
@@ -58,10 +58,11 @@ void dfa_define(dfa * mp,
 		++ptr;
 		//printf("%s\n", ptr);
 	}
-	mp->initial = *initial;
+	mp->initial = initial;
 	for(ptr = finals; *ptr ; ++ptr) {
 		mp->finals[(int)*ptr] = STATE_IS_FINAL;
 	}
+	mp->finals[0x7f] = STATE_IS_NOT_FINAL; /* 念のため */
 }
 
 void dfa_reset(dfa * mp) {
@@ -69,10 +70,7 @@ void dfa_reset(dfa * mp) {
 }
 
 char dfa_transfer(dfa * mp, char a) {
-	if (mp->delta[(int)mp->current][(int)a] != 0) /* defined */
-		mp->current = mp->delta[(int)mp->current][(int)a];
-	/* else
-		if omitted, go to and self-loop in the black hole state. */
+	mp->current = mp->delta[(int)mp->current][(int)a];
 	return mp->current;
 }
 
@@ -94,7 +92,7 @@ void dfa_print(dfa * mp) {
 	}
 	for(int i = 0; i < STATE_LIMIT; ++i) {
 		for(int a = 0; a < ALPHABET_LIMIT; ++a) {
-			if ( mp->delta[i][a] ) {
+			if ( mp->delta[i][a] != TRANSITION_NOT_DEFINED) {
 				states[i] = 1;
 				states[(int)mp->delta[i][a]] = 1;
 				alphabet[a] = 1;
@@ -131,7 +129,7 @@ void dfa_print(dfa * mp) {
 	printf("------------+------\n");
 	for(int i = 0; i < STATE_LIMIT; ++i) {
 		for(int a = 0; a < ALPHABET_LIMIT; ++a) {
-			if ( mp->delta[i][a] ) {
+			if ( mp->delta[i][a] != TRANSITION_NOT_DEFINED) {
 				printf(" '%c' , '%c'  | '%c'\n",i,a,mp->delta[i][a]);
 			}
 		}
@@ -150,47 +148,60 @@ void dfa_print(dfa * mp) {
 		}
 	}
 	printf("}\n)\n");
+	fflush(stdout);
 }
 
 /* 文字列にたいして dfa を走らせる */
 int dfa_run(dfa * mp, char * inputstr) {
 	char * ptr = inputstr;
-	printf("On input '%s' :\n", ptr);
+	printf("input '%s' :\n", ptr);
 	dfa_reset(mp); 	/* 状態を初期状態にする */
 	printf("   -> '%c'", mp->current);
 	for ( ; *ptr; ++ptr) {
 		dfa_transfer(mp, *ptr); 	/* 遷移する */
-		printf(", -'%c'-> '%c'", *ptr, mp->current);
+		if (mp->current == TRANSITION_NOT_DEFINED)
+			printf(", -'%c'-> %s", *ptr, "(not defined)");
+		else
+			printf(", -'%c'-> '%c'", *ptr, mp->current);
 	}
 	if ( dfa_accepting(mp) ) {
 		/* 受理した */
 		printf(", \naccepted.\n");
+		fflush(stdout);
 		return STATE_IS_FINAL;
 	} else {
 		/* 却下した */
 		printf(", \nrejected.\n");
+		fflush(stdout);
 		return STATE_IS_NOT_FINAL;
 	}
 }
 
-int command_arguments(int , char ** , char * , char * , char * , char *);
+int command_arguments(int , char ** , char ** , char * , char ** , char *);
 
 int main(int argc, char **argv) {
-	char * delta = "0a1,0b2,1a2,1b0", *initial = "0", *finals = "0";
-	char input_buff[1024] = "abaababbab";
-	if ( command_arguments(argc, argv, delta, initial, finals, input_buff) )
+	char *delta = "0a1,0b0,1a0,1b1", initial = '0', *finals = "0";
+	char input_buff[255] = "abbaaabababb";
+	if ( command_arguments(argc, argv, &delta, &initial, &finals, input_buff) )
 		return 1;
 
 	dfa M;
-	printf("M is using %0.2f Kbytes.\n\n", (double)(sizeof(M)/1024) );
+	//printf("%s\n", delta);
+	//printf("M is using %0.2f Kbytes memory space.\n\n", (double)(sizeof(M)/1024) );
 	dfa_define(&M, delta, initial, finals);
 	dfa_print(&M);
-	if (strlen(input_buff))
+	if (strlen(input_buff)) {
 		dfa_run(&M, input_buff);
-	else {
+	} else {
+		printf("Type an input as a line, or quit by the empty line.\n");
+		fflush(stdout);
 		/* 標準入力から一行ずつ，入力文字列として走らせる */
-		while( fgets(input_buff, 1023, stdin) ) {
-			for(char * p = input_buff+strlen(input_buff); *--p == '\n'; *p = '\0') ; /* 改行は無視 */
+		while( fgets(input_buff, 255, stdin) ) {
+			char * p;
+			for(p = input_buff; *p != '\n' && *p != '\r' && *p != 0; ++p) ;
+			*p = '\0'; /* 行末の改行は消す */
+			if (!strlen(input_buff))
+				break;
 			dfa_run(&M, input_buff);
 		}
 	}
@@ -199,14 +210,14 @@ int main(int argc, char **argv) {
 }
 
 
-int command_arguments(int argc, char * argv[], char * delta, char * initial, char * finals, char * input) {
+int command_arguments(int argc, char * argv[], char ** delta, char * initial, char ** finals, char * input) {
 	if (argc > 1) {
 		if (strcmp(argv[1], "-h") == 0 ) {
 			printf("usage: command \"transition triples\" \"initial state\" \"final states\" (\"input string\")\n");
-			printf("example: dfa.exe \"%s\" \"%s\" \"%s\"\n", delta, initial, finals);
+			printf("example: dfa.exe \"%s\" \"%c\" \"%s\"\n\n", *delta, *initial, *finals);
 			return 1;
 		} else if (argc == 4 || argc == 5 ) {
-			delta = argv[1]; initial = argv[2]; finals = argv[3];
+			*delta = argv[1]; *initial = argv[2][0]; *finals = argv[3];
 			if (argc == 5 )
 				strcpy(input, argv[4]);
 			else
@@ -216,8 +227,8 @@ int command_arguments(int argc, char * argv[], char * delta, char * initial, cha
 			return 1;
 		}
 	} else {
-		printf("define M by buily-in example: \"%s\" \"%s\" \"%s\"\n", delta, initial, finals);
-		printf("(Use 'command -h' to get a help message.)\n");
+		printf("define M by buily-in example: \"%s\" \"%c\" \"%s\"\n", *delta, *initial, *finals);
+		printf("(Use 'command -h' to get a help message.)\n\n");
 	}
 	return 0;
 }
