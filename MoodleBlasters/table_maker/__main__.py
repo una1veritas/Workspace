d2dd6173cf8584e202792e4b9012ec42077b6456
Sin Shimozono <una.veritas@icloud.com>
2021-11-11 17:21:13 +0900

    make table

--- MoodleBlasters/table_maker/__main__.py
+++ MoodleBlasters/table_maker/__main__.py
@@ -1,16 +1,18 @@
 import os
-import pathlib
-import datetime
-import time
-import platform
+import sys
 
-fullpath_name = u'/Users/sin/Dropbox/離散数学Ⅱ-出席＋レポート'
+fullpath_name = u'/Users/sin/Dropbox/アルゴリズム設計Ａ/202104/data'
 
-p = pathlib.Path(fullpath_name)
-print(p.stat())
+# p = pathlib.Path(fullpath_name)
+# print(p.stat())
 
-print(os.path.getmtime(fullpath_name))
-# 1549094615.9723485
-
-print(os.path.getctime(fullpath_name))
-# 1549094615.9723485
+print(fullpath_name)
+if os.path.isdir(fullpath_name) :
+    for filename in sorted(os.listdir(fullpath_name)):
+        if os.path.isdir(os.path.join(fullpath_name,filename)) and filename.startswith('C11L202-2021-0101-1121010101-'):
+            print(filename)
+# print(os.path.getmtime(fullpath_name))
+# # 1549094615.9723485
+#
+# print(os.path.getctime(fullpath_name))
+# # 1549094615.9723485
