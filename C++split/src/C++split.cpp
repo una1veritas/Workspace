bb8c0e47a9c55ac0fa8d562e6bb082030fcef11c
Sin Shimozono <una.veritas@me.com>
2022-06-09 00:01:05 +0900

    split

---
+++ C++split/src/C++split.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : C++split.cpp
+// Author      : Sin Shimozono
+// Version     :
+// Copyright   : GPL
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <iterator>
+using namespace std;
+
+int main(const int argc, const char * argv[]) {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+
+
+	return 0;
+}
