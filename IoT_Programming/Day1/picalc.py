bd8498d0021d301f3df3c7eb462db6d5b099bff1
Sin Shimozono <una.veritas@me.com>
2022-07-08 01:14:54 +0900

    py

--- IoT_Programming/Day1/picalc.py
+++ IoT_Programming/Day1/picalc.py
@@ -2,12 +2,16 @@
 #π/4 を求めるものである．
 #ここでは i 番目の pi4 とその直前の値 prev の平均の 4 倍（和の2倍）としてπを求める
 
+import sys
+
+error = float(sys.argv[1])
+
 pi4 = 1
 prev = 0
 sgn = -1
 
 i = 1
-while 2*abs(pi4-prev) > 0.1:
+while 2*abs(pi4-prev) > error:
     prev = pi4
     pi4 += sgn/(2*i+1)
     sgn = -sgn
