3df29d7abcaaf43a1aba038df52de4b1c22a0741
Sin Shimozono <una.veritas@me.com>
2022-08-23 09:48:59 +0900

    tex

--- IoT_Programming/Day1/sieve_byset.py
+++ IoT_Programming/Day1/sieve_byset.py
@@ -3,20 +3,27 @@ import sys
 # エラトステネスのふるい
 # 最小範囲は 100 まで，コマンドライン引数があれば最初の引数を範囲とする．
 
-n = 100
+n = 1024
 ith = 0
+
 if len(sys.argv) > 1 :
-    n = max(n, int(sys.argv[1]) )
-if len(sys.argv) > 2 :
-    ith = max(ith, int(sys.argv[2]) )
+    ith = max(ith, int(sys.argv[1]) )
 
-pset = set(range(2, n+1)) # range を set に変換
+while True :
+    print("With array of size " + str(n) + ".")
+    pset = set(range(2, n+1)) # range を set に変換
 
-for i in range(2, n+1) :
-    for c in range(i*2, n+1, i):
-        pset.discard(c)
+    for i in range(2, n+1) :
+        for c in range(i*2, n+1, i):
+            pset.discard(c)
+    
+    if ith == 0 :
+        break
+    if len(pset) >= ith :
+        break
+    n *= 2
 
-print("Input: " + str(n))
+print("the number of found primes: " + str(len(pset)))
 parray = sorted(pset)
 if ith :
     print(parray[ith-1])
