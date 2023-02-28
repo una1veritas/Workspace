5f329f1e8fa63afe3cc81f00f5930f163da77687
una1veritas <una.vertas@icloud.com>
2023-03-01 00:06:46 +0900

    table maker

--- MoodleBlasters/table_maker/__main__.py
+++ MoodleBlasters/table_maker/__main__.py
@@ -1,32 +1,28 @@
 import os, glob, sys
 
-if len(sys.argv) <= 2 or len(sys.argv[1]) == 0 :
-    print('arguments: [base directory path] [csv name list file name]', file=sys.stderr)
+path = ""
+namelist = ""
+if len(sys.argv) <= 2 :
+    print('arguments: [base directory] [csv name list file]', file=sys.stderr)
     exit(1)
 else:
     if os.path.isdir(sys.argv[1]) :
         path = sys.argv[1]
     else:
         path = os.path.dirname(sys.argv[1])
-print(sys.argv[1:])
+    if os.path.isfile(sys.argv[2]):
+        namelist = sys.argv[2]
+    else:
+        print("file "+sys.argv[2]+ " does not exist.")
+        exit(1)
 print("path = " + path)
+print("name list file = " + namelist)
 
-namelistf = ''
-if len(sys.argv[2]) != 0 :
-    namelistf = path+'/'+sys.argv[2]
-else:
-    if len(glob.glob(path+'seiseki*.csv')) == 1 :
-        namelistf = glob.glob(path+'/seiseki*.csv')[0]
-        print('Found "'+namelistf+'" for registered students names.')
-if len(namelistf) == 0 :
-    print('Not found name flie "namelist*.csv" at '+path+'. ', file=sys.stderr)
-    exit(1)
-
-print('Reading the list of registered students from '+namelistf)
+print('Reading the list of registered students from '+namelist)
 db_registered = list()
 COMMENTESCAPE = '#'
 separator = ','
-with open(namelistf, encoding = "utf-8-sig") as f:
+with open(namelist, encoding = "utf-8-sig") as f:
     for l in f.readlines():
         l = l.strip('"')
         if l[0] == COMMENTESCAPE : continue
@@ -34,6 +30,7 @@ with open(namelistf, encoding = "utf-8-sig") as f:
         # (sid, namestr, dep, grade)
         db_registered.append( (fields[2], fields[1], fields[4], fields[6]) )
         #print(fields)
+print()
 
 db_registered.sort(key=(lambda x: x[0]))
 #for i in db_registered:
@@ -44,9 +41,9 @@ for row in db_registered[:5] :
 else:
     print("...", end="\n\n")
 
-print('Collecting assignment folders.')
+print('Collecting assignment folders...')
 db_folders = list()
-for fdname in glob.glob(path+'/*'):
+for fdname in glob.glob(os.path.join(path, '*')):
     if  os.path.isdir(fdname) : 
         db_folders.append( fdname  )
 db_folders.sort()
@@ -55,7 +52,7 @@ if len(db_folders) :
         print(folder)
     else:
         print("...")
-    print('Found ' + str(len(db_folders)) + ' folders for reports.', end="\n\n")
+    print('Found ' + str(len(db_folders)) + ' folders.', end="\n\n")
 else:
     print('No assignment folders. stop.')
     exit()
@@ -63,40 +60,42 @@ else:
 print('Building attendance table...')
 att_table = dict()
 for folder in db_folders:
-    submissions = [d for d in glob.glob(folder)]
-    print(folder, str(submissions))
-    break
-    for file_or_folder in submissions:
+    submissions = [d for d in glob.glob(os.path.join(folder,"*"))]
+    print(os.path.basename(folder), "submissions = ", len(submissions))
+    for f_or_d in submissions:
         folder_sum = 0
         folder_count = 0
-        with os.scandir(file_or_folder) as it:
-            for entry in it:
-                if not entry.name.startswith('.') and entry.is_file() :
-                    folder_count += 1
-                    folder_sum += entry.stat().st_size
-        if folder_sum < 16*1024 :
-            print(file_or_folder+'/'+entry.name)
-            print('warning! folder sum is '+str(round(folder_sum/1024,1))+', less than 16kB.')
-        sid = os.path.basename(file_or_folder).split('_')[0]
-        att_table[(folder,sid)] = folder_sum if folder_count > 0 else 'x'
+        if os.path.isdir(f_or_d) :
+            with os.scandir(f_or_d) as it:
+                for entry in it:
+                    if not entry.name.startswith('.') and entry.is_file() :
+                        folder_count += 1
+                        folder_sum += entry.stat().st_size
+            print(folder_sum)
+            if folder_sum < 16*1024 :
+                print(f_or_d+'/'+entry.name)
+                print('warning! folder sum is '+str(round(folder_sum/1024,1))+', less than 16kB.')
+            sid = os.path.basename(f_or_d).split('_')[0]
+            att_table[(os.path.basename(folder),sid)] = folder_sum if folder_count > 0 else 'x'
 
-#print(att_table.keys())    
-#exit()
+print(att_table.keys())    
 
 presence = dict()
 presence[' sid'] = list()
 for (sid, namestr, dep, grade) in db_registered:
     presence[sid] = list()
 for dt in db_folders:
-    presence[' sid'].append(dt)
+    datename = os.path.basename(dt)
+    presence[' sid'].append(datename)
     for (sid, namestr, dep, grade) in db_registered:
-        if (dt, sid) in att_table :
-            sz = str(att_table[(dt, sid)]/1024)
+        print(datename, sid)
+        if (datename, sid) in att_table :
+            sz = str(att_table[(datename, sid)]/1024)
         else:
             sz = 0
         presence[sid].append(str(sz))
 
-with open(path+'attendance.csv', mode='w', encoding='utf-8-sig') as outputf:
+with open(os.path.join(path, 'attendance.csv'), mode='w', encoding='utf-8-sig') as outputf:
     for sid in presence.keys():
         outputf.write(sid)
         outputf.write(',')
