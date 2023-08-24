32de11bec1755fdbe94885cd12688c3977c65d3a
Sin Shimozono <una.veritas@icloud.com>
2023-08-24 16:11:36 +0900

    table maker 4

--- MoodleBlasters/tablemaker4/__main__.py
+++ MoodleBlasters/tablemaker4/__main__.py
@@ -24,18 +24,19 @@ def read_registered_students(path):
                     continue # skip this an empty or a comment line.
                 if line_counter == 0 :
                     # table column header
-                    table['header'] = a_line.split('\t')
+                    table['header'] = a_line.split(',')
                 else:
                     row = a_line.split(',')
                     table['cells'].append(row)
                 line_counter += 1
-            print(line_counter)
+            #print(line_counter)
     return table
 
-def inspect_reports(path, table):
+def inspect_reports(path):
     if not os.path.isdir(path) :
         print('error: this is not a directory path.')
         exit()
+    table = dict()
     fdname_list = glob.glob(os.path.join(path, '*'))
     assignment_folders = list()
     for each in fdname_list:
@@ -44,17 +45,63 @@ def inspect_reports(path, table):
     assignment_folders.sort()
     for folder in assignment_folders :
         folderpath = os.path.join(path, folder)
+        table[folder] = dict()
         for filepath in glob.glob(os.path.join(folderpath, '*')) :
             filename = os.path.basename(filepath)
-            sid = filename.split('_')[1]
-            print(folder, sid, os.path.getsize(filepath))
-            
+            sid = filename.split('_')[0]
+            #print(folder, sid, os.path.getsize(filepath))
+            table[folder][sid] = os.path.getsize(filepath)
+    return table
 
+def make_table(registered, reports):
+    students = list()
+    assignments = list()
+    sidpos = -1
+    #print(registered['header'])
+    for pos in range(len(registered['header'])):
+        if registered['header'][pos] == u'学生番号' :
+            sidpos = pos
+            break
+    if sidpos == -1 :
+        print('error: can''t find id in regtable header.')
+        exit()
+    for entry in registered['cells']:
+        students.append(entry[sidpos])
+    students.sort()
+    #print(students)
+    assignments = sorted(reports.keys())
+    #print(assignments)
+    
+    result = list()
+    ref = dict()
+    result.append([u'学生番号'])
+    # タイトル行　課題名
+    for each in assignments :
+        result[-1].append(each)
+    for each in students:
+        result.append([each])
+        ref[each] = result[-1]
+        for i in range(len(assignments)) :
+            result[-1].append(0)
+    #print(ref)
+    for assign_id in range(len(reports)):
+        for sid in students:
+            if sid in reports[assignments[assign_id]] :
+                assignname = assignments[assign_id]
+                submsize = reports[assignname][sid]
+                ref[sid][1+assign_id] = submsize
+    return result
 
 regtable = read_registered_students(dirpath)
-repotable = dict()
-for a_pair in sorted([(each[4], each[7]) for each in regtable['cells']]) :
-    repotable[a_pair[0]] = dict()
-
-inspect_reports(dirpath, repotable)
+repotable = inspect_reports(dirpath)
+table = make_table(regtable, repotable)
+with open('result.txt', mode = 'w') as outfile:
+    for row in table:
+        outfile.write(row[0])
+        if len(row) > 1 :
+            for i in range(1, len(row)):
+                outfile.write('\t')
+                outfile.write(str(row[i]))
+        outfile.write('\n')
+        
 print("finished.")
