44cc29afb4956ffb6133697676f8c808df5a7ee8
una1veritas <una.vertas@icloud.com>
2024-02-18 17:49:46 +0900

    feedback

--- PyBThesisCollator/feedbackmaker/__main__.py
+++ PyBThesisCollator/feedbackmaker/__main__.py
@@ -6,6 +6,112 @@ Created on 2024/01/20
 import sys, os, glob, re
 from openpyxl import Workbook, load_workbook
 
+def feedback_opening():
+    return '''<?xml version="1.0" encoding="UTF-8" ?>
+<FEEDBACK VERSION="200701" COMMENT="XML-Importfile for mod/feedback">
+     <ITEMS>
+'''
+def feedback_closing():
+    return '''     </ITEMS>
+</FEEDBACK>
+'''
+
+def feedback_session_header(start_id, gno, tspan, supers, id_eval = None):
+    if id_eval == None :
+        id_eval = start_id+1
+    tmpstr = '''          <ITEM TYPE="label" REQUIRED="0">
+               <ITEMID><![CDATA[{id_header}]]></ITEMID>
+               <ITEMTEXT><![CDATA[]]></ITEMTEXT>
+               <ITEMLABEL><![CDATA[]]></ITEMLABEL>
+               <PRESENTATION><![CDATA[<h4>グループ{group_no}, {time_span} ({supervisors})</h4>]]></PRESENTATION>
+               <OPTIONS><![CDATA[]]></OPTIONS>
+               <DEPENDITEM><![CDATA[0]]></DEPENDITEM>
+               <DEPENDVALUE><![CDATA[]]></DEPENDVALUE>
+          </ITEM>
+          <ITEM TYPE="label" REQUIRED="0">
+               <ITEMID><![CDATA[{id_evalhint}]]></ITEMID>
+               <ITEMTEXT><![CDATA[]]></ITEMTEXT>
+               <ITEMLABEL><![CDATA[]]></ITEMLABEL>
+               <PRESENTATION><![CDATA[<p><strong><span style="color: #ff0000;">評価値　　A: 100点、B:90点、C:80点、D:70点、E:60点、X:0点</span></strong></p>]]></PRESENTATION>
+               <OPTIONS><![CDATA[]]></OPTIONS>
+               <DEPENDITEM><![CDATA[0]]></DEPENDITEM>
+               <DEPENDVALUE><![CDATA[]]></DEPENDVALUE>
+          </ITEM>
+'''.format(id_header=start_id, group_no = gno, time_span=tspan,supervisors=supers,id_evalhint= id_eval)
+    return (tmpstr, id_eval+1)
+
+def feedback_questions(start_id, sid, sname):
+    tmpstr = '''          <ITEM TYPE="label" REQUIRED="0">
+               <ITEMID><![CDATA[{id_qheader}]]></ITEMID>
+               <ITEMTEXT><![CDATA[]]></ITEMTEXT>
+               <ITEMLABEL><![CDATA[]]></ITEMLABEL>
+               <PRESENTATION><![CDATA[<h5>{studentid}&nbsp; &nbsp;{studentname}</h5>]]></PRESENTATION>
+               <OPTIONS><![CDATA[]]></OPTIONS>
+               <DEPENDITEM><![CDATA[0]]></DEPENDITEM>
+               <DEPENDVALUE><![CDATA[]]></DEPENDVALUE>
+          </ITEM>
+          <ITEM TYPE="multichoice" REQUIRED="0">
+               <ITEMID><![CDATA[{id_q0}]]></ITEMID>
+               <ITEMTEXT><![CDATA[この学生の指導教員です]]></ITEMTEXT>
+               <ITEMLABEL><![CDATA[{studentid}-0]]></ITEMLABEL>
+               <PRESENTATION><![CDATA[c>>>>>YES<<<<<1]]></PRESENTATION>
+               <OPTIONS><![CDATA[h]]></OPTIONS>
+               <DEPENDITEM><![CDATA[0]]></DEPENDITEM>
+               <DEPENDVALUE><![CDATA[]]></DEPENDVALUE>
+          </ITEM>
+          <ITEM TYPE="multichoice" REQUIRED="0">
+               <ITEMID><![CDATA[{id_q1}]]></ITEMID>
+               <ITEMTEXT><![CDATA[1. 研究成果]]></ITEMTEXT>
+               <ITEMLABEL><![CDATA[{studentid}-1]]></ITEMLABEL>
+               <PRESENTATION><![CDATA[r>>>>>A|B|C|D|E|X<<<<<1]]></PRESENTATION>
+               <OPTIONS><![CDATA[h]]></OPTIONS>
+               <DEPENDITEM><![CDATA[0]]></DEPENDITEM>
+               <DEPENDVALUE><![CDATA[]]></DEPENDVALUE>
+          </ITEM>
+          <ITEM TYPE="multichoice" REQUIRED="0">
+               <ITEMID><![CDATA[{id_q2}]]></ITEMID>
+               <ITEMTEXT><![CDATA[2. 問題解決]]></ITEMTEXT>
+               <ITEMLABEL><![CDATA[{studentid}-2]]></ITEMLABEL>
+               <PRESENTATION><![CDATA[r>>>>>A|B|C|D|E|X<<<<<1]]></PRESENTATION>
+               <OPTIONS><![CDATA[h]]></OPTIONS>
+               <DEPENDITEM><![CDATA[0]]></DEPENDITEM>
+               <DEPENDVALUE><![CDATA[]]></DEPENDVALUE>
+          </ITEM>
+          <ITEM TYPE="multichoice" REQUIRED="0">
+               <ITEMID><![CDATA[{id_q3}]]></ITEMID>
+               <ITEMTEXT><![CDATA[3. コミュニケーション能力]]></ITEMTEXT>
+               <ITEMLABEL><![CDATA[{studentid}-3]]></ITEMLABEL>
+               <PRESENTATION><![CDATA[r>>>>>A|B|C|D|E|X<<<<<1]]></PRESENTATION>
+               <OPTIONS><![CDATA[h]]></OPTIONS>
+               <DEPENDITEM><![CDATA[0]]></DEPENDITEM>
+               <DEPENDVALUE><![CDATA[]]></DEPENDVALUE>
+          </ITEM>
+          <ITEM TYPE="multichoice" REQUIRED="0">
+               <ITEMID><![CDATA[{id_q4}]]></ITEMID>
+               <ITEMTEXT><![CDATA[4. 論理的記述能力]]></ITEMTEXT>
+               <ITEMLABEL><![CDATA[{studentid}-4]]></ITEMLABEL>
+               <PRESENTATION><![CDATA[r>>>>>A|B|C|D|E|X<<<<<1]]></PRESENTATION>
+               <OPTIONS><![CDATA[h]]></OPTIONS>
+               <DEPENDITEM><![CDATA[0]]></DEPENDITEM>
+               <DEPENDVALUE><![CDATA[]]></DEPENDVALUE>
+          </ITEM>
+          <ITEM TYPE="multichoice" REQUIRED="0">
+               <ITEMID><![CDATA[{id_q5}]]></ITEMID>
+               <ITEMTEXT><![CDATA[5. 継続的学習能力（指導教員のみ評価が有効）]]></ITEMTEXT>
+               <ITEMLABEL><![CDATA[{studentid}-5]]></ITEMLABEL>
+               <PRESENTATION><![CDATA[r>>>>>A|B|C|D|E|X<<<<<1]]></PRESENTATION>
+               <OPTIONS><![CDATA[h]]></OPTIONS>
+               <DEPENDITEM><![CDATA[{id_q0}]]></DEPENDITEM>
+               <DEPENDVALUE><![CDATA[YES]]></DEPENDVALUE>
+          </ITEM>
+     </ITEMS>
+</FEEDBACK>
+'''.format(id_qheader=start_id, studentid=sid, studentname = sname, 
+           id_q0 = start_id + 1, id_q1 = start_id + 2, id_q2 = start_id + 3,
+           id_q3 = start_id + 4, id_q4 = start_id + 5, id_q5 = start_id + 7)
+    return (tmpstr, start_id+8)
+
+
 #row_heading 行を heading としてシートの内容を読み出す
 def btinfo_db(fname, row_heading = 1):
     try:
@@ -46,9 +152,9 @@ def main(argv):
 
     try:
         (btcolumn, btrows) = btinfo_db(btinfofile, 2)
-        btrows.sort(key=lambda x: x[btcolumn["研究室内発表順"]]) # sort by 研究室内順番
-        btrows.sort(key=lambda x: x[btcolumn["研究室発表順"]]) # sort by 発表順番
-        btrows.sort(key=lambda x: x[btcolumn["発表グループ"]]) # sort by グループ
+        #btrows.sort(key=lambda x: x[btcolumn["開始"]]) # sort by 研究室内順番
+        #btrows.sort(key=lambda x: x[btcolumn["終了"]]) # sort by 発表順番
+        #btrows.sort(key=lambda x: x[btcolumn["グループ"]]) # sort by グループ
         #print(wb.sheetnames)
         # print(btcolumn)
         # for r in btrows:
@@ -59,7 +165,23 @@ def main(argv):
         print("Failed to read workshhet file {}.".format(btinfofile))
         exit(1)
     
-    with open("tempout.csv", mode="w", encoding="utf-8") as outcsvf:
+    print(btcolumn)
+    super = None
+    for a_row in btrows:
+        if a_row[btcolumn['学生番号']] is None :
+            print("rest")
+            continue
+        next_super = a_row[btcolumn['指導教員']]
+        if super is None :
+            super = next_super
+        elif super != next_super :
+            # new session
+            super = next_super
+        print(super, a_row)
+    print()
+    for a_line in feedback_questions(1000,"123C5678","川津花子")[0].split("\n"):
+        print(a_line)
+#    with open("tempout.csv", mode="w", encoding="utf-8") as outcsvf:
         
     print("bye.")
     
