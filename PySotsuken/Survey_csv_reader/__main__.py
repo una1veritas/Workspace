891e95e3910b3247a66e5aa0cece396f6e2f6dde
Sin Shimozono <una.veritas@me.com>
2024-11-26 08:37:01 +0900

    sotsuken

--- PySotsuken/Survey_csv_reader/__main__.py
+++ PySotsuken/Survey_csv_reader/__main__.py
@@ -14,20 +14,21 @@ def get_survey_dict(infilename):
             a_line = a_line.replace('\u3000', ' ')
             items = a_line.strip().split(",")
             lablist = list()
+            #print(items[2], items[6:])
             for no_labname in items[6:] :
-                labpair = no_labname.split('：')
-                if labpair[0] != '' :
-                    labpair[0] = int(labpair[0])
-                    if labpair[0] == 23 : continue
-                    lablist.append(labpair)
+                if no_labname == '' :
+                    continue
+                labpair = no_labname.split('：') 
+                labpair[0] = int(labpair[0]) 
+                if labpair[0] == 23 : 
+                    continue
+                if len(lablist) > 0 and labpair[0] >= 30 :
+                    continue
+                lablist.append(labpair)
+            #print(lablist)
             if items[2] in svy_dict:
                 print('Error: duplicated SID!')
                 raise ValueError
-            if lablist[0] == 23 :
-                lablist = lablist[2:]
-            else:
-                lablist = lablist[:2] + lablist[4:]  
-            print(lablist)
             svy_dict[items[2]] = lablist
     return svy_dict
     
@@ -43,40 +44,47 @@ def get_gpa_dict(filename = dict()) :
                 gpa_dict[items[4]] = float('NaN')
     return gpa_dict
 
+def assign_by_gpa_first(gpadict, survey_dict):
+    gpasorted = sorted(gpadict.items(), key = lambda x: x[1], reverse = True)
+    assign = dict()
+    sid_set = set([sid for sid, sgpa in gpasorted])
+    sum = 0
+    count = 0
+    for sid, sgpa in gpasorted:
+        #print(sid, sgpa)
+        if sid not in survey_dict:
+            print("Error: No desire found for " + sid)
+            continue
+        desired = survey_dict[sid]
+        #print(sid, desired)
+        for i in range(len(desired)) :
+            a_pair = desired[i]
+            (lid, lname) = (a_pair[0], a_pair[1])
+            if lid not in assign:
+                assign[lid] = list()
+            if len(assign[lid]) >= 5 :
+                continue
+            assign[lid].append(sid)
+            sum += i + 1
+            count += 1
+            sid_set.remove(sid)
+            break
+    print("average = ", sum/count)
+    return assign
+
+def assign_by_desire_first(gpa_dict, svy_dict):
+    gpasorted = sorted(gpa_dict.items(), key = lambda x: x[1], reverse = True)
+    assign = dict()
+    # for i in range()
+    return assign
+    
 if __name__ == '__main__':
     survey_dict = get_survey_dict("./students_svy.csv")
     print(survey_dict)
     gpa_dict = get_gpa_dict("./students_gpa.csv")
     print(gpa_dict)
-    gpasorted = sorted(gpa_dict.items(), key = lambda x: x[1], reverse = True)
-    print(gpasorted)
-    assign = dict()
-    count = 0
-    xcount = 0
-    sid_set = set([sid for sid, sgpa in gpasorted])
-    for sid, sgpa in gpasorted:
-        print(sid, sgpa)
-        if sid not in survey_dict:
-            print("Error: No desire found for " + sid)
-            continue
-        desired = survey_dict[sid]
-        print(sid, desired)
-        for a_pair in desired:
-            (lid, lname) = (a_pair[0], a_pair[1])
-            print(lid, lname)
-            if lid not in assign:
-                assign[lid] = list()
-            if len(assign[lid]) >= 5 :
-                xcount += 1
-                continue
-            assign[lid].append(sid)
-            sid_set.remove(sid)
-            count += 1
-            break
-    sum = 0
-    for lid, assigned in assign.items():
+    assignment = assign_by_gpa_first(gpa_dict, survey_dict)
+    
+    for lid, assigned in assignment.items():
         print(lid, assigned)
-        sum += len(assigned)
-    print("count=", count, " xcount=", xcount)
-    print("sum = ", sum, " sid_set size ", len(sid_set))
     exit(0)
