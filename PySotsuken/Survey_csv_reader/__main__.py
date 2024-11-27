9781121642c991af7e100449d41e33459921e0f5
Sin Shimozono <una.veritas@icloud.com>
2024-11-27 15:59:04 +0900

    bygpa

--- PySotsuken/Survey_csv_reader/__main__.py
+++ PySotsuken/Survey_csv_reader/__main__.py
@@ -3,6 +3,11 @@ Created on 2024/11/25
 
 @author: sin
 '''
+import math
+import statistics
+
+LAB_ASSIGNMENT_UPPERBPUND = 6
+DESIREED_LIST_LENGTH = 6
 
 def get_survey_dict(infilename):
     lcounter = 0
@@ -56,21 +61,21 @@ def assign_by_gpa_first(gpadict, survey_dict):
         if sid not in survey_dict:
             print("Error: No desire found for ",sid, sgpa)
             continue
-        desired = survey_dict[sid]
+        desired_list = survey_dict[sid]
         #print(sid, desired)
-        for i in range(len(desired)) :
-            a_pair = desired[i]
+        for i in range(len(desired_list)) :
+            a_pair = desired_list[i]
             (lid, lname) = (a_pair[0], a_pair[1])
             if lid not in assign:
                 assign[lid] = list()
-            if len(assign[lid]) >= 5 :
+            if len(assign[lid]) >= LAB_ASSIGNMENT_UPPERBPUND :
                 continue
-            assign[lid].append(sid)
+            assign[lid].append( (sid, math.ceil(sgpa*1000)/1000 ) )
             sum += i + 1
             count += 1
             sid_set.remove(sid)
             break
-    print("average = ", sum/count)
+    print("gpa first satisfaction average = ", sum/count)
     print("remained = ", sid_set)
     return assign
 
@@ -79,26 +84,49 @@ def assign_by_desire_first(gpa_dict, svy_dict):
     set_students = set(gpa_dict.keys())
     labs_determined = set()
     assign = dict()
-    for i in range(10) :
+    sum = 0
+    count = 0
+    for i in range(DESIREED_LIST_LENGTH) :
         print("desired rank ", i)
-        print(len(labs_determined), len(set_students))
+        print("labs determined = ", len(labs_determined), "remained = ", len(set_students))
         for sid in set_students:
             if sid in svy_dict and i < len(svy_dict[sid]) :
                 lid, lname = svy_dict[sid][i]
                 if lid not in assign :
                     assign[lid] = list()
-                if len(assign[lid]) < 6 :
-                    assign[lid].append((sid, gpa_dict[sid]))
-        print(assign)
+                if len(assign[lid]) < LAB_ASSIGNMENT_UPPERBPUND :
+                    assign[lid].append( (sid, math.ceil(gpa_dict[sid]*1000)/1000 ) )
+                    sum += i + 1
+                    count += 1
         for lid in assign:
             if lid in labs_determined : continue 
-            if len(assign[lid]) >= 6 :
-                assign[lid] = assign[lid][:6]
+            if len(assign[lid]) >= LAB_ASSIGNMENT_UPPERBPUND :
+                assign[lid] = assign[lid][:LAB_ASSIGNMENT_UPPERBPUND]
                 labs_determined.add(lid)
             for stu in assign[lid] :
                 if stu[0] in set_students:
                     set_students.remove(stu[0])
+    print("desire first satisfaction average = ", sum/count)
     return assign
+    
+def write_assignment(assignment, fname):
+    with open(fname, "w") as f:
+        for lid in sorted(list(assignment.keys())):
+            gpa_list = [e[1] for e  in assignment[lid]]
+            f.write(str(lid))
+            f.write('\t')
+            f.write(str(round(statistics.mean(gpa_list), 2)))
+            f.write('\t')
+            if len(gpa_list) >= 2 :
+                f.write(str(round(statistics.stdev(gpa_list), 2)))
+            else:
+                f.write("--")
+            f.write('\t')
+            for ea in assignment[lid]:
+                f.write(str(ea))
+                f.write('\t')
+            f.write('\n')
+    return
     
 if __name__ == '__main__':
     survey_dict = get_survey_dict("./students_svy.csv")
@@ -106,10 +134,9 @@ if __name__ == '__main__':
     gpa_dict = get_gpa_dict("./students_gpa.csv")
     print(gpa_dict)
     assignment = assign_by_gpa_first(gpa_dict, survey_dict)
-    
-    for lid, assigned in assignment.items():
-        print(lid, assigned)
-    
-    assign_by_desire_first(gpa_dict, survey_dict)
+    write_assignment(assignment, "bygpa.txt")
+
+    assignment = assign_by_desire_first(gpa_dict, survey_dict)
+    write_assignment(assignment, "bydesire.txt")
     
     exit(0)
