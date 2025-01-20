244b91ddd7a679dc7ac2892db2bff91f8a9d2c73
Sin Shimozono <una.veritas@me.com>
2025-01-20 21:11:30 +0900

    survey

--- PySotsuken/IntensionSurvey2024/__main__.py
+++ PySotsuken/IntensionSurvey2024/__main__.py
@@ -7,6 +7,33 @@ import pandas as pd
 import re
 import sys
 
+def read_quiz_answers(csv_file_path, columns=25):
+    df = pd.read_csv(csv_file_path)
+    # 括弧内の単語を抽出する正規表現パターン
+    pattern = r'{(.*?)}'
+    tbl = list() # as list of lists
+    for _, row in df[['名前','身分','IDナンバ','メールアドレス','受験完了','解答 1','解答 2']].iterrows() :
+        newrow = [row['名前'],row['身分'],row['IDナンバ'],row['メールアドレス'],row['受験完了']]
+        words = re.findall(pattern, row['解答 1'])
+        if len(words) == 0 :
+            newrow.append('N.A.')
+            print('error')
+        else:
+            newrow.append(words[0])
+        # 正規表現を使って括弧内の単語を抽出
+        words = re.findall(pattern, row['解答 2'])
+        for t in words:
+            thepair = t.split('：')
+            if len(thepair) != 2 :
+                continue
+            newrow.append(t)
+        if len(newrow) < columns :
+            for _ in range(len(newrow), columns) :
+                newrow.append('')
+        tbl.append(newrow)
+    return tbl
+
+
 if __name__ == '__main__':
     # CSVファイルのパスを指定
     csv_file_path = 'テスト形式配属希望調査.csv'
@@ -14,36 +41,18 @@ if __name__ == '__main__':
         csv_file_path = sys.argv[1]
     out_csv_filename = '.'.join(csv_file_path.split('.')[:-1]) + '.out.csv'
     print(out_csv_filename)
-    # CSVファイルを読み込む
-    df = pd.read_csv(csv_file_path)
-    # 括弧内の単語を抽出する正規表現パターン
-    table = list()
-    pattern = r'{(.*?)}'
-    for row_index, row in df[['IDナンバ','名前','解答 1','解答 2']].iterrows() :
-        intenseq = list()
-        intenseq.append(row['IDナンバ'])
-        words = re.findall(pattern, row['解答 1'])
-        if len(words) == 0 :
-            intenseq.append('N.A.')
-            print('error')
-        else:
-            intenseq.append(words[0])
-        # 正規表現を使って括弧内の単語を抽出
-        words = re.findall(pattern, row['解答 2'])
-        # 抽出した単語を表示
-        for t in words:
-            thepair = t.split('：')
-            if len(thepair) != 2 :
-                continue
-            intenseq.append(t)
-        if len(intenseq) > 0 :
-            table.append(intenseq)
-    with open(out_csv_filename, 'w', encoding='utf-8-sig') as outf :
-        columnnames = ['学生番号'] + ['第{0}希望'.format(i) for i in range(1,21)]
-        outf.write(','.join(columnnames))
-        outf.write('\n')
-        for r in table:
-            outf.write(','.join(r))
-            outf.write('\n')
-    print('finished.')
+    tbl = read_quiz_answers(csv_file_path, 25)
+    colnames = ['氏名','グループ','学生番号','メールアドレス','日付']
+    for i in range(1, 25-len(colnames)+1) :
+        colnames.append('第{0}希望'.format(i))
+    df = pd.DataFrame(tbl, columns=colnames)
+    # CSVファイルに書き出す
+    # df.to_csv(out_csv_filename, index=True, encoding='utf-8-sig')
+    with open(out_csv_filename, mode='w', encoding='utf-8-sig') as f :
+        f.write(','.join(df.columns.tolist()))
+        f.write('\n')
+        for _, row in df.iterrows():
+            f.write(','.join(row.tolist()))
+            f.write('\n')                
     
+    print('finished.')
