dc95bc866ec9feca18538f2dee98a7158c389540
Sin Shimozono <una.veritas@me.com>
2025-04-05 12:28:52 +0900

    text to speerch

--- PyWebSocket/demo/websocket_server.py
+++ PyWebSocket/demo/websocket_server.py
@@ -6,12 +6,14 @@ Created on 2025/04/04
 
 import asyncio
 import websockets
+import pyttsx3
 
 async def handler(websocket):
     print(f"New connection from {websocket.remote_address}")
     try:
         async for message in websocket:
             print(f"Received message: {message}")
+            text_to_speech(message, 'com.apple.eloquence.en-GB.Shelley')
             await websocket.send(f"Echo: {message}")
     except websockets.exceptions.ConnectionClosed as e:
         print(f"Connection closed: {e}")
@@ -21,4 +23,25 @@ async def main():
     print("Server started on ws://0.0.0.0:8765")
     await server.wait_closed()
 
+def text_to_speech(text, voice_id : str = 'com.apple.eloquence.ja-JP.Eddy'):
+    if len(text) == 0 :
+        return
+    # Initialize the TTS engine
+    #engine = pyttsx3.init()
+    
+    if voice_id != None :
+        # Set the voice by ID
+        engine.setProperty('voice', voice_id)  # Change index to select different voices
+    
+    # Set properties before adding anything to speak
+    engine.setProperty('rate', 150)    # Speed percent (can go over 100)
+    engine.setProperty('volume', 0.9)  # Volume 0-1
+
+    # Add text to be spoken
+    engine.say(text)
+
+    # Run the speech engine
+    engine.runAndWait()
+
+engine = pyttsx3.init()
 asyncio.run(main())
