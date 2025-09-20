6f7eeaba60be061ca2567e99939bb21ca2b8165d
Sin Shimozono <una.veritas@me.com>
2025-09-20 16:17:30 +0900

    "40.60" succeed

--- basic_io_test/src/basic_io_test.c
+++ basic_io_test/src/basic_io_test.c
@@ -11,7 +11,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char toupper(char a) {
+char upper_case(char a) {
 	if ( a < 'a' )
 		return a;
 	if ( a < 'z'+1 )
@@ -20,43 +20,37 @@ char toupper(char a) {
 }
 
 char hex2nib(char a) {
+	if ( a < '0' )
+		return a | 0x80;
 	if (a < '9' + 1) {
 		a -= '0';
-		if ( a < 0 )
-			return -1;
 		return a;
-	} else {
-		a = toupper(a);
-		if (a < 'F' + 1) {
-			a -= 'A';
-			if ( a < 0 )
-				return -1;
-			a += 10;
-			return a;
-		} else
-			return -1;
 	}
+	a = upper_case(a);
+	if ( a < 'A' )
+		return a | 0x80;
+	if ( a < 'F' + 1) {
+		a -= 'A' - 10;
+		return a;
+	}
+	return a | 0x80;
 }
 
 int main(const int argc, const char * argv[]) {
 	puts("Hello World!!!"); /* prints Hello World!!! */
 
-	if (argc <= 1)
-		return EXIT_SUCCESS;
-	printf("input %s\n", argv[1]);
-	for(const char * ptr = argv[1]; *ptr; ++ptr) {
-		char u = hex2nib(*ptr);
-		printf("%d ", u);
+	if (argc > 1) {
+		printf("input %s\n", argv[1]);
+		for(const char * ptr = argv[1]; *ptr; ++ptr) {
+			unsigned char u = hex2nib(*ptr);
+			printf("%d ", u);
+		}
 	}
 
 	printf("\nfor all the chars.\n");
-	for(char c = 0x20; c < 0x7f; ++c) {
-		char u = hex2nib(c);
-		if ( u == -1 ) {
-			printf("%c --> error\n", c);
-		} else {
-			printf("%c --> %d\n", c, u);
-		}
+	for(unsigned char c = 0x20; c < 0x7f; ++c) {
+		unsigned char u = hex2nib(c);
+		printf("%c --> %d\n", c, u);
 	}
 
 	return EXIT_SUCCESS;
