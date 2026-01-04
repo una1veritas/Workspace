6769930b0c31577ba9c2e854155cb2d61e24004c
Sin Shimozono <una.veritas@me.com>
2026-01-04 21:49:21 +0900

    shuffle16

--- PythonTest/bitshuffle16/__main__.py
+++ PythonTest/bitshuffle16/__main__.py
@@ -5,21 +5,21 @@ Created on 2026/01/04
 '''
 import random
 
-'''[11, 1, 14, 9, 13, 10, 8, 3, 0, 12, 15, 7, 2, 6, 5, 4]'''
-
-def shuffle16(val16, shuffled): 
-    val16 ^= 0xa55a
+def shuffle16(val16, shuffled = [11, 1, 14, 9, 5, 10, 8, 3, 0, 12, 15, 7, 2, 6, 13, 4]): 
+    val16 = val16^0x49d6 + 0x3ba9
     res16 = 0
     for i in shuffled :
         res16 <<= 1
         res16 |= (val16>>shuffled[i]) & 1
-    return res16
+    return res16 & 0xffff
+
 
 if __name__ == '__main__':
-    remained = set(range(0x10000))
-    shuffle_seq = list(range(16))
-    random.shuffle(shuffle_seq)
+    occurred = set(range(0x10000))
+    #shuffle_seq = list(range(16))
+    #random.shuffle(shuffle_seq)
     for i in range(0, 0x10000) :
-        remained.remove(shuffle16(i, shuffle_seq))
-        print(f'{shuffle16(i, shuffle_seq):04x}')
-    print(remained)
+        shuffled = shuffle16(i)
+        occurred.add(shuffled)
+        print(f'{shuffled:04x}')
+    print(len(occurred))
