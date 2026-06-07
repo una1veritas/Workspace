a4216fa5a55fa48bf75160caa1c1db163cdb861f
Sin Shimozono <una.veritas@me.com>
2026-06-08 01:46:32 +0900

    java test

---
+++ JavaTest/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Test t = new Test(8);
+		
+		// TODO Auto-generated method stub
+		System.out.println("Do something.");
+		System.out.println(t.specificValue);
+
+	}
+
+}
---
+++ JavaTest/src/Test.java
@@ -0,0 +1,10 @@
+
+public class Test {
+	private int id;
+
+	public static int specificValue = 9;
+	
+	public Test(int myid) {
+		id = myid;
+	}
+}
